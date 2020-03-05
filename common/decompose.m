2d82cdb14a40e7e507396f56e24c26d6b43e2908
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2020-03-05 14:53:15 -0800

    Looking at decompose + studyPt.

--- common/decompose.m
+++ common/decompose.m
@@ -9,9 +9,22 @@ function [ rvecC, normR ] = decompose( vecX, matV )
 	sizeK = size(matV,2);
 	assert( sizeK <= sizeX );
 	assert( isrealarray(matV,[sizeX,sizeK]) );
-	assert( abs( matV'*matV - eye(sizeK,sizeK) ) < 10.0 * (sizeK^3) * eps );
-	rvecC = vecX'*matV;
-	vecR = vecX - (matV*(rvecC'));
+	%
+	% If require matV to be orthonormal...
+	%assert( abs( matV'*matV - eye(sizeK,sizeK) ) < 10.0 * (sizeK^3) * eps );
+	%rvecC = vecX'*matV;
+	%vecR = vecX - (matV*(rvecC'));
+	%
+	% But, instead...
+	vecR = vecX;
+	rvecC = zeros(1,sizeK);
+	for k=1:sizeK
+		denom = sum(matV(:,k).^2);
+		assert( 0.0 < denom );
+		rvecC(k) = vecX' * matV(:,k) / denom;
+		vecR -= rvecC(k) * matV(:,k );
+	end
+	%
 	normR = sqrt( sum(vecR.^2) );
 return;
 end
--- groot/test_studyPt.m
+++ groot/test_studyPt.m
@@ -7,9 +7,9 @@ tic();
 %sizeX = 100;
 sizeX = 2;
 sizeF = sizeX;
-seedPrm = demoFunc0101_genSeedPrm("lin-easy");
+%seedPrm = demoFunc0101_genSeedPrm("lin-easy");
 %seedPrm = demoFunc0101_genSeedPrm("easy");
-%seedPrm = demoFunc0101_genSeedPrm("moderate");
+seedPrm = demoFunc0101_genSeedPrm("moderate");
 seedPrm.sizeX = sizeX;
 seedPrm.sizeF = sizeF;
 funcPrm = demoFunc0101_genFuncPrm(seedPrm);
@@ -20,6 +20,29 @@ studyPtPrm = [];
 funchF = @(vecXDummy)( demoFunc0101_eval( vecXDummy, funcPrm ) );
 funchJ = @(vecXDummy)( demoFunc0101_evalJaco( vecXDummy, funcPrm ) );
 studyPtPrm.funchJ = funchJ;
-[ vecXSuggested, retCode, studyPtDatOut ] = studyPt( funchF, vecX0, studyPtPrm );
+[ vecXSuggested, retCode, datOut ] = studyPt( funchF, vecX0, studyPtPrm );
+%
+if (1)
+	hold off;
+	plot( [0,1], [0,0], 'ko-', 'linewidth', 7, 'markersize', 20 );
+	hold on;
+	%
+	vecB = funcPrm.x0 - vecX0;
+	[ c, normR ] = decompose( datOut.vecDelta_newton, vecB );
+	plot( [0,c], [0,normR], 'bx-', 'linewidth', 6, 'markersize', 18 );
+	[ c, normR ] = decompose( datOut.vecDelta_newton_omegaMin, vecB );
+	plot( [0,c], [0,normR], 'cx-', 'linewidth', 5, 'markersize', 16 );
+	[ c, normR ] = decompose( datOut.vecDelta_gradDir, vecB );
+	plot( [0,c], [0,normR], 'g+-', 'linewidth', 4, 'markersize', 14 );
+	[ c, normR ] = decompose( datOut.vecDelta_gradDir_omegaMin, vecB );
+	plot( [0,c], [0,normR], 'ys-', 'linewidth', 3, 'markersize', 12 );
+	[ c, normR ] = decompose( datOut.vecDelta_levenberg_omegaMin, vecB );
+	plot( [0,c], [0,normR], 'rp-', 'linewidth', 2, 'markersize', 10 );
+	%
+	axis equal;
+	grid on;
+	hold off;
+	axpand;
+end
 %
 toc();
