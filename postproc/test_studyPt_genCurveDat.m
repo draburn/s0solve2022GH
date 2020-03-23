5c00ec580891d624f47f25bc24a1481ccd0bc8d3
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2020-03-23 02:31:45 -0700

    test_studyPt_genCurveDat: A case where seeing much looping back to origin.

--- postproc/test_studyPt_genCurveDat.m
+++ postproc/test_studyPt_genCurveDat.m
@@ -1,3 +1,4 @@
+
 clear;
 commondefs;
 thisFile = "test_studyPt_genCurveDat";
@@ -9,14 +10,17 @@ sizeX = 100;
 sizeF = 100;
 %
 %seedPrm = demoFunc0101_genSeedPrm("lin-easy");
-seedPrm = demoFunc0101_genSeedPrm("moderate");
 %seedPrm = demoFunc0101_genSeedPrm("easy");
+%seedPrm = demoFunc0101_genSeedPrm("moderate");
+seedPrm = demoFunc0101_genSeedPrm("xhard");
 %randState = mod(round(time),1E6);
 %randState = 0;
 %randState = 677832;
 %randState = 952523; % Try with 5x100x100xSTEPTYPE__GRADDIR.
 %randState = 88; % Try with 100x100x100x"moderate" STEPTYPE__LEVCURVE_SCALED.
-randState = 953150; % Try with 100x100x100x"moderate" STEPTYPE__LEVCURVE_SCALED.
+%randState = 953150; % Try with 100x100x100x"moderate" STEPTYPE__LEVCURVE_SCALED.
+%randState = 955286;
+randState = 955618;
 echo__randStat = randState
 seedPrm.randState = randState;
 seedPrm.sizeX = sizeX;
@@ -38,7 +42,7 @@ vecXSecret = funcPrm.x0;
 %
 prm = [];
 [ curveDat, retCode, datOut ] = studyPt_genCurveDat( ...
-  funchF, vecX0, matV, matW, matH, vecG, STEPTYPE__LEVCURVE_SCALED, prm );
+  funchF, vecX0, matV, matW, matH, vecG, STEPTYPE__LEVCURVE, prm );
 indexOfMin = curveDat.indexOfMin
 minResultIsIncluded = curveDat.minResultIsIncluded
 numNuVals = size(curveDat.rvecNu,2);
@@ -85,7 +89,7 @@ viz_matTemp = curveDat.matY - (viz_vecV*viz_rvecX1);
 viz_rvecX2 = sqrt(sum(viz_matTemp.^2,1));
 plot( ...
   viz_rvecX1, viz_rvecX2, 'o-', ...
-  [0], [0], '.', ...
+  [0], [0], 'k+', 'linewidth', 3, 'markersize', 20, ...
   viz_rvecX1(indexOfMin), viz_rvecX2(indexOfMin), 's', 'linewidth', 3, 'markersize', 10 );
 %
 grid on;
@@ -93,12 +97,25 @@ grid on;
 %
 viz_matV = eye(sizeK,sizeK);
 viz_matY = viz_matV' * curveDat.matY;
+
+for n=1:floor(sizeK/2)
+numFigs++; figure(numFigs);
+i1 = (2*n)-1; i2 = 2*n;
+plot( ...
+  viz_matY(i1,:), viz_matY(i2,:), 'o-', ...
+  [0], [0], 'k+', 'linewidth', 3, 'markersize', 20, ...
+  viz_matY(i1,indexOfMin), viz_matY(i2,indexOfMin), 's', 'linewidth', 3, 'markersize', 10 );
+grid on;
+end
+return
+
+
 %
 numFigs++; figure(numFigs);
 i1 = 1; i2 = 2;
 plot( ...
   viz_matY(i1,:), viz_matY(i2,:), 'o-', ...
-  [0], [0], '.', ...
+  [0], [0], 'k+', 'linewidth', 3, 'markersize', 20, ...
   viz_matY(i1,indexOfMin), viz_matY(i2,indexOfMin), 's', 'linewidth', 3, 'markersize', 10 );
 grid on;
 %
@@ -106,6 +123,14 @@ numFigs++; figure(numFigs);
 i1 = 3; i2 = 4;
 plot( ...
   viz_matY(i1,:), viz_matY(i2,:), 'o-', ...
-  [0], [0], '.', ...
+  [0], [0], 'k+', 'linewidth', 3, 'markersize', 20, ...
+  viz_matY(i1,indexOfMin), viz_matY(i2,indexOfMin), 's', 'linewidth', 3, 'markersize', 10 );
+grid on;
+%
+numFigs++; figure(numFigs);
+i1 = 1; i2 = 4;
+plot( ...
+  viz_matY(i1,:), viz_matY(i2,:), 'o-', ...
+  [0], [0], 'k+', 'linewidth', 3, 'markersize', 20, ...
   viz_matY(i1,indexOfMin), viz_matY(i2,indexOfMin), 's', 'linewidth', 3, 'markersize', 10 );
 grid on;
