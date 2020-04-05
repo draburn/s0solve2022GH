aeecff4f8a0083c624d3b0e6f2049e12da48f73b
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2020-04-05 00:29:20 -0700

    snaplinspace: The "snap values are 'too' close" case is hopefully working.

--- myutil/snaplinspace.m
+++ myutil/snaplinspace.m
@@ -1,6 +1,6 @@
-function  fVals = snaplinspace( fMin_approx, fMax_approx, numVal_approx, fSnapA, fSnapB )
-	assert(isposintscalar(numVal_approx));
-	assert(numVal_approx>=2);
+function  fVals = snaplinspace( fMin_approx, fMax_approx, numVals_approx, fSnapA, fSnapB )
+	assert(isposintscalar(numVals_approx));
+	assert(numVals_approx>=2);
 	assert(isrealscalar(fMin_approx));
 	assert(isrealscalar(fMax_approx));
 	assert(isrealscalar(fSnapA));
@@ -9,15 +9,22 @@ function  fVals = snaplinspace( fMin_approx, fMax_approx, numVal_approx, fSnapA,
 	assert( fSnapA < fSnapB );
 	assert( fSnapB <= fMax_approx );
 	%
-	delta_approx = (fMax_approx-fMin_approx)/(numVal_approx);
+	delta_approx = (fMax_approx-fMin_approx)/(numVals_approx-1.0);
 	nA_approx = (fSnapA-fMin_approx)/delta_approx;
 	nB_approx = (fSnapB-fMin_approx)/delta_approx;
 	%
-	% To-Do: Implement case(s) where this assertion fails.
-	assert( nB_approx - nA_approx >= 1.0 );
+	%assert( nB_approx - nA_approx >= 1.0 );
+	deltaN_approx = nB_approx - nA_approx;
+	if ( deltaN_approx < 0.5 )
+		f0 = (fSnapA+fSnapB)/2.0;
+		fSnapA = f0 - (0.5*delta_approx);
+		fSnapB = f0 + (0.5*delta_approx);
+		deltaN_approx = 1;
+	end
 	%
+	deltaN = round(deltaN_approx);
 	nA = round(nA_approx);
-	nB = round(nB_approx);
+	nB = nA + deltaN;
 	assert( nB - nA >= 1 );
 	delta = (fSnapB-fSnapA)/(nB-nA);
 	numBelowA = ceil( (fSnapA-fMin_approx)/delta );
@@ -34,7 +41,7 @@ end
 %!test
 %!	fMin_approx = 0.0;
 %!	fMax_approx = 1.0;
-%!	numVals_approx = 10;
+%!	numVals_approx = 5;
 %!	fSnapA = 0.1;
 %!	fSnapB = 1.0/sqrt(2);
 %!	fVals = snaplinspace( ...
@@ -49,3 +56,24 @@ end
 %!	assert( resA < sqrt(eps) );
 %!	assert( resB < sqrt(eps) );
 %!	assert( resX > sqrt(eps) );
+%!	assert( fVals(1) <= fMin_approx );
+%!	assert( fVals(end) >= fMax_approx );
+
+%!test
+%!	fMin_approx = 0.0;
+%!	fMax_approx = 1.0;
+%!	numVals_approx = 5;
+%!	fSnapA = 0.1;
+%!	fSnapB = 1.0/sqrt(2);
+%!	fSnapB *= 0.2;
+%!	fVals = snaplinspace( ...
+%!	  fMin_approx, ...
+%!	  fMax_approx, ...
+%!	  numVals_approx, ...
+%!	  fSnapA, ...
+%!	  fSnapB );
+%!	resA = min(abs(fVals-fSnapA));
+%!	resB = min(abs(fVals-fSnapB));
+%!	assert( abs(resA-resB) <= eps );
+%!	assert( fVals(1) <= fMin_approx );
+%!	assert( fVals(end) >= fMax_approx );
