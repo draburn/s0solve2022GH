b59a4ecc620c6644bf0f61b0b987806b0372883e
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2020-04-29 15:38:47 -0700

    Added mytuil/setprngstates.m.

--- myutil/myclear.m
+++ myutil/myclear.m
@@ -1,8 +1,5 @@
 clear;
 tic();
 %
-randState = 0;
-%randState = mod(round(time),1E6);
-msg("myclear",__LINE__,["randState = " num2str(randState) "."]);
-rand("state",randState);
-randn("state",randState);
+figIndex = 0;
+setprngstates();
---
+++ myutil/setprngstates.m
@@ -0,0 +1,14 @@
+function newState = setprngstates( newState=[], printState=true )
+	thisFile = "setprngstates";
+	if (isempty(newState))
+		newState = mod( round(now*1E11), 1E8 );
+	end
+	if (printState)
+		msg(thisFile,__LINE__,["Setting pRNG states to " num2str(newState) "."]);
+	end
+	rand("state",newState);
+	randn("state",newState);
+	rande("state",newState);
+	randp("state",newState);
+	randg("state",newState);
+end
