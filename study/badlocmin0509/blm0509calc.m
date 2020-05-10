d14ea95c1ae21eff23cc24190ccc639528cfe2c8
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2020-05-09 17:14:02 -0700

    blm0509calc: switched to gradient, seeing expected zero.

--- study/badlocmin0509/blm0509calc.m
+++ study/badlocmin0509/blm0509calc.m
@@ -4,17 +4,21 @@ thisFile = "blm0509calc";
 fdjaco_prm.epsFD = 1E-6;
 fdjaco_prm.fdOrder = 2;
 funchJ = @(x)( fdjaco( funchF, x, fdjaco_prm ) );
-vecXStart = [-0.49;0.01];
+%vecXStart = [-0.49;0.01];
+%vecXStart = [-0.48;-0.45];
+vecXStart = [-0.5008417558849074;-0.0424983458406762];
 %
 vecX = vecXStart;
 for n=1:50
 	vecF = funchF(vecX);
 	matJ = funchJ(vecX);
-	if ( rcond(matJ'*matJ) < 1e-16 )
-		break;
-	end
-	[matU,matS,matV] = svd(matJ);
-	vecDeltaTemp = -(matJ'*matJ)\(matJ'*vecF);
+	vecG = matJ'*vecF;
+	%if ( rcond(matJ'*matJ) < 1e-16 )
+	%	break;
+	%end
+	%[matU,matS,matV] = svd(matJ);
+	%vecDeltaTemp = -(matJ'*matJ)\(matJ'*vecF);
+	vecDeltaTemp = -vecG;
 	m = 0;
 	while (1)
 		vecXTemp = vecX+vecDeltaTemp;
@@ -34,16 +38,29 @@ for n=1:50
 	end
 	vecX += vecDelta;
 	msg( thisFile, __LINE__, sprintf( ...
-	 "  %2d;   %10.3e,   %10.3e;   %10.3e,   %10.3e", ...
-	 n, norm(vecF), norm(vecDelta),
-	 matS(1,1), matS(2,2) ));
+	 "  %2d;   %10.3e,   %10.3e,   %10.3e", ...
+	 n, norm(vecF), norm(vecG), norm(vecDelta) ));
 end
 %
 vecXM = vecX
 vecFM = vecF
+vecGM = vecG
 matJM = matJ
 [matUM,matSM,matVM] = svd(matJM);
 %%[matUM,matSM,matVM] = svd(matJM'*matJM);
 
-matUM(:,1)' * vecFM
-% THIS WAS SUPPOSED TO BE NEAR ZERO!
+norm(matUM(:,1)' * vecFM)/norm(vecFM)
+% THIS SHOULD BE VERY SMALL.
+
+if (0)
+	rvecLambda = linspace(-10.0,10.0,1001);
+	%rvecLambda = linspace(-1.0,1.0,10001);
+	%rvecLambda = linspace(-0.475,-0.473,100000);
+	matX = matVM(:,2)*rvecLambda + repmat(vecXM,size(rvecLambda));
+	matF = funchF(matX);
+	semilogy(rvecLambda,sqrt(sum(matF.^2,1)),'o-');
+	%semilogy(rvecLambda,sqrt(sum((matUM(:,2)' * matF).^2,1)),'o-');
+	grid on;
+	% THIS WILL ONLY SHOW A STEEP DIP IF THE
+	% "ROOT-ISH CURVE" IS A LINE.
+end
--- study/badlocmin0509/blm0509viz.m
+++ study/badlocmin0509/blm0509viz.m
@@ -5,16 +5,15 @@ numX1Vals = 201;
 numX2Vals = 201;
 x1Vals = linspace(-2,2,numX1Vals);
 x2Vals = linspace(-2,2,numX2Vals);
-%x1Vals = linspace(-0.9,0.1,numX1Vals);
-%x2Vals = linspace(-0.1,0.7,numX2Vals);
-%x1Vals = linspace(-0.55,0.05,numX1Vals);
-%x2Vals = linspace(-0.05,0.35,numX2Vals);
-%x1Vals = linspace(-0.8,0.1,numX1Vals);
-%x2Vals = linspace(-0.1,0.3,numX2Vals);
 x1Vals = linspace(-0.6,0.05,numX1Vals);
 x2Vals = linspace(-0.02,0.05,numX2Vals);
 %x1Vals = linspace(-0.5,-0.45,numX1Vals);
 %x2Vals = linspace(0.006,0.014,numX2Vals);
+%
+x1Vals = linspace(-0.7,0.02,numX1Vals);
+x2Vals = linspace(-0.1,0.02,numX2Vals);
+x1Vals = linspace(-0.6,-0.4,numX1Vals);
+x2Vals = linspace(-0.06,-0.02,numX2Vals);
 %
 [ gridX1, gridX2 ] = ndgrid( x1Vals, x2Vals );
 matX = [ ...
@@ -26,8 +25,8 @@ gridF2 = reshape( matF(2,:), [numX1Vals,numX2Vals] );
 gridOmega = 0.5*(gridF1.^2+gridF2.^2);
 %
 %funchViz = @(f)( sign(f).*( abs(f).^(1.0/2.0) ) );
-funchViz = @(f)( ( abs(f).^(1.0/2.0) ) );
-%funchViz = @(f)( abs(asinh(f*100.0)/100.0) );
+%funchViz = @(f)( ( abs(f).^(1.0/2.0) ) );
+funchViz = @(f)( abs(asinh(f*100.0)/100.0) );
 %funchViz = @(f)( abs(asinh(f*1000.0)/1000.0) );
 %funchViz = @(f)( abs(asinh(f*10000.0)/10000.0) );
 cmap = colormap(jet(1000));
