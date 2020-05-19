968fc796cd023aeec9adbe6e047fd96eae1b7874
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2020-05-18 17:35:34 -0700

    demofunc0518 near-complete.

--- study/demofunc0518/demofunc0518_eval.m
+++ study/demofunc0518/demofunc0518_eval.m
@@ -11,6 +11,71 @@ function matF = demofunc0518_eval( matX, funcPrm )
 	end
 	%
 	% Other numerator terms.
+	if (1)
+	%
+	%
+	if ( 2 == funcPrm.polyOrder )
+		for m=1:sizeF
+		for n1=1:sizeX
+		for n2=1:n1
+			matF(m,:) += funcPrm.c2(m,n1,n2)*(matX(n1,:).*matX(n2,:));
+		end
+		end
+		end
+	elseif ( 3 == funcPrm.polyOrder )
+		for m=1:sizeF
+		for n1=1:sizeX
+		for n2=1:n1
+			temp0 = repmat( funcPrm.c2(m,n1,n2), [1,numVals] );
+			for n3=1:n2
+				temp0 += funcPrm.c3(m,n1,n2,n3)*matX(n3,:);
+			end
+			matF(m,:) += temp0.*matX(n1,:).*matX(n2,:);
+		end
+		end
+		end
+	elseif ( 4 == funcPrm.polyOrder )
+		for m=1:sizeF
+		for n1=1:sizeX
+		for n2=1:n1
+			temp0 = repmat( funcPrm.c2(m,n1,n2), [1,numVals] );
+			for n3=1:n2
+				temp1 = repmat( funcPrm.c3(m,n1,n2,n3), [1,numVals] );
+				for n4=1:n3
+					temp1 += funcPrm.c4(m,n1,n2,n3,n4)*matX(n4,:);
+				end
+				temp0 += temp1 .* matX(n3,:);
+			end
+			matF(m,:) += temp0.*matX(n1,:).*matX(n2,:);
+		end
+		end
+		end
+	elseif ( 5 == funcPrm.polyOrder )
+		for m=1:sizeF
+		for n1=1:sizeX
+		for n2=1:n1
+			temp0 = repmat( funcPrm.c2(m,n1,n2), [1,numVals] );
+			for n3=1:n2
+				temp1 = repmat( funcPrm.c3(m,n1,n2,n3), [1,numVals] );
+				for n4=1:n3
+					temp2 = repmat( funcPrm.c4(m,n1,n2,n3,n4), [1,numVals] );
+					for n5=1:n4
+						temp2 += funcPrm.c5(m,n1,n2,n3,n4,n5)*matX(n5,:);
+					end
+					temp1 += temp2 .* matX(n4,:);
+				end
+				temp0 += temp1 .* matX(n3,:);
+			end
+			matF(m,:) += temp0.*matX(n1,:).*matX(n2,:);
+		end
+		end
+		end
+	end
+	%
+	%
+	else
+	%
+	%
 	if ( 2 <= funcPrm.polyOrder )
 	for m=1:sizeF
 	for n1=1:sizeX
@@ -30,6 +95,37 @@ function matF = demofunc0518_eval( matX, funcPrm )
 	end
 	end
 	end
+	end
+	if ( 4 <= funcPrm.polyOrder )
+	for m=1:sizeF
+	for n1=1:sizeX
+	for n2=1:n1
+	for n3=1:n2
+	for n4=1:n3
+		matF(m,:) += funcPrm.c4(m,n1,n2,n3,n4)*(matX(n1,:).*matX(n2,:).*matX(n3,:).*matX(n4,:));
+	end
+	end
+	end
+	end
+	end
+	end
+	if ( 5 <= funcPrm.polyOrder )
+	for m=1:sizeF
+	for n1=1:sizeX
+	for n2=1:n1
+	for n3=1:n2
+	for n4=1:n3
+	for n5=1:n4
+		matF(m,:) += funcPrm.c5(m,n1,n2,n3,n4,n5)*(matX(n1,:).*matX(n2,:).*matX(n3,:).*matX(n4,:).*matX(n5,:));
+	end
+	end
+	end
+	end
+	end
+	end
+	end
+	%
+	%
 	end
 	%
 	% Numerator exp terms.
--- study/demofunc0518/test_demofunc0518.m
+++ study/demofunc0518/test_demofunc0518.m
@@ -3,9 +3,11 @@ sizeX = 2;
 sizeF = 2;
 funcPrm.vecF0 = zeros(sizeF,1);
 funcPrm.matJ0 = eye(sizeF,sizeX);
-funcPrm.polyOrder = 3;
+funcPrm.polyOrder = 5;
 funcPrm.c2 = randn(sizeF,sizeX,sizeX);
 funcPrm.c3 = randn(sizeF,sizeX,sizeX,sizeX);
+funcPrm.c4 = randn(sizeF,sizeX,sizeX,sizeX,sizeX);
+funcPrm.c5 = randn(sizeF,sizeX,sizeX,sizeX,sizeX,sizeX);
 %
 funcPrm.expPrm.numTerms = 0;
 for n=1:0
@@ -32,16 +34,17 @@ vecXRoot = [0;0];
 vecFPreRoot = demofunc0518_eval(vecXRoot,funcPrm);
 %
 funchF = @(x)( demofunc0518_eval(x,funcPrm) - repmat(vecFPreRoot,[1,size(x,2)]) );
+funchF([1;1])
 %
 funchF1XY = @(x,y)( funchF([x;y])(1,:) );
 funchF2XY = @(x,y)( funchF([x;y])(2,:) );
 funchFNXY = @(x,y)( sqrt(sum(funchF([x;y]).^2,1)) );
 %
-vizScl = 10.0;
+vizScl = 1.0;
 vizFunch = @(x)(asinh(x));
 funchVizXY = @(x,y)( vizFunch(vizScl*funchFNXY(x,y))/vizScl );
 %contourfunch( funchVizXY, [1.725,1.727,-0.785,-0.783] )
-contourfunch( funchVizXY, 1*[-1,1,-1,1] )
+contourfunch( funchVizXY, 3*[-1,1,-1,1] );
 cMap = 0.6 +0.4*jet;
 cMap(1,:) *= 0.4;
 cMap(end,:) *= 0.4;
