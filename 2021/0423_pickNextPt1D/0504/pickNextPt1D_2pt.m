348caf6c3feeac7fee2fe2bb89bbf1eb20676704
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-05-07 03:59:06 -0700

    pickNextPt1D_2pt.m: improved excecption messages.

--- 2021/0423_pickNextPt1D/0504/pickNextPt1D_2pt.m
+++ 2021/0423_pickNextPt1D/0504/pickNextPt1D_2pt.m
@@ -3,34 +3,35 @@ function xNext = pickNextPt1D_2pt( xVals, fVals, prm = [] )
 	thisFile = "pickNextPoint1D_2pt.m";
 	%
 	% Check data types...
-	assert(isrealarray(xVals));
-	assert(2==size(size(xVals),2));
-	assert(1==size(xVals,1));
-	assert(isrealarray(fVals));
-	assert(2==size(size(fVals),2));
-	assert(1==size(fVals,1));
-	assert(size(xVals,2)==size(fVals,2));
 	numPts = size(xVals,2);
+	assert( isrealarray(xVals,[1,numPts]) );
+	assert( isrealarray(fVals,[1,numPts]) );
 	%
 	% Check requirements...
-	assert(2==numPts);
+	assert( 2 == numPts );
 	%
 	% Check unsupported cases...
-	assert(0==sum(0.0>=diff(xVals))); % Require xVals strictly increasing.
+	xValsAreStrictlyIncreasing =( 0==sum(0.0>=diff(xVals)) );
+	assert(xValsAreStrictlyIncreasing);
+	%
 	avgF = sum(fVals)/numPts;
 	avgFSq = sum(fVals.^2)/numPts;
 	varSqF = avgFSq - (avgF^2);
 	epsF = mygetfield(prm,"epsF",eps^0.5);
-	assert(isrealarray(epsF,[1,1]));
+	assert( isrealarray(epsF,[1,1]) );
 	assert( 0.0 < epsF );
-	assert(	varSqF > (epsF^2)*avgFSq ); % Require fVals not all same.
-	assert(0==sum(0.0==fVals)); % Require fVals all non-zero.
-	assert(0==sum(0.0>fVals(1)*fVals)); % Require all fVals have same sign.
+	fValRangeIsSufficient =( varSqF > (epsF^2)*avgFSq );
+	assert( fValRangeIsSufficient );
+	%
+	signF = sign(fVals(1));
+	gVals = signF * fVals;
+	fValsAllHaveSameSign =( 0 == sum(0.0>=gVals) );
+	assert( fValsAllHaveSameSign );
 	%
 	% Do work.
 	% Simple linear extrapolation.
 	xNext = ( xVals(1)*fVals(2) - xVals(2)*fVals(1) ) / (fVals(2)-fVals(1));
-	assert(isrealarray(xNext,[1,1]));
+	assert( isrealarray(xNext,[1,1]) );
 return;
 end
 
