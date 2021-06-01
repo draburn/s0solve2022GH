bc46e2520bd26d57002bab5bc483145e08369d83
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-06-01 01:02:01 -0700

    extFit: started revised version.

---
+++ extFit/extFit__getLocalGradHess_rhoLin.m
@@ -0,0 +1,72 @@
+function datOut = extFit__getLocalGradHess_rhoLin( ...
+  bigX, bigP, xVals, fVals, wVals = [], prm = [] )
+	thisFile = "extFit__getLocalGradHess_rhoLin";
+	if (isempty(wVals))
+		wVals = ones(size(xVals));
+	end
+	%
+	epsX = mygetfield(  prm,  "epsX",  min([ ...
+	  sqrt(eps)*(max(xVals)-min(xVals)), ...
+	  sqrt(sqrt(eps))*min(abs(diff(xVals))) ])  );
+	epsP = mygetfield( prm, "epsP", sqrt(eps) );
+	%
+	rhoVals0  = extFit__getRhoVals( bigX, bigP, xVals, fVals, wVals );
+	rhoValsPX = extFit__getRhoVals( bigX+epsX, bigP, xVals, fVals, wVals );
+	rhoValsMX = extFit__getRhoVals( bigX-epsX, bigP, xVals, fVals, wVals );
+	rhoValsPP = extFit__getRhoVals( bigX, bigP+epsP, xVals, fVals, wVals );
+	rhoValsMP = extFit__getRhoVals( bigX, bigP-epsP, xVals, fVals, wVals );
+	%
+	rhoDXVals = ( rhoValsPX - rhoValsMX ) / ( 2.0 * epsX );
+	rhoDPVals = ( rhoValsPP - rhoValsMP ) / ( 2.0 * epsP );
+	%
+	sumRXSq = sum( wVals .* rhoDXVals .* rhoDXVals );
+	sumRPSq = sum( wVals .* rhoDPVals .* rhoDPVals );
+	sumRXRP = sum( wVals .* rhoDPVals .* rhoDXVals );
+	sumRXF  = sum( wVals .* rhoDXVals .* rhoVals0 );
+	sumRPF  = sum( wVals .* rhoDPVals .* rhoVals0 );
+	%
+	vecG = -[ sumRXF; sumRPF ];
+	matH = [ sumRXSq, sumRXRP; sumRXRP, sumRPSq ];
+	%
+	% Copy main stuff to datOut...
+	datOut.vecG = vecG;
+	datOut.matH = matH;
+	%
+	% Copy other stuff to datOut...
+	datOut.bigX = bigX;
+	datOut.bigP = bigP;
+	datOut.xVals = xVals;
+	datOut.fVals = fVals;
+	datOut.wVals = wVals;
+	datOut.prm = prm;
+	datOut.epsX = epsX;
+	datOut.epsP = epsP;
+	datOut.rhoVals0 = rhoVals0;
+	datOut.rhoValsPX = rhoValsPX;
+	datOut.rhoValsMX = rhoValsMX;
+	datOut.rhoValsPP = rhoValsPP;
+	datOut.rhoValsMP = rhoValsMP;
+	datOut.rhoDXVals = rhoDXVals;
+	datOut.rhoDPVals = rhoDPVals;
+	datOut.sumRXSq = sumRXSq
+	datOut.sumRPSq = sumRPSq
+	datOut.sumRXRP = sumRXRP
+	datOut.sumRXF  = sumRXF
+	datOut.sumRPF  = sumRPF
+return;
+end
+
+%!test
+%!	thisFile = "test extFit__getLocalGradHess_rhoLin"
+%!	bigA = 1.0;
+%!	bigB = 1.0;
+%!	bigX = 0.5;
+%!	bigP = 2.0;
+%!	funchF = @(x)( bigA + bigB * abs(x-bigX).^bigP );
+%!	xVals = linspace( -2.0, 3.0, 6 );
+%!	fVals = funchF(xVals);
+%!	bigX_guess = bigX + 0.1;
+%!	bigP_guess = bigP + 0.1;
+%!	datOut = extFit__getLocalGradHess_rhoLin( bigX_guess, bigP_guess, xVals, fVals )
+%!	1.0 - (datOut.matH(1,2)^2)/(datOut.matH(1,1)*datOut.matH(2,2))
+%!	msg( thisFile, __LINE__, "CHECK ABOVE VALUES!" );
---
+++ extFit/extFit__getRhoVals.m
@@ -0,0 +1,32 @@
+function rhoVals = extFit__getRhoVals( bigX, bigP, xVals, fVals, wVals = [] )
+	thisFile = "extFit__getRhoVals";
+	if (isempty(wVals))
+		wVals = ones(size(xVals));
+	end
+	dVals = abs( xVals - bigX ).^bigP;
+	sigma1 = sum(wVals);
+	sigmaD = sum(wVals.*dVals);
+	sigmaDD = sum(wVals.*dVals.*dVals);
+	sigmaF = sum(wVals.*fVals);
+	sigmaFD = sum(wVals.*dVals.*fVals);
+	vecC = [ sigma1, sigmaD; sigmaD, sigmaDD ] \ [ sigmaF; sigmaFD ];
+	rhoVals = vecC(1) + (vecC(2) * dVals) - fVals;
+return;
+end
+
+%!test
+%!	thisFile = "test extFit__getRhoVals"
+%!	setprngstates();
+%!	bigA = randn();
+%!	bigB = randn();
+%!	bigX = randn()*exp(3*randn());
+%!	bigP = 0.3 + abs(randn());
+%!	numPts = 5 + round(abs(randn()*exp(3*randn())));
+%!	funchF = @(x)( bigA + bigB * abs(x-bigX).^bigP );
+%!	xVals = randn(1,numPts).*exp(randn()) + bigX;
+%!	fVals_before = funchF(xVals)
+%!	noiseLevel = 0%0.01*abs(randn())*sum(abs(fVals_before))/numPts;
+%!	noiseVals = noiseLevel*randn(1,numPts)
+%!	fVals = fVals_before + noiseVals;
+%!	rVals = extFit__getRhoVals( bigX, bigP, xVals, fVals )
+%!	msg( thisFile, __LINE__, "ARE THESE VALUES FOR REASONABLE?" );
