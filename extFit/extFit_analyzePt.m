8b569c639cd5ace8f88c5a497a26190c1c22cb29
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-06-03 04:17:48 -0700

    extFit_analyzePt: added checks; slow-down is insignificant.

--- extFit/extFit_analyzePt.m
+++ extFit/extFit_analyzePt.m
@@ -16,6 +16,14 @@ function datOut = extFit_analyzePt( x0, p, rvecX, rvecF, rvecW=[] )
 		rvecW = ones(size(rvecX));
 	end
 	%
+	% These checks cause slow-down, but, it's still fast enough.
+	numPts = size(rvecX,2);
+	assert( isrealscalar(x0) );
+	assert( isrealscalar(p) );
+	assert( isrealarray(rvecX,[1,numPts]) );
+	assert( isrealarray(rvecF,[1,numPts]) );
+	assert( isrealarray(rvecW,[1,numPts]) );
+	%
 	rvecD = abs(rvecX-x0).^p;
 	rvecWD = rvecW.*rvecD;
 	sigma1 = sum(rvecW);
@@ -25,8 +33,8 @@ function datOut = extFit_analyzePt( x0, p, rvecX, rvecF, rvecW=[] )
 	sigmaDF = sum(rvecWD.*rvecF);
 	%
 	denom = sigma1 * sigmaDD - (sigmaD.*sigmaD);
-	bigA = ( sigmaDD * sigmaF - sigmaD * sigmaDF ) ./ denom;
-	bigB = ( sigma1 * sigmaDF - sigmaD * sigmaF ) ./ denom;
+	bigA = ( sigmaDD * sigmaF  - sigmaD * sigmaDF ) ./ denom;
+	bigB = ( sigma1  * sigmaDF - sigmaD * sigmaF  ) ./ denom;
 	rvecRho = bigA + bigB * rvecD - rvecF;
 	omega = 0.5 * sum( rvecW .* rvecRho .* rvecRho ) / sigma1;
 	%
@@ -66,7 +74,7 @@ end
 %!test
 %!	thisFile = "test extFit_analyzePt 1";
 %!	setprngstates();
-%!	numFigs = 2;
+%!	numFigs = 0;
 %!	%
 %!	sizeY = 2;
 %!	sizeP = 2;
@@ -75,7 +83,7 @@ end
 %!	funch_f = @(x)( randn + randn*( abs(x-randn).^(1.0+abs(randn)) ) );
 %!	%
 %!	[ y_mat, p_mat ] = meshgrid( y_vec, p_vec );
-%!	x_rvec = randn(1,5000);
+%!	x_rvec = randn(1,50000);
 %!	f_rvec = funch_f( x_rvec );
 %!	%
 %!	msg( thisFile, __LINE__, "Doing new calc..." );
@@ -120,7 +128,7 @@ end
 %!test
 %!	thisFile = "test extFit_analyzePt 2";
 %!	setprngstates(0);
-%!	numFigs = 0;
+%!	numFigs = 2;
 %!	%
 %!	sizeY = 201;
 %!	sizeP = 101;
