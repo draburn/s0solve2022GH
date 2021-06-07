263adf28605d01c00fe366583737cf4701d73852
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-06-07 00:35:33 -0700

    extFit: Found a few tricky cases.

--- extFit/test_extFit.m
+++ extFit/test_extFit.m
@@ -1,10 +1,14 @@
 	clear;
 	thisFile = "test_extFit";
 	numFigs = 0;
-	%setprngstates(19719664); % eye(2,2) may be better here.
-	setprngstates(77173824); % Better initial guess would help; goes negative in P.
-	%setprngstates(80489888);
 	%setprngstates();
+	%
+	%setprngstates(40286208); % Yikes!
+	setprngstates(77173824); % Breaks.
+	%
+	% DRaburn 2021.06.07: cases below may be boring.
+	%setprngstates(19719664); % eye(2,2) may be better here.
+	%setprngstates(80489888);
 	%setprngstates(10743664); % Image plots glitched. Turn off axis(suqare), "a", zoom out.
 	%setprngstates(77077008);
 	%setprngstates(5932672)
@@ -14,7 +18,6 @@
 	%setprngstates(26846592); % Massive slowdown. And, ?!?!?!?!
 	%setprngstates(98584480);
 	%setprngstates(40121600); % Here, H2 might be better.
-	%numPts = 5 + round(abs(randn()*exp(abs(3.0*randn()))))
 	numPts = 5 + round(abs(randn()*exp(abs(randn()))))
 	bigX_secret = randn()*exp(abs(3.0*randn()))
 	bigP_secret = 1.0 + 3.0*abs(randn())
@@ -68,9 +71,9 @@
 		rvecX = sort([ rvecX, bigX ]);
 		rvecF = funchF( rvecX );
 	end
-	msg( thisFile, __LINE__, sprintf( "bigX_secret = %g.", bigX_secret ) );
-	msg( thisFile, __LINE__, sprintf( "bigX = %g.", bigX ) );
-	msg( thisFile, __LINE__, sprintf( "res bigX = %g.", abs(bigX-bigX_secret) ) );
+	msg( thisFile, __LINE__, sprintf( "secret values = ( %g, %g ).", bigX_secret, bigP_secret ) );
+	msg( thisFile, __LINE__, sprintf( "algori values = ( %g, %g ).", bigX, bigP ) );
+	msg( thisFile, __LINE__, sprintf( "residu values = ( %g, %g ).", abs(bigX-bigX_secret), abs(bigP-bigP_secret) ) );
 	return;
 	
 	rvecX_first = linspace(min(rvecX),max(rvecX),101);
