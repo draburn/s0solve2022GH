9c858e660fd41f495636824296de71130fefb6f7
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-06-06 17:40:13 -0700

    extFit: several fixes to _viz.

--- extFit/extFit_viz.m
+++ extFit/extFit_viz.m
@@ -1,6 +1,7 @@
 %function datOut = extFit_viz( bigX, bigP, rvecX, rvecF, rvecW=[], prm=[] )
 	clear;
-	setprngstates(26846592); % Massive slowdown.
+	setprngstates(58467872);
+	%setprngstates(26846592); % Massive slowdown. And, ?!?!?!?!
 	%setprngstates(98584480);
 	%setprngstates(40121600); % Here, H2 might be better.
 	%numPts = 5 + round(abs(randn()*exp(abs(3.0*randn()))))
@@ -17,6 +18,9 @@
 	bigX0 = bigX_secret + randn()
 	bigP0 = 2.0
 	%
+	%
+	%
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	commondefs;
 	thisFile = "extFit_viz";
 	numFigs = 0;
@@ -33,120 +37,182 @@
 	%
 	%
 	%
-	epsX_default = ...
-	  min([ sqrt(eps)*(max(rvecX)-min(rvecX)), ...
-	   sqrt(sqrt(eps))*min(abs(diff(rvecX))) ]);
-	epsP_default = sqrt(eps);
-	epsX = mygetfield( prm, "epsX", epsX_default );
-	epsP = mygetfield( prm, "epsP", epsP_default );
+	prm_calcGradHess = mygetfield( prm, "prm_calcGradHess", [] );
+	dat_calcGradHess = extFit_calcGradHess( bigX0, bigP0, rvecX, rvecF, rvecW, prm_calcGradHess );
+	omega0 = dat_calcGradHess.omega0
+	vecG = dat_calcGradHess.vecG
+	matH1 = dat_calcGradHess.matH1
+	assert( 0==sum( diag(matH1)<=0.0 ) );
+	matH2 = dat_calcGradHess.matH2
+	matD0 = eye(2,2);
+	matD1 = diag(diag(matH1));
+	matD2 = diag(abs(diag(matH2)));
 	%
-	omegaDatAtM0 = extFit_calcOmega( bigX0-epsX, bigP0,      rvecX, rvecF, rvecW );
-	omegaDatAt0M = extFit_calcOmega( bigX0,      bigP0-epsP, rvecX, rvecF, rvecW );
-	omegaDatAt00 = extFit_calcOmega( bigX0,      bigP0,      rvecX, rvecF, rvecW );
-	omegaDatAt0P = extFit_calcOmega( bigX0,      bigP0+epsP, rvecX, rvecF, rvecW );
-	omegaDatAtP0 = extFit_calcOmega( bigX0+epsX, bigP0,      rvecX, rvecF, rvecW );
+	prm_findMuOfOmega = mygetfield( prm, "prm_findMuOfOmega", [] );
+	muMin_h1_d0 = 0.0
+	muMin_h1_d1 = 0.0
+	[ muMin_h2_d0, retCode_d0 ] = extFit_findMuOfOmega( 0.0, omega0, vecG, matH2, matD0, prm_findMuOfOmega )
+	[ muMin_h2_d1, retCode_d1 ] = extFit_findMuOfOmega( 0.0, omega0, vecG, matH2, matD1, prm_findMuOfOmega )
+	[ muMin_h2_d2, retCode_d2 ] = extFit_findMuOfOmega( 0.0, omega0, vecG, matH2, matD2, prm_findMuOfOmega )
+	% DRaburn 2021.06.06.
+	% We could reject results based on retCode.
+	% But, we probably don't want to ultimately use H2 anyway.
+	% So, this is just for viz.
 	%
-	omega0 = omegaDatAt00.omega;
-	rvecRhoAtM0 = omegaDatAtM0.rvecRho;
-	rvecRhoAt0M = omegaDatAt0M.rvecRho;
-	rvecRhoAt00 = omegaDatAt00.rvecRho;
-	rvecRhoAt0P = omegaDatAt0P.rvecRho;
-	rvecRhoAtP0 = omegaDatAtP0.rvecRho;
-	%
-	rvecRhoD0 = rvecRhoAt00;
-	rvecRhoDX = ( rvecRhoAtP0 - rvecRhoAtM0 ) / ( 2.0*epsX );
-	rvecRhoDP = ( rvecRhoAt0P - rvecRhoAt0M ) / ( 2.0*epsP );
-	%
-	sigma0X = sum( rvecW .* rvecRhoD0 .* rvecRhoDX );
-	sigma0P = sum( rvecW .* rvecRhoD0 .* rvecRhoDP );
-	sigmaXX = sum( rvecW .* rvecRhoDX .* rvecRhoDX );
-	sigmaPP = sum( rvecW .* rvecRhoDP .* rvecRhoDP );
-	sigmaXP = sum( rvecW .* rvecRhoDX .* rvecRhoDP );
-	%
-	vecG = [ sigma0X; sigma0P ];
-	matH = [ sigmaXX, sigmaXP; sigmaXP, sigmaPP ];
-	matD = diag(abs(diag(matH)));
-	matI = eye(2,2);
+	muCoeff_h1_d0 = norm( matD0 \ vecG ) / norm( (matH1+(muMin_h1_d0*matD0))\vecG );
+	muCoeff_h1_d1 = norm( matD1 \ vecG ) / norm( (matH1+(muMin_h1_d1*matD1))\vecG );
+	muCoeff_h2_d0 = norm( matD0 \ vecG ) / norm( (matH2+(muMin_h2_d0*matD0))\vecG );
+	muCoeff_h2_d1 = norm( matD1 \ vecG ) / norm( (matH2+(muMin_h2_d1*matD1))\vecG );
+	muCoeff_h2_d2 = norm( matD2 \ vecG ) / norm( (matH2+(muMin_h2_d2*matD2))\vecG );
 	%
 	%
+	numCurvePts = 101;
+	rvecLambda = linspace( 0.0, 1.0, numCurvePts ).^0.5;
+	rvecMu_h1_d0 = muMin_h1_d0 + muCoeff_h1_d0 * ( (1.0./rvecLambda) - 1.0 );
+	rvecMu_h1_d1 = muMin_h1_d1 + muCoeff_h1_d1 * ( (1.0./rvecLambda) - 1.0 );
+	rvecMu_h2_d0 = muMin_h2_d0 + muCoeff_h2_d0 * ( (1.0./rvecLambda) - 1.0 );
+	rvecMu_h2_d1 = muMin_h2_d1 + muCoeff_h2_d1 * ( (1.0./rvecLambda) - 1.0 );
+	rvecMu_h2_d2 = muMin_h2_d2 + muCoeff_h2_d2 * ( (1.0./rvecLambda) - 1.0 );
+	for n=1:numCurvePts
+		lambda = rvecLambda(n);
+		if ( lambda < sqrt(eps) )
+			matDelta_h1_d0(:,n) = -lambda * ( matD0 \ vecG ) / muCoeff_h1_d0;
+			matDelta_h1_d1(:,n) = -lambda * ( matD1 \ vecG ) / muCoeff_h1_d1;
+			matDelta_h2_d0(:,n) = -lambda * ( matD0 \ vecG ) / muCoeff_h2_d0;
+			matDelta_h2_d1(:,n) = -lambda * ( matD1 \ vecG ) / muCoeff_h2_d1;
+			matDelta_h2_d2(:,n) = -lambda * ( matD2 \ vecG ) / muCoeff_h2_d2;
+		else
+			matDelta_h1_d0(:,n) = -( matH1 + rvecMu_h1_d0(n)*matD0 ) \ vecG;
+			matDelta_h1_d1(:,n) = -( matH1 + rvecMu_h1_d1(n)*matD1 ) \ vecG;
+			matDelta_h2_d0(:,n) = -( matH2 + rvecMu_h2_d0(n)*matD0 ) \ vecG;
+			matDelta_h2_d1(:,n) = -( matH2 + rvecMu_h2_d1(n)*matD1 ) \ vecG;
+			matDelta_h2_d2(:,n) = -( matH2 + rvecMu_h2_d2(n)*matD2 ) \ vecG;
+		end
+		%
+		rvecDeltaNorm_h1_d0(n) = norm(matDelta_h1_d0(:,n));
+		rvecDeltaNorm_h1_d1(n) = norm(matDelta_h1_d1(:,n));
+		rvecDeltaNorm_h2_d0(n) = norm(matDelta_h2_d0(:,n));
+		rvecDeltaNorm_h2_d1(n) = norm(matDelta_h2_d1(:,n));
+		rvecDeltaNorm_h2_d2(n) = norm(matDelta_h2_d2(:,n));
+		%
+		rvecOmegaM1_h1_d0(n) = omega0 + vecG'*matDelta_h1_d0(:,n) + 0.5*matDelta_h1_d0(:,n)'*matH1*matDelta_h1_d0(:,n);
+		rvecOmegaM1_h1_d1(n) = omega0 + vecG'*matDelta_h1_d1(:,n) + 0.5*matDelta_h1_d1(:,n)'*matH1*matDelta_h1_d1(:,n);
+		rvecOmegaM1_h2_d0(n) = omega0 + vecG'*matDelta_h2_d0(:,n) + 0.5*matDelta_h2_d0(:,n)'*matH1*matDelta_h2_d0(:,n);
+		rvecOmegaM1_h2_d1(n) = omega0 + vecG'*matDelta_h2_d1(:,n) + 0.5*matDelta_h2_d1(:,n)'*matH1*matDelta_h2_d1(:,n);
+		rvecOmegaM1_h2_d2(n) = omega0 + vecG'*matDelta_h2_d2(:,n) + 0.5*matDelta_h2_d2(:,n)'*matH1*matDelta_h2_d2(:,n);
+		%
+		rvecOmegaM2_h1_d0(n) = omega0 + vecG'*matDelta_h1_d0(:,n) + 0.5*matDelta_h1_d0(:,n)'*matH2*matDelta_h1_d0(:,n);
+		rvecOmegaM2_h1_d1(n) = omega0 + vecG'*matDelta_h1_d1(:,n) + 0.5*matDelta_h1_d1(:,n)'*matH2*matDelta_h1_d1(:,n);
+		rvecOmegaM2_h2_d0(n) = omega0 + vecG'*matDelta_h2_d0(:,n) + 0.5*matDelta_h2_d0(:,n)'*matH2*matDelta_h2_d0(:,n);
+		rvecOmegaM2_h2_d1(n) = omega0 + vecG'*matDelta_h2_d1(:,n) + 0.5*matDelta_h2_d1(:,n)'*matH2*matDelta_h2_d1(:,n);
+		rvecOmegaM2_h2_d2(n) = omega0 + vecG'*matDelta_h2_d2(:,n) + 0.5*matDelta_h2_d2(:,n)'*matH2*matDelta_h2_d2(:,n);
+	end
+	dat_calcOmega_h1_d0 = extFit_calcOmega_mat( ...
+	  bigX0 + matDelta_h1_d0(1,:), ...
+	  bigP0 + matDelta_h1_d0(2,:), ...
+	  rvecX, rvecF, rvecX );
+	dat_calcOmega_h1_d1 = extFit_calcOmega_mat( ...
+	  bigX0 + matDelta_h1_d1(1,:), ...
+	  bigP0 + matDelta_h1_d1(2,:), ...
+	  rvecX, rvecF, rvecX );
+	dat_calcOmega_h2_d0 = extFit_calcOmega_mat( ...
+	  bigX0 + matDelta_h2_d0(1,:), ...
+	  bigP0 + matDelta_h2_d0(2,:), ...
+	  rvecX, rvecF, rvecX );
+	dat_calcOmega_h2_d1 = extFit_calcOmega_mat( ...
+	  bigX0 + matDelta_h2_d1(1,:), ...
+	  bigP0 + matDelta_h2_d1(2,:), ...
+	  rvecX, rvecF, rvecW );
+	dat_calcOmega_h2_d2 = extFit_calcOmega_mat( ...
+	  bigX0 + matDelta_h2_d2(1,:), ...
+	  bigP0 + matDelta_h2_d2(2,:), ...
+	  rvecX, rvecF, rvecX );
 	%
+	rvecOmegaAc_h1_d0 = dat_calcOmega_h1_d0.matOmega;
+	rvecOmegaAc_h1_d1 = dat_calcOmega_h1_d1.matOmega;
+	rvecOmegaAc_h2_d0 = dat_calcOmega_h2_d0.matOmega;
+	rvecOmegaAc_h2_d1 = dat_calcOmega_h2_d1.matOmega;
+	rvecOmegaAc_h2_d2 = dat_calcOmega_h2_d2.matOmega;
+	%
+	%
+	%
+	numFigs++; figure(numFigs);
+	plot( ...
+	  rvecLambda, rvecDeltaNorm_h1_d0, 'o-', ...
+	  rvecLambda, rvecDeltaNorm_h1_d1, 's-', ...
+	  rvecLambda, rvecDeltaNorm_h2_d0, '^-', ...
+	  rvecLambda, rvecDeltaNorm_h2_d1, 'v-', ...
+	  rvecLambda, rvecDeltaNorm_h2_d2, '*-' );
+	legend( ...
+	  "H1 D0", ...
+	  "H1 D1", ...
+	  "H2 D0", ...
+	  "H2 D1", ...
+	  "H2 D2", ...
+	  "location", "north" );
+	ylabel( "lambda" );
+	ylabel( "delta norm" );
+	title( "delta norm vs lambda" );
+	grid on;
+	%
+	numFigs++; figure(numFigs);
+	plot( ...
+	  rvecDeltaNorm_h1_d0, rvecOmegaM1_h1_d0, 'o-', ...
+	  rvecDeltaNorm_h1_d1, rvecOmegaM1_h1_d1, 's-', ...
+	  rvecDeltaNorm_h2_d0, rvecOmegaM1_h2_d0, '^-', ...
+	  rvecDeltaNorm_h2_d1, rvecOmegaM1_h2_d1, 'v-', ...
+	  rvecDeltaNorm_h2_d2, rvecOmegaM1_h2_d2, '*-' );
+	legend( ...
+	  "H1 D0", ...
+	  "H1 D1", ...
+	  "H2 D0", ...
+	  "H2 D1", ...
+	  "H2 D2", ...
+	  "location", "north" );
+	xlabel( "delta norm" );
+	ylabel( "omega model 1" );
+	title( "omega model 1 vs delta norm" );
+	grid on;
+	%
+	%
+	%
+	numColors = mygetfield( prm, "numColors", 1000 );
 	sizeBigX = 201;
 	sizeBigP = 203;
 	rvecBigX = sort( bigX0 + 2.0*(bigX_secret-bigX0)*linspace( -0.5, 1.5, sizeBigX ) );
 	rvecBigP = sort( bigP0 + 2.0*(bigP_secret-bigP0)*linspace( -0.5, 1.5, sizeBigP ) );
 	[ matBigX, matBigP ] = meshgrid( rvecBigX, rvecBigP );
-	%
 	dat_calcOmega = extFit_calcOmega_mat( matBigX, matBigP, rvecX, rvecF, rvecW );
+	matOmegaAc = dat_calcOmega.matOmega;
 	%
 	rvecDeltaX = rvecBigX - bigX0;
 	rvecDeltaP = rvecBigP - bigP0;
 	[ matDeltaX, matDeltaP ] = meshgrid( rvecDeltaX, rvecDeltaP );
+	matOmegaM1 = omega0 + matH1(2,1) * matDeltaX .* matDeltaP ....
+	 + matDeltaX .* ( vecG(1) + 0.5*matH1(1,1)*matDeltaX ) ...
+	 + matDeltaP .* ( vecG(2) + 0.5*matH1(2,2)*matDeltaP );
+	matOmegaM2 = omega0 + matH2(2,1) * matDeltaX .* matDeltaP ....
+	 + matDeltaX .* ( vecG(1) + 0.5*matH2(1,1)*matDeltaX ) ...
+	 + matDeltaP .* ( vecG(2) + 0.5*matH2(2,2)*matDeltaP );
 	%
-	matOmegaModel = omega0 + matH(2,1) * matDeltaX .* matDeltaP ....
-	 + matDeltaX .* ( vecG(1) + matH(1,1)*matDeltaX ) ...
-	 + matDeltaP .* ( vecG(2) + matH(2,2)*matDeltaP );
-	%
-	numCurvePts = 53;
-	rvecLambda = linspace( 0.0, 1.0, numCurvePts ).^16;
-	for n=1:numCurvePts
-		lambda = rvecLambda(n);
-		if ( lambda < sqrt(eps) )
-			matDelta_levenberg(:,n) = -lambda*( matI \ vecG );
-			matDelta_levMarq(:,n) = -lambda*( matD \ vecG );
-		else
-			mu = (1.0./lambda) - 1.0;
-			matDelta_levenberg(:,n) = -(matH+mu*matI)\vecG;
-			matDelta_levMarq(:,n) = -(matH+mu*matD)\vecG;
-		end
-		rvecDeltaNorm_levenberg(n) = norm( matDelta_levenberg(:,n) );
-		rvecDeltaNorm_levMarq(n) = norm( matDelta_levMarq(:,n) );
-	end
-	dat_levenberg = extFit_calcOmega_mat( ...
-	  bigX0 + matDelta_levenberg(1,:), ...
-	  bigP0 + matDelta_levenberg(2,:), ...
-	  rvecX, rvecF, rvecW );
-	rvecOmega_levenberg = dat_levenberg.matOmega;
-	dat_levMarq = extFit_calcOmega_mat( ...
-	  bigX0 + matDelta_levMarq(1,:), ...
-	  bigP0 + matDelta_levMarq(2,:), ...
-	  rvecX, rvecF, rvecW );
-	rvecOmega_levMarq = dat_levMarq.matOmega;
-	%
-	rvecOmegaModel_levMarq = omega0 ...
-	  + matH(2,1) * matDelta_levMarq(1,:) .* matDelta_levMarq(2,:) ....
-	  + matDelta_levMarq(1,:) .* ( vecG(1) + matH(1,1)*matDelta_levMarq(1,:) ) ...
-	  + matDelta_levMarq(2,:) .* ( vecG(2) + matH(2,2)*matDelta_levMarq(2,:) );
-	rvecOmegaModel_levenberg = omega0 ...
-	  + matH(2,1) * matDelta_levenberg(1,:) .* matDelta_levenberg(2,:) ....
-	  + matDelta_levenberg(1,:) .* ( vecG(1) + matH(1,1)*matDelta_levenberg(1,:) ) ...
-	  + matDelta_levenberg(2,:) .* ( vecG(2) + matH(2,2)*matDelta_levenberg(2,:) );
-	rvecOmegaModel_levMarq = omega0 ...
-	  + matH(2,1) * matDelta_levMarq(1,:) .* matDelta_levMarq(2,:) ....
-	  + matDelta_levMarq(1,:) .* ( vecG(1) + matH(1,1)*matDelta_levMarq(1,:) ) ...
-	  + matDelta_levMarq(2,:) .* ( vecG(2) + matH(2,2)*matDelta_levMarq(2,:) );
-	%
-	%
-	numColors = mygetfield( prm, "numColors", 1000 );
-	matOmega = dat_calcOmega.matOmega;
-	omegaMax = max(max(matOmega));
+	omegaMax = max(max(matOmegaAc));
 	omegaMin = 0.0;
-	%omegaMax = max(max(matOmegaModel));
-	%omegaMin = min(min(matOmegaModel));
+	%omegaMax = max(max(matOmegaM1));
+	%omegaMin = min(min(matOmegaM1));
+	funch_viz = @(om)( 1 + round((numColors-1)*cap( (om-omegaMin)/(omegaMax-omegaMin) ,0.0,1.0)) );
 	%
-	%funch_viz = @(om)( om );
-	funch_viz = @(om)( (abs(om)) );
+	%
 	%
 	numFigs++; figure(numFigs);
-	%contourf( rvecBigX, rvecBigP, matOmega );
-	image( rvecBigX, rvecBigP, numColors*funch_viz( (matOmega-omegaMin)/(omegaMax-omegaMin) ) );
+	image( rvecBigX, rvecBigP, funch_viz(matOmegaAc) );
 	axis("square");
 	set( get(gcf,"children"), "ydir", "normal" );
 	colormap(mycmap(numColors));
 	hold on;
-	plot( bigX_secret, bigP_secret, 'w*', 'markersize', 25, 'linewidth', 3 );
+	plot( bigX_secret, bigP_secret, 'w*', 'markersize', 25, 'linewidth', 2 );
 	plot( bigX0, bigP0, 'ws', 'markersize', 20, 'linewidth', 2 );
-	plot( matDelta_levenberg(1,:)+bigX0, matDelta_levenberg(2,:)+bigP0, 'w^-' );
-	plot( matDelta_levMarq(1,:)+bigX0, matDelta_levMarq(2,:)+bigP0, 'kv-' );
+	plot( bigX0+matDelta_h1_d0(1,:), bigP0+matDelta_h1_d0(2,:), 'ko-' );
+	plot( bigX0+matDelta_h2_d0(1,:), bigP0+matDelta_h2_d0(2,:), 'rv-' );
 	hold off;
 	grid on;
 	xlabel( "bigX" );
@@ -154,33 +220,35 @@
 	title( "omega vs bigX, bigP" );
 	%
 	numFigs++; figure(numFigs);
-	%contourf( rvecBigX, rvecBigP, matOmegaModel1 );
-	image( rvecBigX, rvecBigP, numColors*funch_viz( (matOmegaModel-omegaMin)/(omegaMax-omegaMin) ) );
+	image( rvecBigX, rvecBigP, funch_viz(matOmegaM1) );
 	axis("square");
 	set( get(gcf,"children"), "ydir", "normal" );
 	colormap(mycmap(numColors));
 	hold on;
-	plot( bigX_secret, bigP_secret, 'w*', 'markersize', 25, 'linewidth', 3 );
+	plot( bigX_secret, bigP_secret, 'w*', 'markersize', 25, 'linewidth', 2 );
 	plot( bigX0, bigP0, 'ws', 'markersize', 20, 'linewidth', 2 );
-	plot( matDelta_levenberg(1,:)+bigX0, matDelta_levenberg(2,:)+bigP0, 'w^-' );
-	plot( matDelta_levMarq(1,:)+bigX0, matDelta_levMarq(2,:)+bigP0, 'kv-' );
+	plot( bigX0+matDelta_h1_d0(1,:), bigP0+matDelta_h1_d0(2,:), 'ko-' );
+	plot( bigX0+matDelta_h2_d0(1,:), bigP0+matDelta_h2_d0(2,:), 'rv-' );
 	hold off;
 	grid on;
 	xlabel( "bigX" );
 	ylabel( "bigP" );
-	title( "omegaModel vs bigX, bigP" );
+	title( "omega model 1 vs bigX, bigP" );
 	%
 	numFigs++; figure(numFigs);
-	plot( ...
-	  rvecDeltaNorm_levenberg, rvecOmega_levenberg, 'o-', ...
-	  rvecDeltaNorm_levMarq, rvecOmega_levMarq, 'x-' );
+	image( rvecBigX, rvecBigP, funch_viz(matOmegaM2) );
+	axis("square");
+	set( get(gcf,"children"), "ydir", "normal" );
+	colormap(mycmap(numColors));
+	hold on;
+	plot( bigX_secret, bigP_secret, 'w*', 'markersize', 25, 'linewidth', 2 );
+	plot( bigX0, bigP0, 'ws', 'markersize', 20, 'linewidth', 2 );
+	plot( bigX0+matDelta_h1_d0(1,:), bigP0+matDelta_h1_d0(2,:), 'ko-' );
+	plot( bigX0+matDelta_h2_d0(1,:), bigP0+matDelta_h2_d0(2,:), 'rv-' );
+	hold off;
 	grid on;
-	xlabel( "delta norm" );
-	ylabel( "omega" );
-	title( "omega vs delta norm" );
-	legend( ...
-	  "lev", ...
-	  "levMarq", ...
-	  "location", "northEast" );
+	xlabel( "bigX" );
+	ylabel( "bigP" );
+	title( "omega model 2 vs bigX, bigP" );
 return;
 %end
