4adfb66d98dd6e27d289a4532f7624d486f690b4
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-07-15 23:06:46 -0700

    study/20210715extFitter: added several figs.

--- study/20210715extFitter/test_extFitterOneSided.m
+++ study/20210715extFitter/test_extFitterOneSided.m
@@ -5,7 +5,7 @@
 	%
 	%
 	setprngstates(0);
-	xVals = sort(abs(randn(1,200)));
+	xVals = sort(abs(randn(1,10)));
 	fVals = abs(xVals).^4;
 	%
 	%
@@ -14,29 +14,16 @@
 	%
 	%sVals = linspace(-0.1,0.0,31);
 	%pVals = linspace(3.8,4.2,51);
-	sVals = linspace(-0.2,0.1,51);
+	sVals = linspace(-0.2,0.2,51);
 	pVals = linspace(3.0,5.0,51);
 	[ sMesh, pMesh ] = meshgrid( sVals, pVals );
 	size1 = size(sMesh,1);
 	size2 = size(sMesh,2);
 	[ omegaMesh, bigG0Mesh, bigG1Mesh, dat ] = extFitterOneSided_getOmegaMesh( ...
 	  sMesh, pMesh, xVals, fVals, nOfClosestPt );
+	xExtMesh = dat.bigX0 + dat.bigX1*sMesh;
+	fExtMesh = dat.bigF0 + dat.bigF1*bigG0Mesh;
 	sqrtOmegaMesh = sqrt(omegaMesh);
-	%
-	numFigs++; figure(numFigs);
-	plot( xVals, fVals, 'ko-', 'linewidth', 5, 'markersize', 20 );
-	grid on;
-	hold on;
-	yVals = ( xVals - dat.bigX0 ) / dat.bigX1;
-	for i1=(size1+[-3,1,3])/2
-	for i2=(size2+[-3,1,3])/2
-		gModelVals = bigG0Mesh(i1,i2) ...
-		  + bigG1Mesh(i1,i2)*(abs(yVals-sMesh(i1,i2)).^pMesh(i1,i2));
-		fModelVals = dat.bigF0 + dat.bigF1*gModelVals;
-		plot( xVals, fModelVals, 'x-', 'linewidth', 3, 'markersize', 10 );
-	end
-	end
-	hold off;
 	%
 	%
 	%
@@ -127,142 +114,140 @@
 	xlabel( "s" );
 	ylabel( "p" );
 	title( [ strZ " vs s, p" ] );
-return;
-
-	  
-
-
-
-function extFinder_viz( xVals, fVals, bigSLo, bigSHi, bigPLo, bigPHi, prm=[] )
-	
-	commondefs;
-	thisFile = "extFinder_viz";
-	numFigs = mygetfield( prm, "numFigs", 0 );
 	%
-	gVals = abs(fVals);
-	[ foo, nOfPtWiseMin ] = min( gVals )
 	%
-	if (isempty(bigSLo))
-		bigSLo = xVals(nOfPtWiseMin-1);
+	%
+	meshZ = xExtMesh;
+	strZ = "x_{ext}";
+	numFigs++; figure(numFigs);
+	plot( sMesh((size1+[-3,1,3])/2,:)', meshZ((size1+[-3,1,3])/2,:)', 'o-' );
+	grid on;
+	xlabel( "s" );
+	ylabel( strZ );
+	title( [ strZ " vs s" ] );
+	%
+	numFigs++; figure(numFigs);
+	plot( pMesh(:,(size2+[-3,1,3])/2), meshZ(:,(size2+[-3,1,3])/2), 'o-' );
+	grid on;
+	xlabel( "p" );
+	ylabel( strZ );
+	title( [ strZ " vs p" ] );
+	%
+	numContours = 30;
+	numColors = numContours+1;
+	numFigs++; figure(numFigs);
+	contourf( sVals, pVals, meshZ, numContours );
+	axis("square");
+	set( get(gcf,"children"), "ydir", "normal" );
+	colormap(mycmap(numColors));
+	grid on;
+	xlabel( "s" );
+	ylabel( "p" );
+	title( [ strZ " vs s, p" ] );
+	%
+	%
+	%
+	meshZ = fExtMesh;
+	strZ = "f_{ext}";
+	numFigs++; figure(numFigs);
+	plot( sMesh((size1+[-3,1,3])/2,:)', meshZ((size1+[-3,1,3])/2,:)', 'o-' );
+	grid on;
+	xlabel( "s" );
+	ylabel( strZ );
+	title( [ strZ " vs s" ] );
+	%
+	numFigs++; figure(numFigs);
+	plot( pMesh(:,(size2+[-3,1,3])/2), meshZ(:,(size2+[-3,1,3])/2), 'o-' );
+	grid on;
+	xlabel( "p" );
+	ylabel( strZ );
+	title( [ strZ " vs p" ] );
+	%
+	numContours = 30;
+	numColors = numContours+1;
+	numFigs++; figure(numFigs);
+	contourf( sVals, pVals, meshZ, numContours );
+	axis("square");
+	set( get(gcf,"children"), "ydir", "normal" );
+	colormap(mycmap(numColors));
+	grid on;
+	xlabel( "s" );
+	ylabel( "p" );
+	title( [ strZ " vs s, p" ] );
+	%
+	%
+	%
+	numFigs++; figure(numFigs);
+	plot( xVals, fVals, 'ko-', 'linewidth', 5, 'markersize', 20 );
+	grid on;
+	xlabel( "x" );
+	ylabel( "f" );
+	hold on;
+	yVals = ( xVals - dat.bigX0 ) / dat.bigX1;
+	for i1=(size1+[-3,1,3])/2
+	for i2=(size2+[-3,1,3])/2
+		gModelVals = bigG0Mesh(i1,i2) ...
+		  + bigG1Mesh(i1,i2)*(abs(yVals-sMesh(i1,i2)).^pMesh(i1,i2));
+		fModelVals = dat.bigF0 + dat.bigF1*gModelVals;
+		plot( xVals, fModelVals, 'x-', 'linewidth', 3, 'markersize', 10 );
 	end
-	if (isempty(bigSHi))
-		bigSHi = xVals(nOfPtWiseMin+1);
 	end
-	%
-	numColors = 1000;
-	sizeBigS = 51;
-	sizeBigP = 53;
-	bigSVals = linspace( bigSLo, bigSHi, sizeBigS );
-	bigPVals = linspace( bigPLo, bigPHi, sizeBigP );
-	[ bigSMesh, bigPMesh ] = meshgrid( bigSVals, bigPVals );
-	tic();
-	[ omegaMesh, bigAMesh, bigBMesh, bigCMesh ] = extFinder_getOmegaMesh( bigSMesh, bigPMesh, xVals, fVals, nOfPtWiseMin );
-	min(min(omegaMesh))
-	max(max(omegaMesh))
-	toc();
-	%
-	numFigs++; figure(numFigs);
-	%plot( bigSVals((sizeBigP+1)/2,:), omegaMesh((sizeBigP+1)/2,:) );
-	plot( bigSMesh((sizeBigP+[-3,1,3])/2,:)', omegaMesh((sizeBigP+[-3,1,3])/2,:)', 'o-' );
-	grid on;
-	xlabel( "bigS" );
-	ylabel( "omega" );
-	title( "omega vs bigS" );
-	%
-	numFigs++; figure(numFigs);
-	plot( bigPMesh(:,(sizeBigS+[-3,1,3])/2), omegaMesh(:,(sizeBigP+[-3,1,3])/2), 'o-' );
-	grid on;
-	xlabel( "bigP" );
-	ylabel( "omega" );
-	title( "omega vs bigP" );
-	%
-	numFigs++; figure(numFigs);
-	%contourf( bigSVals, bigPVals, asinh(omegaMesh*1e6)/1e6, 50 );
-	%contourf( bigSVals, bigPVals, log(sqrt(eps)*max(max(omegaMesh))+omegaMesh), 50 );
-	contourf( bigSVals, bigPVals, (omegaMesh).^0.5, 20 );
-	axis("square");
-	set( get(gcf,"children"), "ydir", "normal" );
-	colormap(mycmap(numColors));
-	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigP" );
-	title( "omega vs bigS, bigP" );
+	hold off;
 	%
 	%
 	%
 	numFigs++; figure(numFigs);
-	plot( bigSMesh((sizeBigP+[-3,1,3])/2,:)', bigAMesh((sizeBigP+[-3,1,3])/2,:)', 'o-' );
+	semilogy( xVals, fVals, 'ko-', 'linewidth', 5, 'markersize', 20 );
 	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigA" );
-	title( "bigA vs bigS" );
-	%
-	numFigs++; figure(numFigs);
-	plot( bigPMesh(:,(sizeBigS+[-3,1,3])/2), bigAMesh(:,(sizeBigP+[-3,1,3])/2), 'o-' );
-	grid on;
-	xlabel( "bigP" );
-	ylabel( "bigA" );
-	title( "bigA vs bigP" );
-	%
-	numFigs++; figure(numFigs);
-	contourf( bigSVals, bigPVals, bigAMesh, 20 );
-	axis("square");
-	set( get(gcf,"children"), "ydir", "normal" );
-	colormap(mycmap(numColors));
-	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigP" );
-	title( "bigA vs bigS, bigP" );
+	xlabel( "x" );
+	ylabel( "f" );
+	hold on;
+	yVals = ( xVals - dat.bigX0 ) / dat.bigX1;
+	for i1=(size1+[-3,1,3])/2
+	for i2=(size2+[-3,1,3])/2
+		gModelVals = bigG0Mesh(i1,i2) ...
+		  + bigG1Mesh(i1,i2)*(abs(yVals-sMesh(i1,i2)).^pMesh(i1,i2));
+		fModelVals = dat.bigF0 + dat.bigF1*gModelVals;
+		semilogy( xVals, fModelVals, 'x-', 'linewidth', 3, 'markersize', 10 );
+	end
+	end
+	hold off;
 	%
 	%
 	%
 	numFigs++; figure(numFigs);
-	plot( bigSMesh((sizeBigP+[-3,1,3])/2,:)', bigBMesh((sizeBigP+[-3,1,3])/2,:)', 'o-' );
+	plot( xVals, fVals-fVals, 'k-', 'linewidth', 2 );
 	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigB" );
-	title( "bigB vs bigS" );
-	%
-	numFigs++; figure(numFigs);
-	plot( bigPMesh(:,(sizeBigS+[-3,1,3])/2), bigBMesh(:,(sizeBigP+[-3,1,3])/2), 'o-' );
-	grid on;
-	xlabel( "bigP" );
-	ylabel( "bigB" );
-	title( "bigB vs bigP" );
-	numFigs++; figure(numFigs);
-	contourf( bigSVals, bigPVals, bigBMesh, 20 );
-	axis("square");
-	set( get(gcf,"children"), "ydir", "normal" );
-	colormap(mycmap(numColors));
-	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigP" );
-	title( "bigB vs bigS, bigP" );
+	xlabel( "x" );
+	ylabel( "f_{model} - f" );
+	hold on;
+	yVals = ( xVals - dat.bigX0 ) / dat.bigX1;
+	for i1=(size1+[-3,1,3])/2
+	for i2=(size2+[-3,1,3])/2
+		gModelVals = bigG0Mesh(i1,i2) ...
+		  + bigG1Mesh(i1,i2)*(abs(yVals-sMesh(i1,i2)).^pMesh(i1,i2));
+		fModelVals = dat.bigF0 + dat.bigF1*gModelVals;
+		plot( xVals, fModelVals-fVals, 'x-', 'linewidth', 3, 'markersize', 10 );
+	end
+	end
+	hold off;
 	%
 	%
 	%
 	numFigs++; figure(numFigs);
-	plot( bigSMesh((sizeBigP+[-3,1,3])/2,:)', bigCMesh((sizeBigP+[-3,1,3])/2,:)', 'o-' );
+	plot( xVals, fVals./abs(fVals)-1.0, 'k-', 'linewidth', 2 );
 	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigC" );
-	title( "bigC vs bigS" );
-	%
-	numFigs++; figure(numFigs);
-	plot( bigPMesh(:,(sizeBigS+[-3,1,3])/2), bigCMesh(:,(sizeBigP+[-3,1,3])/2), 'o-' );
-	grid on;
-	xlabel( "bigP" );
-	ylabel( "bigC" );
-	title( "bigC vs bigP" );
-	numFigs++; figure(numFigs);
-	contourf( bigSVals, bigPVals, bigCMesh, 20 );
-	axis("square");
-	set( get(gcf,"children"), "ydir", "normal" );
-	colormap(mycmap(numColors));
-	grid on;
-	xlabel( "bigS" );
-	ylabel( "bigP" );
-	title( "bigC vs bigS, bigP" );
-	%
+	xlabel( "x" );
+	ylabel( "f_{model}/|f| - 1" );
+	hold on;
+	yVals = ( xVals - dat.bigX0 ) / dat.bigX1;
+	for i1=(size1+[-3,1,3])/2
+	for i2=(size2+[-3,1,3])/2
+		gModelVals = bigG0Mesh(i1,i2) ...
+		  + bigG1Mesh(i1,i2)*(abs(yVals-sMesh(i1,i2)).^pMesh(i1,i2));
+		fModelVals = dat.bigF0 + dat.bigF1*gModelVals;
+		plot( xVals, fModelVals./abs(fVals)-1.0, 'x-', 'linewidth', 3, 'markersize', 10 );
+	end
+	end
+	hold off;
 return;
-end
