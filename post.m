421eb664f05bc5186b0e5ae1e396f38718e8527a
Plasma Dan <draburn@p-grp.nucleng.kyoto-u.ac.jp>
2021-07-20 01:07:14 -0700

    Start s0solve20210719.

--- post.m
+++
@@ -1,9 +0,0 @@
-iterToViz = 3
-studyPt_prm.vecXSecret = funcPrm.x0;
-tic();
-this_funchF = grootDatOut.funchF;
-this_vecX = grootDatOut.iterDat(iterToViz).vecX;
-this_vecV = eye(sizeX,sizeX);
-this_vecW = prm.funchJ(this_vecX);
-p = studyPt( this_funchF, this_vecX, this_vecV, this_vecW, studyPt_prm );
-toc();
--- work_that_was.m
+++
@@ -1,6 +0,0 @@
-clear;
-init;
-prc0522_3db;
-%
-prc0522_3db_trace
-
