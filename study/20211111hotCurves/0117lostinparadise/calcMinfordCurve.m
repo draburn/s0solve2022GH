58165dcd77c5fad740651b864850436f1aab81c1
Plasma Dan <draburn@alumni.princeton.edu>
2022-01-24 15:31:07 -0800

    0117lostinparadise: Added calcMinfordCurve.m.

---
+++ study/20211111hotCurves/0117lostinparadise/calcMinfordCurve.m
@@ -0,0 +1,66 @@
+function [ matX, datOut ] = calcMinfordCurve( vecX0, funchOmega, prm=[] )
+	%
+	sizeX = size(vecX0,1);
+	assert(isrealarray(vecX0,[sizeX,1]));
+	%
+	matS = [];
+	if (~isempty(matS))
+		assert(isrealarray(matS,[sizeX,sizeX]));
+		matS_nonEmpty = matS;
+	else
+		matS_nonEmpty = eye(sizeX,sizeX);
+		% Use this for convenience, but, for efficiency,
+		% let sub-modules know that matS is actually empty.
+	end
+	datOut = [];
+	%
+	%
+	%
+	iterLimit = 10000;
+	deltaR = 0.1;
+	vecXC = vecX0;
+	%[ omega0, vecNablaOmega0 ] = funchOmega( vecX0 );
+	%tolMagNablaOmega = mygetfield( prm, "tolMagNablaOmega", eps025*norm(vecNablaOmega0) + eps050*sizeX );
+	%
+	bigR = 0.0;
+	iterCount = 0;
+	%
+	vecX = vecX0;
+	onSurf = false;
+	matX(:,iterCount+1) = vecX;
+	while (1)
+		iterCount++;
+		if ( iterCount > iterLimit )
+			msg( __FILE__, __LINE__, "Reached iterLimit." );
+			return;
+		end
+		%
+		bigR = bigR + deltaR;
+		vecX = matX(:,iterCount);
+		%
+		%
+		if ( isempty(matS) )
+			funchSurf = @(x)( funcSurfEllip( x, vecXC, bigR ) );
+		else
+			funchSurf = @(x)( funcSurfEllip( x, vecXC, bigR, matS ) );
+		end
+		%
+		vecX_next = findOmegaMinWithinSurf( vecX, funchSurf, funchOmega );
+		%
+		assert( isrealarray(vecX_next,[sizeX,1]) );
+		s_next = norm(matS_nonEmpty*(vecX_next-vecXC));
+		if ( s_next < bigR - (0.5*deltaR) )
+			msg( __FILE__, __LINE__, "Reached local min." );
+			return;
+		end
+		if ( s_next > bigR*(1.0-sqrt(eps)) )
+			if ( s_next > bigR + (0.5*deltaR) )
+				msg( __FILE__, __LINE__, "WARNING: Generated point lies well outside surface." );
+			end
+			vecX_next = vecXC + bigR*(vecX_next-vecXC)/s_next;
+		end
+		matX(:,iterCount+1) = vecX_next;
+	end
+	%
+return;
+end
---
+++ study/20211111hotCurves/0117lostinparadise/findOmegaMinWithinSurf.m
@@ -0,0 +1,55 @@
+function [ vecX, retCode, datOut ] = findOmegaMinWithinSurf( vecX0, funchSurf, funchOmega, prm=[] )
+	debugMode = mygetfield( prm, "debugMode", true );
+	%
+	% Validate main input.
+	sizeX = size(vecX0,1);
+	if ( debugMode )
+		assert( isscalar(debugMode) );
+		assert( isbool(debugMode) );
+		assert( isrealarray(vecX0,[sizeX,1]) );
+		[ vecS0, vecU0, vecV0, matNablaST0 ] = funchSurf( vecX0 );
+		[ omega0, vecNablaOmega0 ] = funchOmega( vecX0 );
+		assert( isrealarray(vecS0,[sizeX,1]) );
+		assert( isrealarray(vecU0,[sizeX,1]) );
+		assert( isrealarray(vecV0,[sizeX,1]) );
+		assert( isrealarray(matNablaST0,[sizeX,sizeX]) );
+		assert( abs(norm(vecU0)-1.0) < sizeX*(eps^0.75) );
+		assert( abs(norm(vecV0)-1.0) < sizeX*(eps^0.75) );
+		assert( isrealscalar(omega0) );
+		assert( isrealarray(vecNablaOmega0,[sizeX,1]) );
+	end
+	datOut = [];
+	%
+	%
+	% Set up the objective function that combines the input funchSurf and funchOmega.
+	tauX = mygetfield( prm, "tauX", 1e-2 );
+	h0 = mygetfield( prm, "h0", norm(vecNablaOmega0)/tauX );
+	epsX = mygetfield( prm, "epsX", 1e-5 );
+	useProvidedGradients = mygetfield( prm, "useProvidedGradients", true );
+	if ( debugMode )
+		assert( isrealscalar(tauX) );
+		assert( isrealscalar(h0) );
+		assert( isrealscalar(epsX) );
+		assert( 0.0 < epsX );
+		assert( isscalar(useProvidedGradients) );
+		assert( isbool(useProvidedGradients) );
+	end
+	prm.h0 = h0;
+	prm.tau = tauX;
+	prm.epsX = epsX;
+	funchBigF = @(dummyX)( funcOmegaWithinSurf( dummyX, funchOmega, funchSurf, prm ) );
+	%
+	%
+	% Do work.
+	if (useProvidedGradients)
+		fminunc_opts = optimset( 'GradObj', 'on' );
+		vecX = fminunc( funchBigF, vecX0, fminunc_opts );
+	else
+		vecX = fminunc( funchBigF, vecX0 );
+	end
+	if ( debugMode )
+		assert( isrealarray(vecX,[sizeX,1]) );
+	end
+	retCode = 0;
+return;
+end
