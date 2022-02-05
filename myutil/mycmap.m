28145329f5b9af0d405f556e467982441a7badfa
Plasma Dan <draburn@alumni.princeton.edu>
2022-02-05 00:03:33 -0800

    Settling code... worked on "basic" (cnstH) curves...

--- myutil/mycmap.m
+++ myutil/mycmap.m
@@ -1,5 +1,6 @@
 function cMap = mycmap(n=1000)
-	cMap = 0.7 + (0.3*jet(n));
+	%%%cMap = 0.7 + (0.3*jet(n));
+	cMap = 0.7 + (0.3*colormap("default"));
 	cMap(1,:) *= 0.5;
 	cMap(end,:) *= 0.5;
 	cMap(end,:) += 0.5;
--- study/20211111hotCurves/0117lostinparadise/calcBasicGradCurve.m
+++ study/20211111hotCurves/0117lostinparadise/calcBasicGradCurve.m
@@ -1,3 +1,5 @@
+% function [ vecXVals, datOut ] = calcBasicGradCurve( vecX0, omega0, vecG0, matH, prm=[] )
+
 function [ vecXVals, datOut ] = calcBasicGradCurve( vecX0, omega0, vecG0, matH, prm=[] )
 	%
 	sizeX = size(vecX0,1);
@@ -90,3 +92,61 @@ function [ vecXVals, datOut ] = calcBasicGradCurve( vecX0, omega0, vecG0, matH, 
 	vecXVals(:,end+1) = vecXB + s*vecZ;
 return;
 end
+
+
+%!test
+%!	sizeX = 2;
+%!	sizeF = 2;
+%!	switch (1)
+%!	case 0
+%!		vecX0 = zeros(sizeX,1);
+%!		vecF0 = ones(sizeF,1);
+%!		matJ0 = eye(sizeF,sizeX);
+%!	case 1
+%!		setprngstates();
+%!		vecX0 = randn(sizeX,1);
+%!		vecF0 = randn(sizeF,1);
+%!		matJ0 = randn(sizeF,sizeX);
+%!	end
+%!	omega0 = (vecF0'*vecF0)/2.0;
+%!	vecG0 = matJ0'*vecF0;
+%!	matH0 = matJ0'*matJ0;
+%!	funchF = @(dummyX)( vecF0 + matJ0*(dummyX-vecX0) );
+%!	funchOmega = @(dummyX)( sumsq(funchF(dummyX),1)/2.0 );
+%!	%
+%!	vecXPts = calcBasicGradCurve( vecX0, omega0, vecG0, matH0 );
+%!	numPts = size(vecXPts,2);
+%!	assert( isrealarray(vecXPts,[sizeX,numPts]) );
+%!	%
+%!	x1Lo = min(vecXPts(1,:));
+%!	x1Hi = max(vecXPts(1,:));
+%!	x2Lo = min(vecXPts(2,:));
+%!	x2Hi = max(vecXPts(2,:));
+%!	x1Diff = max([ 0.1, x1Hi-x1Lo ]);
+%!	x2Diff = max([ 0.1, x2Hi-x2Lo ]);
+%!	x1Lo = x1Lo - 0.3*x1Diff
+%!	x1Hi = x1Hi + 0.3*x1Diff
+%!	x2Lo = x2Lo - 0.3*x2Diff
+%!	x2Hi = x2Hi + 0.3*x2Diff
+%!	numX1Vals = 101;
+%!	numX2Vals = 81;
+%!	%
+%!	x1Vals = linspace( x1Lo, x1Hi, numX1Vals );
+%!	x2Vals = linspace( x2Lo, x2Hi, numX2Vals );
+%!	[ x1Mesh, x2Mesh ] = meshgrid( x1Vals, x2Vals );
+%!	vecXVals = [ reshape(x1Mesh,1,[]); reshape(x2Mesh,1,[]) ];
+%!	omegaVals = funchOmega( vecXVals );
+%!	omegaMesh = reshape( omegaVals, numX2Vals, numX1Vals );
+%!	%
+%!	contourf( x1Mesh, x2Mesh, sqrt(omegaMesh) );
+%!	colormap( mycmap(256) );
+%!	hold on;
+%!	plot( ...
+%!	  vecX0(1), vecX0(2), 'p', 'linewidth', 4, 'markersize', 30, ...
+%!	  vecXPts(1,:), vecXPts(2,:), 'o-', 'linewidth', 2, 'markersize', 10, ...
+%!	  vecXPts(1,end), vecXPts(2,end), 'x-', 'linewidth', 4, 'markersize', 25 );
+%!	hold off;
+%!	grid on;
+%!	xlabel( "x1" );
+%!	ylabel( "x2" );
+%!	title( "sqrt(omega) vs x1, x2" );
