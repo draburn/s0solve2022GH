737aec97aeec2fe323b803122d81d5806e202f8c
Plasma Dan <draburn@alumni.princeton.edu>
2022-02-07 13:52:00 -0800

    calcLevCurve_focq: Added "curveSelector == -2", which selects min xi point.

--- numopt/calcLevCurve_focq.m
+++ numopt/calcLevCurve_focq.m
@@ -2,7 +2,7 @@
 %  [ vecXPts, datOut ] = calcLevCurve_focq( vecX0, vecF0, matJ0, vecPhiHat, vecEta, prm=[] )
 % Calculates points along the "F One Component Quadratic" Levenberg curve for the specified quantities.
 % The model function F is given by...
-%  vecF = vecF + matJ0*vecDelta + vecEta*(vecPhiHat'*vecDelta)^2
+%  vecF = vecF0 + matJ0*vecDelta + vecEta*(vecPhiHat'*vecDelta)^2
 % where vecDelta = vecX - vecX0. (Note the absence of "0.5*" in front of vecEta.)
 % For values of "p" for which there are more than one solution,
 %  the returned vecX is selected per prm.curveSelector.
@@ -94,6 +94,15 @@ function [ vecXPts, datOut ] = calcLevCurve_focq( vecX0, vecF0, matJ0, vecPhiHat
 			vecXPts(:,n) = vecXRoots(:,1);
 		case 3
 			switch (curveSelector)
+			case -2
+				vecDeltaRoots = vecXRoots-vecX0;
+				vecFRoots = vecF0 + matJ0*vecDeltaRoots + vecEta*((vecPhiHat'*vecDeltaRoots).^2);
+				xiRoots = p*(vecFRoots.^2) + (1.0-p)*( (sz*zRoots).^2 + sumsq(matSY*vecYRoots,1) );
+				if ( xiRoots(1) < xiRoots(3) )
+					vecXPts(:,n) = vecXRoots(:,1);
+				else
+					vecXPts(:,n) = vecXRoots(:,3);
+				end
 			case -1
 				vecXPts(:,n) = vecXRoots(:,1);
 			case 0
@@ -306,6 +315,116 @@ end
 %!	prm = [];
 %!	%prm.debugMode = true;
 %!	prm.pPts = linspace(0.0,1.0,201).^2;
+%!	%
+%!	[ vecXPts, datOut ] = calcLevCurve_focq( vecX0, vecF0, matJ0, vecPhiHat, vecEta, prm );
+%!	numPts = size(vecXPts,2);
+%!	assert( isrealarray(vecXPts,[sizeX,numPts]) );
+%!	%
+%!	vecXPts_l = zeros(sizeX,datOut.multiRootCounter);
+%!	vecXPts_c = zeros(sizeX,datOut.multiRootCounter);
+%!	vecXPts_r = zeros(sizeX,datOut.multiRootCounter);
+%!	for n=1:datOut.multiRootCounter
+%!		vecXPts_l(:,n) = datOut.multiRoot(n).vecXRoots(:,1);
+%!		vecXPts_c(:,n) = datOut.multiRoot(n).vecXRoots(:,2);
+%!		vecXPts_r(:,n) = datOut.multiRoot(n).vecXRoots(:,3);
+%!	end
+%!	%
+%!	%
+%!	x1Lo = min([ min(vecXPts_l(1,:)), min(vecXPts_c(1,:)), min(vecXPts_r(1,:)), min(vecXPts(1,:)) ])
+%!	x1Hi = max([ max(vecXPts_l(1,:)), max(vecXPts_c(1,:)), max(vecXPts_r(1,:)), max(vecXPts(1,:)) ])
+%!	x2Lo = min([ min(vecXPts_l(2,:)), min(vecXPts_c(2,:)), min(vecXPts_r(2,:)), min(vecXPts(2,:)) ])
+%!	x2Hi = max([ max(vecXPts_l(2,:)), max(vecXPts_c(2,:)), max(vecXPts_r(2,:)), max(vecXPts(2,:)) ])
+%!	x1Diff = max([ 0.1, x1Hi-x1Lo ]);
+%!	x2Diff = max([ 0.1, x2Hi-x2Lo ]);
+%!	x1Lo = x1Lo - 0.3*x1Diff;
+%!	x1Hi = x1Hi + 0.3*x1Diff;
+%!	x2Lo = x2Lo - 0.3*x2Diff;
+%!	x2Hi = x2Hi + 0.3*x2Diff;
+%!	numX1Vals = 101;
+%!	numX2Vals = 81;
+%!	%
+%!	x1Vals = linspace( x1Lo, x1Hi, numX1Vals );
+%!	x2Vals = linspace( x2Lo, x2Hi, numX2Vals );
+%!	[ x1Mesh, x2Mesh ] = meshgrid( x1Vals, x2Vals );
+%!	vecXVals = [ reshape(x1Mesh,1,[]); reshape(x2Mesh,1,[]) ];
+%!	omegaVals = funchOmega( vecXVals );
+%!	omegaMesh = reshape( omegaVals, numX2Vals, numX1Vals );
+%!	%
+%!	numFigs++; figure(numFigs);
+%!	contourf( x1Mesh, x2Mesh, sqrt(omegaMesh) );
+%!	colormap( mycmap(256) );
+%!	hold on;
+%!	plot( ...
+%!	  vecX0(1), vecX0(2), 'kp', 'linewidth', 5, 'markersize', 50, ...
+%!	  vecXPts(1,:), vecXPts(2,:), 'ko-', 'linewidth', 2, 'markersize', 20, ...
+%!	  vecXPts(1,end), vecXPts(2,end), 'ko-', 'linewidth', 4, 'markersize', 30, ...
+%!	  vecXPts_l(1,:), vecXPts_l(2,:), 'rv', 'linewidth', 2, 'markersize', 15, ...
+%!	  vecXPts_c(1,:), vecXPts_c(2,:), 'gs', 'linewidth', 2, 'markersize', 10, ...
+%!	  vecXPts_r(1,:), vecXPts_r(2,:), 'b^', 'linewidth', 2, 'markersize', 5 );
+%!	hold off;
+%!	grid on;
+%!	xlabel( "x1" );
+%!	ylabel( "x2" );
+%!	title( "sqrt(omega) vs x1, x2" );
+
+
+
+%!test
+%!	numFigs = 2;
+%!	sizeX = 2;
+%!	sizeF = 2;
+%!	switch (10020)
+%!	case 0
+%!		vecX0 = zeros(sizeX,1);
+%!		vecF0 = ones(sizeF,1);
+%!		matJ0 = eye(sizeF,sizeX);
+%!		vecEta = ones(sizeX,1);
+%!	case 1
+%!		setprngstates();
+%!		vecX0 = randn(sizeX,1);
+%!		vecF0 = randn(sizeF,1);
+%!		matJ0 = randn(sizeF,sizeX);
+%!		vecEta = randn(sizeF,1);
+%!	case 10010
+%!		% "1->3->1 detour".
+%!		vecX0 = zeros(sizeX,1);
+%!		vecF0 = [ -2.0; 1.0 ];
+%!		matJ0 = [ 1.0, 0.0; 0.0, 0.0 ];
+%!		vecEta = [ 2.0; 1.0 ];
+%!	case 10011
+%!		% "1->3->1 disconnected loop".
+%!		vecX0 = zeros(sizeX,1);
+%!		vecF0 = [ -2.0; 1.0 ];
+%!		matJ0 = [ 1.0, 0.0; 0.0, 0.02 ];
+%!		vecEta = [ 2.0; 1.0 ];
+%!	case 10020
+%!		% "1->3->1 cts -> 3"
+%!		vecX0 = zeros(sizeX,1);
+%!		vecF0 = [ -100.0; 1.0 ];
+%!		matJ0 = [ 3.1, 0.0; 0.0, -3.0 ];
+%!		vecEta = [ 0.1; 1.0 ];
+%!	case 10050
+%!		setprngstates(22027120);
+%!		vecX0 = randn(sizeX,1);
+%!		vecF0 = randn(sizeF,1);
+%!		matJ0 = randn(sizeF,sizeX);
+%!		vecEta = randn(sizeF,1);
+%!	end
+%!	omega0 = (vecF0'*vecF0)/2.0;
+%!	vecG0 = matJ0'*vecF0;
+%!	matH0 = matJ0'*matJ0;
+%!	% We'll take vecPhiHat to be the eigenvec for the absmin eigval of matH0.
+%!	[ matPsi0, matLambda0 ] = eig(matH0);
+%!	[ lambda0AbsMin0, nOfAbsMin0 ] = min(abs(diag(matLambda0)));
+%!	vecPhiHat = matPsi0(:,nOfAbsMin0);
+%!	%
+%!	funchF = @(dummyX)( vecF0 + matJ0*(dummyX-vecX0) + vecEta*sumsq(vecPhiHat'*(dummyX-vecX0),1) );
+%!	funchOmega = @(dummyX)( sumsq(funchF(dummyX),1)/2.0 );
+%!	%
+%!	prm = [];
+%!	%prm.debugMode = true;
+%!	prm.pPts = linspace(0.0,1.0,201).^2;
+%!	prm.curveSelector = -2;
 %!	%
 %!	[ vecXPts, datOut ] = calcLevCurve_focq( vecX0, vecF0, matJ0, vecPhiHat, vecEta, prm );
 %!	numPts = size(vecXPts,2);
