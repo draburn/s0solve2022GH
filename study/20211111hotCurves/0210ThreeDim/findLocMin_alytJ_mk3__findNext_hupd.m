90608f58277563e6e9bc23bb2dacbc5bba42f931
Plasma Dan <draburn@alumni.princeton.edu>
2022-02-18 15:28:09 -0800

    .

--- study/20211111hotCurves/0210ThreeDim/findLocMin_alytJ_mk3__findNext_hupd.m
+++ study/20211111hotCurves/0210ThreeDim/findLocMin_alytJ_mk3__findNext_hupd.m
@@ -5,6 +5,7 @@ return;
 %
 matK = zeros(sizeX,sizeX);
 trialCount = 0;
+haveBestSoFar = false;
 vecX_next = []; % Keep track of best so far.
 vecF_next = []; % Keep track of best so far.
 omega_next = []; % Keep track of best so far.
@@ -27,6 +28,7 @@ while (1)
 	genCount = 0;
 	mu = 0.0;
 	matH = matJTJ + matK;
+	matH = (matH'+matH)/2.0;
 	while (1)
 		genCount++;
 		if ( genCount > genLimit )
@@ -75,7 +77,7 @@ while (1)
 	vecF_trial = funchFJ( vecX_trial );
 	if ( ~isrealarray(vecF_trial,[sizeF,1]) )
 		msgif( debugMode, __FILE__, __LINE__, "Function evaluation failed." );
-		if (~isempty(vecX_next))
+		if (haveBestSoFar)
 			msgif( __FILE__, __LINE__, "Function evaluation failed even though it succeeded for an earlier trial step!" );
 			msgif( __FILE__, __LINE__, "This suggests the invalid space is not simply at distant values of x." );
 		endif
@@ -96,7 +98,7 @@ while (1)
 	assert( c_hugeChangeToF >= 2.0 );
 	if ( norm(vecF_trial-vecF) >= c_hugeChangeToF*norm(vecF) )
 		msgif( debugMode, __FILE__, __LINE__, "Function was radically different at trial point." );
-		if (~isempty(vecX_next))
+		if (haveBestSoFar)
 			msgif( __FILE__, __LINE__, "Function was radically different even though it was reasonable for an earlier trial step!" );
 			msgif( __FILE__, __LINE__, "This suggests the presence of a sepratrix." );
 		endif
@@ -107,22 +109,56 @@ while (1)
 	endif
 	%
 	%
+	% Having gotten here, the step is reasonable enough that we can use the results to update matK.
+	if ( omega_trial >= omega )
+		msgif( debugMode, __FILE__, __LINE__, "omega_trial >= omega" );
+		matK += (2.0*(omega_trial-omegaModel_trial)/sumsq(vecDelta_trial)^2)*(vecDelta_trial*(vecDelta_trial'));
+		continue;
+	endif
+	%
+	%
 	%
 	% Check outright acceptance criterion.
-	if ( omega_trial <= (1.0-c_acceptance)*omega + c_acceptance*omegaModel_trial )
+	c_acceptOutright = 0.3; % MAKE PARAM
+	if ( omega_trial <= (1.0-c_acceptOutright)*omega + c_acceptOutright*omegaModel_trial )
 		msgif( debug, __FILE__, __LINE__, "Step is very good!" );
-		if (~isempty(omega_next))
-		if ( omega_trial >= omega_next )
-			error( "Somehow this very good step is no better than an earlier one. This should be impossible." );
+		if (haveBestSoFar)
+			assert( ~isempty(omega_trial) );
+			if ( omega_trial >= omega_next )
+				error( "Somehow this very good step is no better than an earlier one. This should be impossible." );
+			endif
 		endif
-		endif
+		% Update to matK won'd be used, but, let's do it anyway.
+		matK += (2.0*(omega_trial-omegaModel_trial)/sumsq(vecDelta_trial)^2)*(vecDelta_trial*(vecDelta_trial'));
+		haveBestSoFar = true; % Though, this result won't be used anywhere.
 		vecX_next = vecX_trial;
 		vecF_next = vecF_trial;
 		omega_next = omega_trial;
 		return;
 	endif
 	%
+	% Is this the best result so far?
+	if ( ~haveBestSoFar )
+		haveBestSoFar = true;
+		vecX_next = vecX_trial;
+		vecF_next = vecF_trial;
+		omega_next = omega_trial;
+	elseif ( omega_trial > omega_next )
+		msgif( debugMode, __FILE__, __LINE__, "omega_trial > omega_next. This suggests a sepratrix." );
+		% We'll accept the best so far (==next) if it's below a loose threshold...
+		c_acceptReluctant = 1e-3; % MAKE PARAM
+		vecDelta_next = vecX_next - vecX;
+		omegaModel_next = omega + vecG'*vecDelta_next + 0.5*(vecDelta_next'*matH*vecDelta_next); % Repeated. POITROME.
+		% Note that omegaModel_next uses the current K, not the one used to calc _next.
+		if ( omega_next <= (1.0-c_acceptReluctant)*omega + c_acceptReluctant*omegaModel_next )
+			msg( __FILE__, __LINE__, "Omega increased while backtracking and previous(?) trial is good enough." );
+			return;
+		end
+	elseif ( omega_trial < omega_next )
+		vecX_next = vecX_trial;
+		vecF_next = vecF_trial;
+		omega_next = omega_trial;
+	endif
 	%
-	% Having gotten here, the step is reasonable enough that we can use the results to update matK.
-	% We also conditionally might accept the step.
+	matK += (2.0*(omega_trial-omegaModel_trial)/sumsq(vecDelta_trial)^2)*(vecDelta_trial*(vecDelta_trial'));
 endwhile
