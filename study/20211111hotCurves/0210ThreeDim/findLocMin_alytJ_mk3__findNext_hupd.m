042fa5da41bdc66e4706cf98b8e7c5c581ffc298
Plasma Dan <draburn@alumni.princeton.edu>
2022-02-18 15:12:40 -0800

    .

--- study/20211111hotCurves/0210ThreeDim/findLocMin_alytJ_mk3__findNext_hupd.m
+++ study/20211111hotCurves/0210ThreeDim/findLocMin_alytJ_mk3__findNext_hupd.m
@@ -2,19 +2,17 @@ msg( __FILE__, __LINE__, "WIP!" );
 vecX_next = vecX - 0.0001*vecG;
 return;
 
-trialLimit = 100;
-wolfeC = 0.9;
-dGenFailureBTFactor = 0.1;
-c_hugeChangeToF = 10.0;
-dGenRadicallyWrongBTFactor = 0.1;
 %
 matK = zeros(sizeX,sizeX);
 trialCount = 0;
 vecX_next = []; % Keep track of best so far.
+vecF_next = []; % Keep track of best so far.
+omega_next = []; % Keep track of best so far.
 dGenMin = dTreg;
 while (1)
 	% Check pre-iter imposed limits.
 	trialCount++;
+	trialLimit = 100;
 	if ( trialCount > trialLimit )
 		msgif( debugMode, __FILE__, __LINE__, "Reached trialLimit." );
 		return;
@@ -58,6 +56,7 @@ while (1)
 		%
 		% Check if step is too small.
 		% Should we also check omegaFallTol and stepSizeTol???
+		wolfeC = 0.9; % MAKE PARAM
 		if ( abs(vecDelta_trial'*(vecG+matH*vecDelta_trial)) >= wolfeC * abs(vecDelta_trial'*vecG) )
 			msgif( debugMode, __FILE__, __LINE__, "Step is too small per Wolfe curvature condition. Giving up." );
 			fooLHS = vecDelta_trial'*(vecG+matH*vecDelta_trial)
@@ -76,41 +75,52 @@ while (1)
 	vecF_trial = funchFJ( vecX_trial );
 	if ( ~isrealarray(vecF_trial,[sizeF,1]) )
 		msgif( debugMode, __FILE__, __LINE__, "Function evaluation failed." );
+		if (~isempty(vecX_next))
+			msgif( __FILE__, __LINE__, "Function evaluation failed even though it succeeded for an earlier trial step!" );
+			msgif( __FILE__, __LINE__, "This suggests the invalid space is not simply at distant values of x." );
+		endif
+		dGenFailureBTFactor = 0.1; % MAKE PARAM.
 		dGenMin *= dGenFailureBTFactor;
 		msgif( debugMode, __FILE__, __LINE__, "Updating dTreg." );
 		dTreg = dGenMin;
 		continue;
-	end
+	endif
 	omega_trial = sumsq(vecF_trial,1)/2.0;
+	%
+	%
+	%
+	% Find how far vecF_trial is from vecF, vecFModel.
+	% Since ||vecFModel-vecF|| <= 2.0*||vecF|| is guaranteed,
+	%  we can simply compare against vecF.
+	c_hugeChangeToF = 10.0; % MAKE PARAM.
+	assert( c_hugeChangeToF >= 2.0 );
+	if ( norm(vecF_trial-vecF) >= c_hugeChangeToF*norm(vecF) )
+		msgif( debugMode, __FILE__, __LINE__, "Function was radically different at trial point." );
+		if (~isempty(vecX_next))
+			msgif( __FILE__, __LINE__, "Function was radically different even though it was reasonable for an earlier trial step!" );
+			msgif( __FILE__, __LINE__, "This suggests the presence of a sepratrix." );
+		endif
+		dGenRadicallyWrongBTFactor = 0.1; % MAKE PARAM.
+		dGenMin *= dGenRadicallyWrongBTFactor;
+		dTreg = dGenMin;
+		continue;
+	endif
 	%
 	%
 	%
 	% Check outright acceptance criterion.
 	if ( omega_trial <= (1.0-c_acceptance)*omega + c_acceptance*omegaModel_trial )
 		msgif( debug, __FILE__, __LINE__, "Step is very good!" );
+		if (~isempty(omega_next))
+		if ( omega_trial >= omega_next )
+			error( "Somehow this very good step is no better than an earlier one. This should be impossible." );
+		endif
+		endif
 		vecX_next = vecX_trial;
+		vecF_next = vecF_trial;
+		omega_next = omega_trial;
 		return;
-	end
-	%
-	%
-	%
-	% Find how far vecF_trial is from (1-b)*vecF + b*vecFModel, minimized over b.
-	foo1 = ( vecF_trial - vecF )' * ( vecFModel_trial - vecF );
-	foo2 = sumsq( vecFModel_trial - vecF );
-	if ( foo1 <= 0.0 )
-		b = 0.0;
-	elseif ( foo1 >= foo2 )
-		b = 1.0;
-	else
-		b = foo1 / foo2;
 	endif
-	vecRho = vecF_trial - ( (1.0-b)*vecF + b*vecFModel_trial );
-	if ( norm(vecRho) > c_hugeChangeToF * norm(vecF) )
-		msgif( debugMode, __FILE__, __LINE__, "Function was radially different at trial point." );
-		dGenMin *= dGenRadicallyWrongBTFactor;
-		dTreg = dGenMin;
-		continue;
-	end
 	%
 	%
 	% Having gotten here, the step is reasonable enough that we can use the results to update matK.
