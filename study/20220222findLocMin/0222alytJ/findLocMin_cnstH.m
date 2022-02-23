4a8b8fe5246a940eab1783a86e01e7bea544511a
Plasma Dan <draburn@alumni.princeton.edu>
2022-02-23 10:57:36 -0800

    .

--- study/20220222findLocMin/0222alytJ/findLocMin_cnstH.m
+++ study/20220222findLocMin/0222alytJ/findLocMin_cnstH.m
@@ -1,27 +1,49 @@
 % Function...
-%  [?] = findLocMin_cnstH(?)
+%  [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], datIn=[] )
 % Returns vecDelta corresponding to the local min of the omega model,
-%  possibly subject to a trust region and a "reasonableness" check such as omega >= 0.
+%  possibly subject to a trust region and a "reasonableness" constraint (such as omega >= 0).
 
 function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], datIn=[] )
 	%
 	%
 	% Parse input.
-	sizeX = size(vecG0,1);
+	sizeX = size(vecG,1);
 	debugMode = mygetfield( prm, "debugMode", true );
-	if (debugMode)
+	if ( debugMode )
 		assert( isrealscalar(omega0) );
-		assert( isrealarray(vecG0,[sizeX,1]) );
+		assert( isrealarray(vecG,[sizeX,1]) );
 		assert( isrealarray(matH,[sizeX,sizeX]) );
 		assert( issymmetric(matH) );
 	endif
 	gNormSq = sumsq(vecG);
 	gNorm = sqrt(gNormSq);
 	hNorm = sqrt(sum(sumsq(matH)));
+	matI = eye(sizeX,sizeX);
+	%
+	muReguCoeff = muReguCoeff( prm, "muReguCoeff", 1.0e-5 );
 	deltaNormMax = mygetfield( prm, "deltaNormMax", 100.0*gNorm/hNorm );
-	deltaNormMaxRelTol = mygetfield( prm, "deltaNormMaxTol", 0.4 );
+	deltaNormMaxRelTol = mygetfield( prm, "deltaNormMaxRelTol", 0.4 );
 	omegaModelMin = mygetfield( prm, "omegaModelMin", [] );
 	omegaModelMinRelTol = mygetifled( prm, "omegaModelMinRelTol", 0.4 );
+	useDeltaNormMax = ~isempty(deltaNormMax);
+	useOmegaModelMin = ~isempty(omegaModelMin);
+	if ( debugMode )
+		assert( isrealscalar(muReguCoeff) );
+		assert( 0.0 < muReguCoeff );
+		if ( useDeltaNormMax )
+			assert( isrealscalar(deltaNormMax) );
+			assert( 0.0 < deltaNormMax );
+			assert( isrealscalar(deltaNormMaxRelTol) );
+			assert( 0.0 < deltaNormMaxRelTol );
+			assert( deltaNormMaxRelTol < 1.0 );
+		endif
+		if ( useOmegaModelMin )
+			assert( isrealscalar(omegaModelMin) );
+			assert( isrealscalar(omegaModelMinRelTol) );
+			assert( 0.0 < omegaModelMinRelTol );
+			assert( omegaModelMinRelTol < 1.0 );
+		elseif
+	endif
 	%
 	%
 	% Set default return values.
@@ -32,7 +54,7 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 	%
 	%
 	% Handle "corner zero" cases.
-	if ( isrealscalar(omegaModelMin) )
+	if ( useOmegaModelMin )
 	if ( omegaModelMin >= omega0 )
 		msgif( debugMode, __FILE__, __LINE__, "Initial objective value is already below specified minimum." );
 		return;
@@ -43,14 +65,14 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 		return;
 	endif
 	if ( 0.0 == hNorm )
-		if ( isrealscalar(omegaModelMin) && isrealscalar(deltaNormMax) )
-			vecDelta = min([ (omega0-omegaModelMin)/gNormSq, deltaNormMax/gNorm ]) * (-vecG);
-		elseif ( isrealscalar(omeagMin) )
-			vecDelta = ((omega0-omegaModelMin)/gNormSq) * (-vecG);
-		elseif ( isrealscalar(deltaNormMax) )
-			vecDelta = (deltaNormMax/gNorm) * (-vecG);
+		if ( useDeltaNormMax && useOmegaModelMin  )
+			vecDelta = -min([ deltaNormMax/gNorm, (omega0-omegaModelMin)/gNormSq ]) * vecG;
+		elseif ( useDeltaNormMax )
+			vecDelta = -(deltaNormMax/gNorm) * vecG;
+		elseif ( useOmegaModelMin )
+			vecDelta = -((omega0-omegaModelMin)/gNormSq) * vecG;
 		else
-			msgif( debugMode, __FILE__, __LINE__, "Hessian is zero and there is no constraint on step." );
+			msgif( debugMode, __FILE__, __LINE__, "Hessian is zero, gradient is not, and there is no constraint on step." );
 		endif
 		return;
 	endif
@@ -63,7 +85,6 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 	[ matR, cholFlag ] = chol( matM );
 	if ( 0 ~= cholFlag )
 		msgif( debugMode, __FILE__, __LINE__, "Cholesky factorization with mu zero failed; trying with a small mu." );
-		muReguCoeff = 1e-5; % Arbitrary.
 		mu = muReguCoeff*hMaxAbs;
 		matM = matH + mu*matI;
 		[ matR, cholFlag ] = chol( matM );
@@ -77,16 +98,13 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 			mu = muCrit + muReguCoeff * ( muCrit + hNorm );
 			matM = matH + mu*matI;
 			matR = chol( matM );
-			cholFlag = 0;
 			%
 			clear matPsi_eig;
 			clear matLambda_eig;
 			clear muCrit;
 		endif
-		clear muReguCoeff;
 	endif
 	vecDelta = -( matR \ (matR'\vecG) );
-	omegaModel = omegaModel = omega0 + vecG'*vecDelta + 0.5*(vecDelta'*matH*vecDelta);
 	%
 	%
 	%
@@ -94,7 +112,7 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 	% As we apply a constraint and backtrack, keep track of previous value of mu (which doesn't satisfy the constraint)
 	% so we can forwardtrack later in case we overshot the constraint.
 	% We'll keep track of quantities that require some effort to calculate:
-	%  mu, matR, vecDelta, omegaModel.
+	%  mu, matR, vecDelta, and (late) omegaModel.
 	% We could track more, but, POITROME.
 	%
 	%
@@ -117,8 +135,7 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 			%
 			muLo = mu;
 			matR_muLo = matR;
-			vecDetla_muLo = vecDelta;
-			omegaModel_muLo = omegaModel;
+			vecDelta_muLo = vecDelta;
 			%
 			% Model: ||delta||^2 = ( a / (b+mu) )^2,
 			%  match ||delta||^2 and d/dmu(||delta||^2) at previous mu.
@@ -133,6 +150,7 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 			matM = matH + mu*matI;
 			matR = chol( matM );
 			vecDelta = -( matR \ ( matR' \ vecG ) );
+			assert( mu > muLo );
 			%
 			haveBTedForDeltaNormMax = true;
 			deltaNormTrgt = deltaNormMin; % Be more aggressive for subsequent iterations.
@@ -142,8 +160,9 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 	%
 	%
 	%
+	omegaModel = omegaModel = omega0 + vecG'*vecDelta + 0.5*(vecDelta'*matH*vecDelta);
 	haveBTedForOmegaModelMin = false;
-	if ( isrealscalar(omegaModelMin) )
+	if ( useOmegaModelMin )
 	if ( omegaModel < omegaModelMin )
 		% Since we apply omegaModelMin after deltaNormMax,
 		%  and, since we've gotten here,
@@ -167,15 +186,15 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 			vecDetla_muLo = vecDelta;
 			omegaModel_muLo = omegaModel;
 			%
-			% Use quadratic model...
-			error( "TO-DO!" );
 			% Model: omegaModel = omega0 - ( g^2 / (c+mu) ).
 			%  match omega at previous mu.
+			% Not a great model, but, should be good enough.
 			mu = mu + normGSq*(omegaTrgt-omegaModel)/((omega0-omegaTrgt)*(omega0-omegaModel));
 			matM = matH + mu*matI;
 			matR = chol( matM );
 			vecDelta = -( matR \ ( matR' \ vecG ) );
 			omegaModel = omega0 + vecG'*vecDelta + 0.5*(vecDelta'*matH*vecDelta);
+			assert( mu > muLo );
 			%
 			haveBTedForOmegaModelMin = true;
 			omegaTrgt = omegaModelMax; % Be more aggressive for subsequent iterations.
@@ -199,7 +218,7 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 			matR_muHi = matR
 			vecDelta_muHi = vecDelta;
 			omegaModel_muHi = omegaModel;
-			error( "TO-DO!" );
+			error( "Not implemented." );
 		endif
 	elseif (haveBTedForDeltaNormMax)
 		% mu should now satisfy deltaNormMax, and muLo should not.
@@ -212,7 +231,7 @@ function [ vecDelta, datOut ] = findLocMin_cnstH( omega0, vecG, matH, prm=[], da
 			matR_muHi = matR
 			vecDelta_muHi = vecDelta;
 			omegaModel_muHi = omegaModel;
-			error( "TO-DO!" );
+			error( "Not implemented." );
 		endif
 	endif
 return;
