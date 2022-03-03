b67754c7c4f9ae4d9c08efc6330d6abc449f8fa0
Plasma Dan <draburn@alumni.princeton.edu>
2022-03-03 13:31:51 -0800

    calcLesquj_basic: various fixes. Massive benefit to distance weights in this ex.

--- study/20220222findLocMin/0302lesquj/calcLesquj_basic.m
+++ study/20220222findLocMin/0302lesquj/calcLesquj_basic.m
@@ -13,19 +13,8 @@ function [ vecX0, vecF0, matJ0, datOut ] = calcLesquj_basic( vecXVals, vecFVals,
 		assert( isrealarray(vecFVals,[sizeF,numVals]) );
 	endif
 	%
-	if (0)
-	omegaVals = sumsq(vecFVals,1)/2.0;
-	[ sorted_omegaVals, sorted_indexVals ] = sort( omegaVals );
-	indexOfPt0 = sorted_indexVals(1);
-	vecX0 = vecXVals( :, indexOfPt0 );
-	vecF0 = vecFVals( :, indexOfPt0 );
-	% Trimming off "0", we'll use the term "pts" instead of "vals".
-	vecXPts = vecXVals( :, sorted_indexVals(2:end) );
-	vecFPts = vecFVals( :, sorted_indexVals(2:end) );
-	numPts = numVals - 1;
-	clear sorted_omegaVals;
-	clear sorted_indexVals;
-	else
+	useDistanceWeights = true;
+	%
 	[ foo, indexOfPt0 ] = min( sumsq(vecFVals,1) );
 	vecX0 = vecXVals( :, indexOfPt0 );
 	vecF0 = vecFVals( :, indexOfPt0 );
@@ -33,22 +22,24 @@ function [ vecX0, vecF0, matJ0, datOut ] = calcLesquj_basic( vecXVals, vecFVals,
 	vecXPts = [ vecXVals(:,1:indexOfPt0-1), vecXVals(:,indexOfPt0+1:end) ];
 	vecFPts = [ vecFVals(:,1:indexOfPt0-1), vecFVals(:,indexOfPt0+1:end) ];
 	numPts = numVals - 1;
-	endif
 	%
-	vecDeltaPts = vecXPts - vecX0; % sizeX x numPts
-	deltaNormSqPts = sumsq(vecDeltaPts,1);
-	minDeltaNormSq = min(deltaNormSqPts);
-	wDistPts = (2.0 * minDeltaNormSq) ./ ( minDeltaNormSq + deltaNormSqPts );
-	%
-	vecRhoPts = vecFPts - vecF0; % sizeX x numPts
-	if (0)
-	matA = vecDeltaPts*(vecDeltaPts'); % sizeX x sizeX
-	matY = vecDeltaPts*(vecRhoPts'); % sizeX x sizeF
+	if (useDistanceWeights)
+		vecDeltaPts = vecXPts - vecX0; % sizeX x numPts
+		deltaNormSqPts = sumsq(vecDeltaPts,1);
+		minDeltaNormSq = min(deltaNormSqPts);
+		wDistPts = (2.0 * minDeltaNormSq) ./ ( minDeltaNormSq + deltaNormSqPts );
+		wDistPts .^= 4;
+		vecRhoPts = vecFPts - vecF0;      % sizeX x numPts
+		foo = vecDeltaPts*diag(wDistPts); % sizeX x numPts
+		matA = foo*(vecDeltaPts');        % sizeX x sizeX
+		matY = foo*(vecRhoPts');          % sizeX x sizeF
 	else
-	foo = vecDeltaPts*diag(wDistPts); % sizeX x numPts
-	matA = foo*(vecDeltaPts');        % sizeX x sizeX
-	matY = foo*(vecRhoPts');          % sizeX x sizeF
+		vecDeltaPts = vecXPts - vecX0;     % sizeX x numPts
+		vecRhoPts = vecFPts - vecF0;       % sizeX x numPts
+		matA = vecDeltaPts*(vecDeltaPts'); % sizeX x sizeX
+		matY = vecDeltaPts*(vecRhoPts');   % sizeX x sizeF
 	endif
+	%
 	%
 	[ matR, cholFlag ] = chol( matA );
 	if ( 0~=cholFlag )
@@ -76,11 +67,14 @@ endfunction
 %!	vecF_secret = 0*randn(sizeF,1);
 %!	matJ_secret = randn(sizeF,sizeX);
 %!	matEta_secret = etaLevel*randn(sizeF,sizeX);
-%!	funchF = @(dummyX)( vecF_secret + matJ_secret*(dummyX-vecX_secret) + matEta_secret*((dummyX-vecX_secret).^2) );
+%!	funchF = @(dummyX)( vecF_secret + matJ_secret*(dummyX-vecX_secret) + matEta_secret*((dummyX-vecX_secret).^3) );
 %!	%
-%!	numVals = sizeX+2
+%!	numVals = sizeX+1;
 %!	vecXVals = randn(sizeX,numVals);
+%!	vecXVals = [ vecXVals, 10*randn(sizeX,2*numVals) ];
 %!	vecFVals = funchF(vecXVals) + sigmaLevel*randn(size(vecXVals));
+%!	numVals = size(vecXVals,2)
+%!	%plot( sumsq(vecFVals,1), 'o-' );
 %!	%
 %!	prm = [];
 %!	[ vecX0, vecF0, matJ0, datOut ] = calcLesquj_basic( vecXVals, vecFVals, prm );
@@ -89,7 +83,7 @@ endfunction
 %!	vecXRoot_basic = vecX0 - matJ0 \ vecF0;
 %!	vecXRoot_secret = vecX_secret - matJ_secret \ vecF_secret;
 %!	%
-%!	vecF0_basic = funchF(vecF0);
+%!	vecF0_basic = funchF( vecX0 );
 %!	vecFRoot_basic = funchF( vecXRoot_basic );
 %!	vecFRoot_secret = funchF( vecXRoot_secret );
 %!	%
