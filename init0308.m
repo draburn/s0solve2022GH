48d39d4db4fb777124e7dfff29ff8aeabe352198
Plasma Dan <draburn@alumni.princeton.edu>
2022-03-08 19:27:29 -0800

    .

---
+++ init0308.m
@@ -0,0 +1,17 @@
+page_screen_output(0)
+pkg load optim;
+addpath( "myutil" );
+%
+%my_editor = "/usr/bin/codeblocks";
+my_editor = "/usr/bin/mousepad";
+EDITOR( [my_editor ' %s'] ); % Moot if not USING_GUI; see options within GUI.
+%
+switch 10
+case 10
+	msg(__FILE__,__LINE__,"Adding path \"study/20220222findLocMin/testfunc2021/\".");
+	addpath("study/20220222findLocMin/testfunc2021/");
+	msg(__FILE__,__LINE__,"Adding path \"study/20220222findLocMin/0308baselineSolvers/\".");
+	addpath("study/20220222findLocMin/0308baselineSolvers/");
+otherwise
+	error( "Invalid case." );
+end
