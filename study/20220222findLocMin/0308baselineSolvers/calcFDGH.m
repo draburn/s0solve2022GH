726ba2806032839e3541fe3740a98efd0ed5718c
Plasma Dan <draburn@alumni.princeton.edu>
2022-03-10 20:13:46 -0800

    Adding calcFDGH (equiv earleir evalFDGH.)

---
+++ study/20220222findLocMin/0308baselineSolvers/calcFDGH.m
@@ -0,0 +1,44 @@
+% Function...
+function [ omega0, vecG0, matH0 ] = calcFDGH( vecX0, funchOmega, prm=[] )
+	sizeX = size(vecX0,1);
+	assert( isrealarray(vecX0,[sizeX,1]) );
+	%
+	omega0 = funchOmega(vecX0);
+	assert( isrealscalar(omega0) );
+	%
+	epsG = mygetfield( prm, "epsG", eps^0.4 );
+	assert( isrealscalar(epsG) );
+	assert( 0.0 < epsG );
+	vecG0 = zeros(sizeX,1);
+	for n=1:sizeX
+		vecXP = vecX0; vecXP(n) += epsG;
+		vecXM = vecX0; vecXM(n) -= epsG;
+		omegaP = funchOmega( vecXP );
+		omegaM = funchOmega( vecXM );
+		vecG0(n) = ( omegaP - omegaM ) / (2.0*epsG);
+	endfor
+	%
+	epsH = mygetfield( prm, "epsG", eps^0.2 );
+	assert( isrealscalar(epsH) );
+	assert( 0.0 < epsH );
+	matH0 = zeros(sizeX,sizeX);
+	for m=1:sizeX
+	for n=1:sizeX
+		vecXPP = vecX0; vecXPP(m) += epsH; vecXPP(n) += epsH;
+		vecXPM = vecX0; vecXPM(m) += epsH; vecXPM(n) -= epsH;
+		vecXMP = vecX0; vecXMP(m) -= epsH; vecXMP(n) += epsH;
+		vecXMM = vecX0; vecXMM(m) -= epsH; vecXMM(n) -= epsH;
+		omegaPP = funchOmega( vecXPP );
+		omegaPM = funchOmega( vecXPM );
+		omegaMP = funchOmega( vecXMP );
+		omegaMM = funchOmega( vecXMM );
+		matH0(m,n) = ( omegaPP - omegaPM - omegaMP + omegaMM ) / (4.0*epsH*epsH);
+	endfor
+	endfor
+return;
+end
+
+
+%!test
+%!	funchOmega = @(x)( x.^3 );
+%!	[ omega0, vecG0, matH0 ] = calcFDGH( 1.0, funchOmega )
