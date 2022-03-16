2b83bc3b6b49a8a4376831308aa19b4f29313895
Plasma Dan <draburn@alumni.princeton.edu>
2022-03-16 14:42:46 -0700

    .

--- study/20220222findLocMin/0314pieIsBased/findZero_baseline__curveViz.m
+++ study/20220222findLocMin/0314pieIsBased/findZero_baseline__curveViz.m
@@ -2,6 +2,7 @@ numFigs = 0;
 %
 numPts = 1001;
 pPts = linspace( 0.0, 1.0, numPts );
+pPts = 1.0 - ( 1.0 - pPts.^2).^2;
 vecDeltaPts = zeros(sizeX,numPts);
 vecFModelPts = zeros(sizeF,numPts);
 omegaHModelPts = zeros(1,numPts);
@@ -16,8 +17,9 @@ dPts = sqrt(sumsq(vecDeltaPts,1));
 omegaFModelPts = sumsq(vecFModelPts,1)/2.0;
 %
 %
-numPts2 = 31;
+numPts2 = 51;
 pPts2 = linspace( 0.0, 1.0, numPts2 );
+pPts2 = 1.0 - ( 1.0 - pPts2.^2).^2;
 vecFPts2 = zeros(sizeF,numPts2);
 for n=1:numPts2
 	vecDelta = funchDeltaOfP( pPts2(n) );
