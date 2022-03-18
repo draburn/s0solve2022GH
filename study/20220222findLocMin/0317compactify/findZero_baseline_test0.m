e018567d6a023d5612947d8a3e7d0c2b5761a9e4
Plasma Dan <draburn@alumni.princeton.edu>
2022-03-18 16:49:35 -0700

    .

--- study/20220222findLocMin/0317compactify/findZero_baseline_test0.m
+++ study/20220222findLocMin/0317compactify/findZero_baseline_test0.m
@@ -19,11 +19,17 @@
 	%
 	vecX0 = zeros(sizeX,1);
 	%
-	[ vecXF, vecFF, datOut ] = findZero_baseline( vecX0, funchF );
 	[ vecXF_fg, vecFF_fg, datOut_fg ] = findZero_fsolveGnostic( vecX0, funchF );
+	%
+	prm_broyd = [];
+	prm_broyd.modelGen_prm.useInexactJ = "true";
+	[ vecXF_broyd, vecFF_broyd, datOut_broyd ] = findZero_baseline( vecX0, funchF, prm_broyd );
+	%
+	[ vecXF, vecFF, datOut ] = findZero_baseline( vecX0, funchF );
 	%
 	numFigs++; figure( numFigs );
 	semilogy( ...
 	  datOut_fg.fevalCountVals, datOut_fg.fNormVals+eps, 'o-', 'markersize', 20, 'linewidth', 2, ...
-	  datOut.fevalCountVals, datOut.fNormVals+eps, 'x-', 'markersize', 20, 'linewidth', 2 );
+	  datOut_broyd.fevalCountVals, datOut_broyd.fNormVals+eps, 'x-', 'markersize', 20, 'linewidth', 2, ...
+	  datOut.fevalCountVals, datOut.fNormVals+eps, 'p-', 'markersize', 20, 'linewidth', 2 );
 	grid on;
