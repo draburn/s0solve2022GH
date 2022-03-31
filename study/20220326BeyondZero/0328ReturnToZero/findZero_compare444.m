e1b142c86e64337b9aaa24109079a0ee02827328
Plasma Dan <draburn@alumni.princeton.edu>
2022-03-31 01:43:30 -0700

    _compare444: Looked at cases where 800 stalls.

--- study/20220326BeyondZero/0328ReturnToZero/findZero_compare444.m
+++ study/20220326BeyondZero/0328ReturnToZero/findZero_compare444.m
@@ -1,9 +1,13 @@
 clear;
 setVerbLevs;
-setprngstates(0);
+%setprngstates(0);
 %setprngstates(90186240); %90186240 for 150x150. Some stall.
 %setprngstates(76252688); %76252688 for 150x150 causes div 0 in hack code in lisolf_directed.
-%setprngstates();
+%setprngstates(53244560); % 550 very slow; 880 okay.
+setprngstates(0); % 800 stalls but 550 does not.
+%setprngstates(9216528); % 800 stalls but 550 does not.
+%setprngstates(17374144); % 800 is weird but converges.
+%setprngstates(53858336); % 800 stalls but 550 does not.
 numFigs = 0;
 %
 sizeX = 150; sizeF = 150;
@@ -28,24 +32,34 @@ Df = jacobs( vecX0, funchF );
 msg( __FILE__, __LINE__, sprintf( "rcond(Df'*Df) = %0.3e.", rcond(Df'*Df) ) );
 %
 prm = [];
-prm.iterMax = 200;
-%
 timeSS = time();
 [ vecXF_444, vecFF_444, datOut_444 ] = findZero_444( vecX0, funchF, prm );
 time_444 = time()-timeSS;
 %
+%
+prm = [];
+prm.iterMax = 5;
+prm.iterMax = 200;
 timeSS = time();
 [ vecXF_550, vecFF_550, datOut_550 ] = findZero_550( vecX0, funchF, prm );
 time_550 = time()-timeSS;
 %
+%
+prm = [];
+prm.iterMax = 200;
 timeSS = time();
 [ vecXF_800, vecFF_800, datOut_800 ] = findZero_800( vecX0, funchF, prm );
 time_800 = time()-timeSS;
+%
+timeSS = time();
+[ vecXF_fsolve, vecFF_fsolve, datOut_fsolve, ] = findZero_fsolve( vecX0, funchF );
+time_fsolve = time()-timeSS;
 %
 %
 msg( __FILE__, __LINE__, sprintf( "norm(vecF0) = %g.", norm(vecF0) ) );
 msg( __FILE__, __LINE__, "Solver results..." );
 msg( __FILE__, __LINE__, sprintf( "  %15s:  %11s;  %11s;  %11s;  %11s.", "solver name", "||vecFF||", "iterCount", "fevalCount", "time(s)" ) );
+msg( __FILE__, __LINE__, sprintf( "  %15s:  %11.3e;  %11d;  %11d;  %11.3e.", "fsolve", norm(vecFF_fsolve), datOut_fsolve.iterCount, datOut_fsolve.fevalCount, time_fsolve ) );
 msg( __FILE__, __LINE__, sprintf( "  %15s:  %11.3e;  %11d;  %11d;  %11.3e.", "800", norm(vecFF_800), datOut_800.iterCount, datOut_800.fevalCount, time_800 ) );
 msg( __FILE__, __LINE__, sprintf( "  %15s:  %11.3e;  %11d;  %11d;  %11.3e.", "550", norm(vecFF_550), datOut_550.iterCount, datOut_550.fevalCount, time_550 ) );
 msg( __FILE__, __LINE__, sprintf( "  %15s:  %11.3e;  %11d;  %11d;  %11.3e.", "444", norm(vecFF_444), datOut_444.iterCount, datOut_444.fevalCount, time_444 ) );
