776f6fe6c6c135f33cc6945c6d7fa8be29c0dffb
Plasma Dan <draburn@alumni.princeton.edu>
2022-05-20 20:47:41 -0700

    npe100 looks fine in simple cases only.

--- study/20220326BeyondZero/0520NonPolyExpansion/npe100.m
+++ study/20220326BeyondZero/0520NonPolyExpansion/npe100.m
@@ -6,6 +6,7 @@
 clear;
 numFigs = 0;
 setprngstates(0); sizeX = 5; sizeF = sizeX; sizeB = sizeX; sxexp = 0.0; sfexp = 0.0; sbexp = 0.0; bexp = 0.0; jexp = 0.0; x0exp = 0.0;
+%setprngstates(0); sizeX = 100; sizeF = sizeX; sizeB = sizeX; sxexp = 2.0; sfexp = 1.0; sbexp = 1.0; bexp = 1.0; jexp = 1.0; x0exp = 1.0;
 %
 matSX = diag(exp(sxexp*randn(sizeX,1)));
 matSF = diag(exp(sfexp*randn(sizeF,1)));
@@ -20,28 +21,34 @@ matH = matJ'*matJ;
 matC = matB'*matB;
 hScale = norm(diag(matH));
 cScale = norm(diag(matC));
-muScale = hScale/cScale;
+s = hScale/cScale
+%s = 1.0e-4
 matR = chol( matH );
 bMax = norm( matB * ( matR \ ( matR' \ vecG ) ) );
+clear matR;
+%
+funchVuOfMu = @(mu)( mu + (mu>1.0).*( 2.0 - 1.0./mu - mu ) );
 %
 muSize = 101;
 foo = linspace( 1.0, 0.0, muSize );
 muVals = (1.0-foo.^2).^2;
 for n=1:muSize
-	matR = chol( matH + muVals(n)*muScale*matC );
+	matR = chol( matH + muVals(n)*s*matC );
 	bOfMuVals(n) = norm( matB * ( matR \ ( matR' \ vecG ) ) );
 endfor
+clear matR;
 nuSize = 101;
 foo = linspace( 1.0, 0.0, nuSize );
 nuVals = 1.0-(1.0-foo.^2).^2;
 for n=1:nuSize
-	matR = chol( nuVals(n)*matH + muScale*matC );
+	matR = chol( nuVals(n)*matH + s*matC );
 	bOfNuVals(n) = nuVals(n) * norm( matB * ( matR \ ( matR' \ vecG ) ) );
 endfor
+clear matR;
 %
-muValsAug = [ muVals(2:end), 1.0./nuVals(1:end-1) ];
-nuValsAug = [ 1.0./muVals(2:end), nuVals(1:end-1) ];
-vuValsAug = [ muVals(2:end), 2.0-nuVals(1:end-1) ];
+muPts = [ muVals(2:end), 1.0./nuVals(1:end-1) ]';
+nuPts = [ 1.0./muVals(2:end), nuVals(1:end-1) ]';
+vuPts = [ muVals(2:end), 2.0-nuVals(1:end-1) ]';
 %
 numFigs++; figure(numFigs);
 plot( ...
@@ -50,23 +57,77 @@ plot( ...
 grid on;
 %
 %
-%
+% Look at impact of alpha.
 mu0 = 0.5;
-matR = chol( matH + mu0*muScale*matC );
-vecY0 = matR\(matR'\(-vecG));
+matR0 = chol( matH + mu0*s*matC );
+vecY0 = matR0\(matR0'\(-vecG));
 b0 = norm(matB*vecY0);
-m = sumsq(matR\(matC*vecY0))/(b0^2);
-alpha = m;
+m = s*sumsq(matR0\(matC*vecY0))/(b0^2);
+xPts = muPts - mu0;
 %
-x = muValsAug - mu0;
-opax = 1.0 + alpha*x;
-bModelOfMuValsAug = b0*( (1.0./opax) + (alpha-m)*x./(opax.^2) );
-msk = isfinite(bModelOfMuValsAug)&(bModelOfMuValsAug>=0)&(bModelOfMuValsAug<=bMax);
+alphaVals = [ 0.0, 0.1*m, 0.4*m, 0.5*m, 0.6*m, 0.9*m, 1.0*m, 1.1*m ];
+sizeAlpha = length(alphaVals);
+for n=1:sizeAlpha
+	alpha = alphaVals(n);
+	opaxPts = 1.0 + alpha*xPts;
+	bModelPts = b0*( (1.0./opaxPts) + (alpha-m)*xPts./(opaxPts.^2) );
+	mskPts = isfinite(bModelPts)&(bModelPts>=0)&(bModelPts<=bMax);
+	%
+	bModelPtsOfAlphaVals(:,n) = bModelPts;
+	mskPtsOfAlphaVals(:,n) = mskPts;
+endfor
 %
 numFigs++; figure(numFigs);
 plot( ...
   muVals, bOfMuVals, 'o-', ...
-  2.0-nuVals, bOfNuVals, 'o-', ...
-  vuValsAug(msk), bModelOfMuValsAug(msk), 'x-' );
+  2.0-nuVals, bOfNuVals, 'o-' );
+hold on;
+for n=1:sizeAlpha
+	plot( vuPts(mskPtsOfAlphaVals(:,n)), bModelPtsOfAlphaVals(mskPtsOfAlphaVals(:,n),n), 'x-' );
+endfor
+hold off;
 grid on;
-
+%
+%
+% Pick alpha to match mu1...
+%
+numFigs++; figure(numFigs);
+plot( ...
+  muVals, bOfMuVals, 'o-', 'linewidth', 3, ...
+  2.0-nuVals, bOfNuVals, 'o-', 'linewidth', 3 );
+grid on;
+mu1 = 2.0;
+matR1 = chol( matH + mu1*s*matC );
+vecY1 = matR1\(matR1'\(-vecG));
+b1 = norm(matB*vecY1)
+f1 = b1/b0;
+x1 = mu1-mu0;
+c2 = f1*(x1^2);
+c1 = 2.0*x1*(f1-1.0);
+c0 = f1-1.0+(m*x1);
+discrim = c1^2 - 4.0*c0*c2
+if ( discrim >= 0.0 )
+	alphaP = ( -c1 + sqrt(discrim) ) / (2.0*c2)
+	alphaM = ( -c1 - sqrt(discrim) ) / (2.0*c2)
+	%
+	alpha = alphaP;
+	opaxPts = 1.0 + alpha*xPts;
+	bModelPts = b0*( (1.0./opaxPts) + (alpha-m)*xPts./(opaxPts.^2) );
+	mskPts = isfinite(bModelPts)&(bModelPts>=0)&(bModelPts<=bMax);
+	bModelPtsOfAlphaP = bModelPts;
+	mskPtsOfAlphaP = mskPts;
+	%
+	alpha = alphaM;
+	opaxPts = 1.0 + alpha*xPts;
+	bModelPts = b0*( (1.0./opaxPts) + (alpha-m)*xPts./(opaxPts.^2) );
+	mskPts = isfinite(bModelPts)&(bModelPts>=0)&(bModelPts<=bMax);
+	bModelPtsOfAlphaM = bModelPts;
+	mskPtsOfAlphaM = mskPts;
+	%
+	hold on;
+	plot( vuPts(mskPtsOfAlphaP), bModelPtsOfAlphaP(mskPtsOfAlphaP), 'p-', 'linewidth', 5 );
+	plot( vuPts(mskPtsOfAlphaM), bModelPtsOfAlphaM(mskPtsOfAlphaM), '-', 'linewidth', 4 );
+	plot( funchVuOfMu(mu0), b0, 'o', 'linewidth', 6, 'markersize', 15 );
+	plot( funchVuOfMu(mu1), b1, 'o', 'linewidth', 6, 'markersize', 15);
+	hold off;
+endif
