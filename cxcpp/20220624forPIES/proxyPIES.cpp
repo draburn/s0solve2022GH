4ca1bae970034af99596c7187383aaaeeb48d34a
Plasma Dan <draburn@alumni.princeton.edu>
2022-06-25 00:45:20 -0700

    .

--- cxcpp/20220624forPIES/proxyPIES.cpp
+++ cxcpp/20220624forPIES/proxyPIES.cpp
@@ -1,53 +1,120 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h> // For memset?!?!
 
 #define REAL_TYPE double
-#define REAL_TYPE_FORMAT_STR "%lf"
+//#define REAL_TYPE_FORMAT_STR "%lf"
 #define msg( str ) printf( "[%s.%d] %s\n", __FILE__, __LINE__, str )
 
 int main( void ) {
-	int retCode = -1;
-	int sz = 800;
 	msg( "Hello world!" );
-	FILE *fPtr = fopen( "x.txt", "r" );
-	if ( NULL == fPtr ) {
-		msg( "Failed to open input file." );
-		return __LINE__;
-	}
+	const char *const strInputFilename = "x.txt";
+	const char *const strOutputFilename = "f.txt";
+	const int sizeX = 800;
 	//
+	//
+	//
+	msg( "Attempting to allocate memory for vecX..." );
 	REAL_TYPE *vecX = NULL;
-	vecX = (REAL_TYPE *) malloc( sizeof(REAL_TYPE)*sz );
+	vecX = (REAL_TYPE *) malloc( sizeof(REAL_TYPE)*sizeX );
 	if ( NULL == vecX ) {
 		msg( "Memory allocation for vecX failed." );
 		return __LINE__;
 	}
+	memset( vecX, 0, sizeof(REAL_TYPE)*sizeX );
+	msg( "Successfully allocated memory for vecX." );
 	//
-	for ( int n = 0; n < sz; n++ ) {
-		int retVal = fscanf( fPtr, REAL_TYPE_FORMAT_STR, &(vecX[n]) );
-		//const char *const tempStr = sprintf( "Read vecX(%s) = %s\n", "%d", REAL_TYPE_FORMAT_STR );
-		//printf( tempStr, n, vecX[n] );
-		if ( 1 != retVal ) {
-			msg( "Failed to read next expected value from input file." );
-			return __LINE__;
-		}
-		//const char foo = sprintf( "Read vecX(%s) = %s.\n", "%d", REAL_TYPE_FORMAT_STR );
-		//printf( foo, n, vecX[n] );
-		//const char foo2 = sprintf( foo, n, vecX[n] );
-		//msg( foo2 );
-		printf( "Read vecX(%d) = %lf\n", n, vecX[n] );
+	const int sizeF = sizeX;
+	msg( "Attempting to allocate memory for vecF..." );
+	REAL_TYPE *vecF = NULL;
+	vecF = (REAL_TYPE *) malloc( sizeof(REAL_TYPE)*sizeF );
+	if ( NULL == vecF ) {
+		msg( "Memory allocation for vecF failed." );
+		return __LINE__;
 	}
+	memset( vecF, 0, sizeof(REAL_TYPE)*sizeF );
+	msg( "Successfully allocated memory for vecF." );
+	//
+	//
+	//
 	{
-		REAL_TYPE foo = 0;
-		int retVal = fscanf( fPtr, REAL_TYPE_FORMAT_STR, &foo );
-		if ( 0 < retVal ) {
-			msg( "Input file contains more data than expected." );
+		msg( "Attempting to open input file..." );
+		FILE *fPtr = fopen( strInputFilename, "r" );
+		if ( NULL == fPtr ) {
+			msg( "Failed to open input file." );
 			return __LINE__;
 		}
+		msg( "Successfully open input file." );
+		//
+		msg( "Reading data from input file..." );
+		for ( int n = 0; n < sizeX; n++ ) {
+			int numItemsRead = fscanf( fPtr, "%lf", &(vecX[n]) );
+			//const char *const tempStr = sprintf( "Read vecX(%s) = %s\n", "%d", REAL_TYPE_FORMAT_STR );
+			//printf( tempStr, n, vecX[n] );
+			if ( 1 != numItemsRead ) {
+				if ( 0 == n ) {
+					msg( "Failed to read first expected value from input file." );
+					return __LINE__;
+				}
+				msg( "Failed to read next expected value from input file." );
+				return __LINE__;
+			}
+			//const char foo = sprintf( "Read vecX(%s) = %s.\n", "%d", REAL_TYPE_FORMAT_STR );
+			//printf( foo, n, vecX[n] );
+			//const char foo2 = sprintf( foo, n, vecX[n] );
+			//msg( foo2 );
+			printf( "Read vecX(%d) = %lf\n", n, vecX[n] );
+		}
+		{
+			REAL_TYPE foo = 0;
+			int retVal = fscanf( fPtr, "%lf", &foo );
+			if ( 0 < retVal ) {
+				msg( "Input file contains more data than expected." );
+				return __LINE__;
+			}
+		}
+		msg( "Successfully read number of expected values from input file." );
+		msg( "Closing input file." );
+		fclose( fPtr );
 	}
-	msg( "Successfully read number of expected values from input file." );
-	msg( "Closing file." );
-	fclose( fPtr );
 	//
+	msg( "Calculating output..." );
+	for ( int n = 0; n < sizeF; n++ ) {
+		float x = ((double)n)*((double)sizeF)/(double(sizeX));
+		int m = (int)( x + 0.5 );
+		vecF[m] = (1.0+x)*vecX[n];
+		printf( "Set vecF(%d) = %lf\n", m, vecF[m] );
+	}
+	msg( "Finished calculating output." );//
+	//
+	{
+		msg( "Attempting to open output file..." );
+		FILE *fPtr = fopen( strOutputFilename, "w" );
+		if ( NULL == fPtr ) {
+			msg( "Failed to open output file." );
+			return __LINE__;
+		}
+		msg( "Successfully opened output file." );
+		//
+		msg( "Writing data to output file..." );
+		for ( int n = 0; n < sizeF; n++ ) {
+			int numCharsWritten = fprintf( fPtr, "%0.16lf\n", vecF[n] );
+			if ( 0 >= numCharsWritten ) {
+				if ( 0 == n ) {
+					msg( "Failed to write first value to output file." );
+					return __LINE__;
+				}
+				msg( "Failed to write next value to output file." );
+				return __LINE__;
+			}
+		}
+		//
+		msg( "Closing output file." );
+		fclose( fPtr );
+	}
+	//
+	msg( "Freeing vecF." );
+	free( vecF );
 	msg( "Freeing vecX." );
 	free( vecX );
 return 0;
