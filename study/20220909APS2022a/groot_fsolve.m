2529363410486833c7b1e792b807ba30fd5aeded
Plasma Dan <draburn@alumni.princeton.edu>
2022-09-13 17:42:47 -0700

    Using outputFcn to get cnvg data from fsolve.

--- study/20220909APS2022a/groot_fsolve.m
+++ study/20220909APS2022a/groot_fsolve.m
@@ -1,10 +1,10 @@
 function [ vecXBest, fevalCount, matCnvg, datOut ] = groot_fsolve( funchF, vecX0, fTol=1.0e-6, fallTol=1.0e-7, fevalLimit=1000, prm=[] )
-	mydefs;
+	%mydefs;
 	vecXBest = [];
 	fevalCount = 0;
 	matCnvg = [];
 	datOut = [];
-	prm.verbLev = mygetfield( prm, "verbLev", VERBLEV__FLAGGED );
+	%prm.verbLev = mygetfield( prm, "verbLev", VERBLEV__FLAGGED );
 	%
 	sizeX = size(vecX0,1);
 	assert( isrealarray(vecX0,[sizeX,1]) );
@@ -19,10 +19,11 @@ function [ vecXBest, fevalCount, matCnvg, datOut ] = groot_fsolve( funchF, vecX0
 	MaxIter = fevalLimit; % { 400; ... }
 	MaxFunEvals = fevalLimit; % { Inf; ... }
 	TolFun = fallTol; % { 1e-7; ... }
+
 	%
 	Jacobian = mygetfield( prm, "Jacobian", "off" ); % { "off"; ? }
 	TolX = mygetfield( prm, "TolX", 1e-7 ); % { 1e-7; ... }
-	OutputFcn = mygetfield( prm, "OutputFcn", [] ); % { []; ? }
+	OutputFcn = mygetfield( prm, "OutputFcn", @outfun__global_matCnvg ); % { []; ? }
 	Updating = mygetfield( prm, "Updating", "on" ); % { "on"; "off" } secant(/Broyden) update
 	FunValCheck = mygetfield( prm, "FunValCheck", "off" ); % { "off"; ? } check for NaN & Inf
 	ComplexEqn = mygetfield( prm, "ComplexEqn", "off" ); % { "off"; ? }
@@ -47,24 +48,23 @@ function [ vecXBest, fevalCount, matCnvg, datOut ] = groot_fsolve( funchF, vecX0
 	  "AutoScaling", AutoScaling  );
 	%
 	%
-	%
+	global global_matCnvg = [];
 	if ( includeFJAC )
 		[ fsolve_x, fsolve_fvec, fsolve_info, fsolve_output, fsolve_fjac ] = fsolve( funchF, vecX0, fsolve_options );
 	else
 		[ fsolve_x, fsolve_fvec, fsolve_info, fsolve_output ] = fsolve( funchF, vecX0, fsolve_options );
 		fsolve_fjac = [];
 	endif
+	matCnvg = global_matCnvg;
+	global_matCnvg = [];
 	%
 	vecXBest = fsolve_x;
 	fevalCount = fsolve_output.funcCount;
-	matCnvg = []; % Use "fsolveGnostic" if you want this.
 	%
 	datOut.fsolve_options = fsolve_options;
 	datOut.vecFF = fsolve_fvec;
 	datOut.fsovle_info = fsolve_info;
 	datOut.fsovle_output = fsolve_output;
 	datOut.matJF = fsolve_fjac;
-	%
-	%msg( __FILE__, __LINE__, "NOTE: Because fsolve() stops based on reduction in ||F||, we should call a second time if first time succedes!" );
 return;
 endfunction
