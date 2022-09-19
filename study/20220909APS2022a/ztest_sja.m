9703737c7c411d4c7cf339b212446350aa9eced9
Plasma Dan <draburn@alumni.princeton.edu>
2022-09-18 20:57:02 -0700

    ztest_sja: Example where scratch200 is worse.

--- study/20220909APS2022a/ztest_sja.m
+++ study/20220909APS2022a/ztest_sja.m
@@ -1,20 +1,21 @@
 clear;
 setprngstates(0);
-%sizeK = 3; sizeX = 5;
-sizeK = 10; sizeX = 100;
-sizeF = sizeX;
+%sizeK = 3; sizeX = 5; sizeF = 5;
+sizeK = 13; sizeX = 50; sizeF = 1;
+matV = utorthdrop( randn(sizeX,sizeK) );
 %
-%%%matJSecret = diag( (1:sizeX) );
 matJSecret = eye(sizeX);
 for nf=1:sizeF
+for m=1:3
 	nx = 1 + floor(sizeX*(1.0-100.0*eps)*rand);
 	matJSecret(nf,nx) += randn();
 endfor
-matV = utorthdrop( randn(sizeX,sizeK) );
+endfor
+matJSecret = matJSecret(1,:);
 matW = matJSecret * matV;
 %
 prm = [];
-prm.maxNumNZEPerRow = 9;
+prm.maxNumNZEPerRow = sizeK-1;
 tic();
 [ matJApprox100, datOut ] = sja_scratch100( matV, matW, prm );
 toc();
@@ -23,3 +24,8 @@ tic();
 toc();
 rd100 = reldiff( matJSecret, matJApprox100 )
 rd200 = reldiff( matJSecret, matJApprox200 )
+plot( ...
+  matJSecret, 'o-', ...
+  matJApprox100, 'x-', ...
+  matJApprox200, 's-' );
+grid on;
