0f4a80005b92305087d6f7c6f020320891602f16
Plasma Dan <draburn@alumni.princeton.edu>
2022-09-18 23:02:31 -0700

    sja_scratch300.m: implemented; suspect, as in this ztest_sja.m.

--- study/20220909APS2022a/sja_scratch300.m
+++ study/20220909APS2022a/sja_scratch300.m
@@ -1,4 +1,4 @@
-function [ matJ, datOut ] = sja_scratch200( matV, matW, prm=[] )
+function [ matJ, datOut ] = sja_scratch300( matV, matW, prm=[] )
 	mydefs;
 	sizeX = size(matV,1);
 	sizeF = size(matW,1);
@@ -34,165 +34,75 @@ function [ matJ, datOut ] = sja_scratch200( matV, matW, prm=[] )
 		res0 = norm(vecB);
 		%
 		doRowLoop = true;
+		minListSize = 0; % row loop iterations.
 		nzeList = [];
 		vecJ = zeros(sizeX,1);
 		vecBeta = vecB - matA * vecJ;
 		res = norm(vecBeta);
-		%
 		while (doRowLoop)
 			msgif( verbLev >= VERBLEV__COPIOUS, __FILE__, __LINE__, sprintf( "  %3d:  %10.3f,  %10.3f.", max(size(nzeList)), norm(vecJ), res ) );
 			%
-			%msg( __FILE__, __LINE__, "cemented + 1..." );
-			singlemenResVals = zeros(1,sizeX);
-			for trial_nx = 1 : sizeX
-				if ( ismember( trial_nx, nzeList ) )
-					% We below assume these singlemenResVals(nx) are left at zero.
-					continue;
-				endif
-				trial_nzeList = [ nzeList, trial_nx ];
+			% Look at [ pseudo-cemented, * ].
+			nzeMask = logical(zeros(1,sizeX));
+			nzeMask(nzeList) = true;
+			singlemenResVals = zeros(1,sizeX); % Code below counts on this suffic small for the nzeList elements.
+			for trial_nx = (1:sizeX)(~nzeMask)
 				trial_vecJ = zeros(sizeX,1);
-				trial_vecJ(trial_nzeList) = matA(:,trial_nzeList) \ vecB;
-				trial_vecBeta = vecB - matA * trial_vecJ;
-				trial_res = norm(trial_vecBeta);
-				if ( trial_res < tol*res0 )
-					msgif( verbLev >= VERBLEV__INFO, __FILE__, __LINE__, sprintf( ...
-					  "Row %d: (nx = %d) Short-circuit tol with NZE %d -> %d.", ...
-					  nf, trial_nx, max(size(nzeList)), max(size(trial_nzeList)) ) );
-					nzeList = trial_nzeList;
-					doRowLoop = false;
-					break;
-				endif
-				singlemenResVals(trial_nx) = trial_res;
+				trial_vecJ([nzeList,trial_nx]) = matA(:,[nzeList,trial_nx]) \ vecB;
+				singlemenResVals(trial_nx) = norm( vecB - matA * trial_vecJ );
 			endfor
-			if ( ~doRowLoop )
-				clear trial_*;
-				break;
-			endif
-			clear trial_*;
 			%
-			% Try short-circuit.
-			if ( 1 )
-				[ foo, trial_sorted ] = sort( singlemenResVals );
-				trial_nzeList = trial_sorted(1:maxNumNZEPerRow);
-				trial_vecJ = zeros(sizeX,1);
-				trial_vecJ(trial_nzeList) = matA(:,trial_nzeList) \ vecB;
-				trial_vecBeta = vecB - matA * trial_vecJ;
-				trial_res = norm(trial_vecBeta);
-				if ( trial_res < tol*res0 )
-					msgif( verbLev >= VERBLEV__INFO, __FILE__, __LINE__, sprintf( ...
-					  "Row %d: Short-circuit tol with NZE %d -> %d.", nf, max(size(nzeList)), max(size(trial_nzeList)) ) );
-					nzeList = trial_nzeList;
-					doRowLoop = false;
-					break;
-				endif
-			endif
-			clear trial_*;
-			%
-			% That didn't work.
-			% We'll probably need to cement one value.
-			% But, which?
-			% First, let's do a bit better than the singlemenResVals...
-			%msg( __FILE__, __LINE__, "cemented + best + 1..." );
+			% Check short-circuit for [ pseudo-cemented, several best per signlemen ].
 			[ foo, temp_sorted ] = sort( singlemenResVals );
+			temp_nzeList = temp_sorted(1:maxNumNZEPerRow);
+			temp_vecJ = zeros(sizeX,1);
+			temp_vecJ(temp_nzeList) = matA(:,temp_nzeList) \ vecB;
+			temp_res = norm( vecB - matA * temp_vecJ );
+			if ( temp_res < tol*res0 )
+				msgif( verbLev >= VERBLEV__INFO, __FILE__, __LINE__, sprintf( "Row %d: Reached tol with minListSize %d.", nf, minListSize ) );
+				nzeList = temp_nzeList;
+				doRowLoop = false; % Superfluous?
+				break;
+			endif
+			%
+			% Look at [ pseudo-cemented, several best per signlemen, * ].
+			[ foo, temp_sorted ] = sort( singlemenResVals ); % Assumes singlemenResVals(nzeList) is suffic small (0.0).
 			temp_nzeList = temp_sorted(1:maxNumNZEPerRow-1);
-			% temp_nzeList is probably pretty good, so let's try it...
-			% Let's loop over all elem and see if we can make it better.
-			best_nx = 0;
-			best_res = res0; % Any sufficiently large value is fine here.
-			for trial_nx = 1 : sizeX
-				if ( ismember( trial_nx, temp_nzeList ) )
-					continue;
-				endif
-				trial_nzeList = [ temp_nzeList, trial_nx ];
+			temp_nzeMask = logical(zeros(1,sizeX));
+			temp_nzeMask(temp_nzeList) = true;
+			multiemenResVals = res0 + zeros(1,sizeX); % Code below counts on this being suffic large for the temp_nzeList elements.
+			for trial_nx = (1:sizeX)(~temp_nzeMask)
 				trial_vecJ = zeros(sizeX,1);
-				trial_vecJ(trial_nzeList) = matA(:,trial_nzeList) \ vecB;
-				trial_vecBeta = vecB - matA * trial_vecJ;
-				trial_res = norm(trial_vecBeta);
-				if ( trial_res < tol*res0 )
-					msgif( verbLev >= VERBLEV__INFO, __FILE__, __LINE__, sprintf( ...
-					  "Row %d: (nx = %d) Short-circuit tol with NZE %d -> %d.", ...
-					  nf, trial_nx, max(size(nzeList)), max(size(trial_nzeList)) ) );
-					nzeList = trial_nzeList;
-					doRowLoop = false;
-					break;
-				endif
-				%
-				if ( trial_res < best_res )
-					best_nx = trial_nx;
-					best_nzeList = trial_nzeList;
-					best_vecJ = trial_vecJ;
-					best_vecBeta = trial_vecBeta;
-					best_res = trial_res;
-				endif
+				trial_vecJ([temp_nzeList,trial_nx]) = matA(:,[temp_nzeList,trial_nx]) \ vecB;
+				multiemenResVals(trial_nx) = norm( vecB - matA * trial_vecJ );
 			endfor
-			if ( ~doRowLoop )
-				clear trial_*;
-				clear best_*;
-				break;
-			endif
-			temp_nzeList = best_nzeList;
-			clear trial_*;
-			clear best_*;
-			%
-			%msg( __FILE__, __LINE__, "cemented + best - 1..." );
-			% Great, now we need to decide which to cement.
-			% The "best" is the worst one to drop.
-			best_nx = 0;
-			best_res = 0.0; % Any sufficiently small value is fine here.
-			for trial_nx = 1 : sizeX
-				if ( ismember( trial_nx, nzeList ) )
-					continue;
-				elseif ( ~ismember( trial_nx, temp_nzeList ) )
-					continue;
-				endif
-				trial_nzeList = temp_nzeList( temp_nzeList ~= trial_nx );
-				trial_vecJ = zeros(sizeX,1);
-				trial_vecJ(trial_nzeList) = matA(:,trial_nzeList) \ vecB;
-				trial_vecBeta = vecB - matA * trial_vecJ;
-				trial_res = norm(trial_vecBeta);
-				if ( trial_res < tol*res0 )
-					% Note: this short-circuit should be unreachable in the current code.
-					msgif( verbLev >= VERBLEV__INFO, __FILE__, __LINE__, sprintf( ...
-					  "Row %d: (nx = %d) Short-circuit tol with NZE %d -> %d.", ...
-					  nf, trial_nx, max(size(nzeList)), max(size(trial_nzeList)) ) );
-					nzeList = trial_nzeList;
-					doRowLoop = false;
-					break;
-				endif
-				%
-				if ( trial_res > best_res )
-					best_nx = trial_nx;
-					best_nzeList = trial_nzeList;
-					best_vecJ = trial_vecJ;
-					best_vecBeta = trial_vecBeta;
-					best_res = trial_res;
-				endif
-			endfor
-			if ( ~doRowLoop )
-				clear trial_*;
-				clear best_*;
-				break;
-			endif
-			% Finally, cement this one.
-			nzeList = [ nzeList, best_nx ];
-			vecJ = zeros(sizeX,1);
-			vecJ(nzeList) = matA(:,nzeList) \ vecB;
-			vecBeta = vecB - matA * vecJ;
-			res = norm(vecBeta);
-			clear trial_*;
-			clear best_*;
-			%
-			if ( res < tol*res0 )
-				msgif( verbLev >= VERBLEV__PROGRESS, __FILE__, __LINE__, sprintf( ...
-				  "Row %d: Reached tol with NZE %d.", nf, max(size(nzeList)) ) );
+			[ best_res, best_nx ] = min( multiemenResVals ); % Assumes singlemenResVals(nzeList) is suffic large (res0).
+			temp_nzeList = [ temp_nzeList, best_nx ];
+			if ( best_res < tol*res0 )
+				trial_nzeList = temp_nzeList;
+				msgif( verbLev >= VERBLEV__INFO, __FILE__, __LINE__, sprintf( "Row %d: Reached tol with minListSize %d.", nf, minListSize ) );
+				nzeList = trial_nzeList;
 				doRowLoop = false; % Superfluous?
 				break;
-			elseif ( max(size(nzeList)) >= maxNumNZEPerRow )
-				msgif( verbLev >= VERBLEV__PROGRESS, __FILE__, __LINE__, sprintf( ...
-				  "Row %d: Reached maxNumNZEPerRow with rel res %0.3e.", nf, res / res0 ) );
+			elseif ( minListSize >= maxNumNZEPerRow )
+				msgif( verbLev >= VERBLEV__PROGRESS, __FILE__, __LINE__, sprintf( "Row %d: Reached maxNumNZEPerRow with rel res %0.3e.", nf, res / res0 ) );
+				nzeList = temp_nzeList;
 				doRowLoop = false; % Superfluous?
 				break;
 			endif
+			%
+			% Look at which elements would be worst to drop.
+			% And pseudo-cement those as nzeList for the next iteration.
+			minListSize++;
+			dropResVals = best_res + zeros(1,minListSize);
+			for trial_nz = 1:minListSize
+				trial_vecJ = zeros(sizeX,1);
+				trial_vecJ(temp_nzeList([1:trial_nz-1,trial_nz+1:end])) = matA(:,temp_nzeList([1:trial_nz-1,trial_nz+1:end])) \ vecB;
+				dropResVals(trial_nz) = norm( vecB - matA * trial_vecJ );
+			endfor
+			[ foo, temp_sorted ] = sort( dropResVals, "descend" );
+			nzeList = temp_sorted(1:minListSize);
+			%
 			%msg( __FILE__, __LINE__, "Goodbye." );
 			%return;
 		endwhile
--- study/20220909APS2022a/ztest_sja.m
+++ study/20220909APS2022a/ztest_sja.m
@@ -1,31 +1,46 @@
 clear;
-setprngstates(0);
+setprngstates(58994832);
+numFigs = 0;
 %sizeK = 3; sizeX = 5; sizeF = 5;
-sizeK = 10; sizeX = 100; sizeF = 1;
+%sizeK = 10; sizeX = 100; sizeF = 1;
+sizeK = 7; sizeX = 10; sizeF = sizeX;
+%sizeK = 30; sizeX = 100; sizeF = sizeX;
 matV = utorthdrop( randn(sizeX,sizeK) );
 %
-matJSecret = eye(sizeX);
+matJSecret = zeros(sizeX);
 for nf=1:sizeF
 for m=1:3
 	nx = 1 + floor(sizeX*(1.0-100.0*eps)*rand);
 	matJSecret(nf,nx) += randn();
 endfor
 endfor
-matJSecret = matJSecret(1,:);
 matW = matJSecret * matV;
 %
 prm = [];
 prm.maxNumNZEPerRow = floor( sizeK - sqrt(sizeK) );
-tic();
-[ matJApprox100, datOut ] = sja_scratch100( matV, matW, prm );
-toc();
-tic();
-[ matJApprox200, datOut ] = sja_scratch200( matV, matW, prm );
-toc();
-rd100 = reldiff( matJSecret, matJApprox100 )
-rd200 = reldiff( matJSecret, matJApprox200 )
+time0 = time(); [ matJApprox300, datOut ] = sja_scratch300( matV, matW, prm ); time300 = time()-time0;
+time0 = time(); [ matJApprox200, datOut ] = sja_scratch200( matV, matW, prm ); time200 = time()-time0;
+time0 = time(); [ matJApprox100, datOut ] = sja_scratch100( matV, matW, prm ); time100 = time()-time0;
+rd100 = reldiff( matJSecret, matJApprox100 );
+rd200 = reldiff( matJSecret, matJApprox200 );
+rd300 = reldiff( matJSecret, matJApprox300 );
+msg( __FILE__, __LINE__, sprintf( " sja_scratch100() produced %0.3e in %0.3f seconds.", rd100, time100 ) );
+msg( __FILE__, __LINE__, sprintf( " sja_scratch200() produced %0.3e in %0.3f seconds.", rd200, time200 ) );
+msg( __FILE__, __LINE__, sprintf( " sja_scratch300() produced %0.3e in %0.3f seconds.", rd300, time300 ) );
+%
+numFigs++; figure(numFigs);
 plot( ...
-  matJSecret, 'o-', ...
-  matJApprox100, 'x-', ...
-  matJApprox200, 's-' );
+  sumsq((matJApprox100-matJSecret)'), 'x-', ...
+  sumsq((matJApprox200-matJSecret)'), 's-', ...
+  sumsq((matJApprox300-matJSecret)'), '^-' );
+grid on;
+%
+numFigs++; figure(numFigs);
+n = 1;
+n = 1;
+plot( ...
+  matJApprox100(:,n), 'x-', ...
+  matJApprox200(:,n), 's-', ...
+  matJApprox300(:,n), '^-', ...
+  matJSecret(:,n), 'o-' );
 grid on;
