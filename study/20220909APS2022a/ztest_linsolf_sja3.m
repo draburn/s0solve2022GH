a6e7d31a49ee1249a6b53f1e358ad25bf0bab741
Plasma Dan <draburn@alumni.princeton.edu>
2022-09-21 22:16:43 -0700

    ztest_linsolf_sja3: sja_fast may be working (may not be fast).

---
+++ study/20220909APS2022a/ztest_linsolf_sja3.m
@@ -0,0 +1,28 @@
+clear;
+mydefs;
+setprngstates(20578384);
+numFigs = 0;
+sizeX = 100
+sizeF = 100
+sizeK = 90
+sizeL = 3%ceil(sizeK/2.0)
+%
+matV = randn(sizeX,sizeK);
+matJSecret = eye(sizeF,sizeX);
+for nf=1:sizeF
+for m=1:sizeL-1
+	nx = 1 + floor(sizeX*(1.0-100.0*eps)*rand);
+	nzeListSecret(m) = nx;
+	matJSecret(nf,nx) += randn();
+endfor
+endfor
+%
+%matJSecret
+matV = utorthdrop( matV );
+matW = matJSecret*matV;
+%
+prm = [];
+%prm.maxNumNZEPerRow = 5;
+%tic(); [ matJA, datOut ] = sja_scratch350( matV, matW, prm ); toc(); rd = reldiff( matJSecret, matJA )
+tic(); [ matJA, datOut ] = sja_basic( matV, matW, prm ); toc(); rd = reldiff( matJSecret, matJA )
+tic(); [ matJA, datOut ] = sja_fast( matV, matW, prm ); toc(); rd = reldiff( matJSecret, matJA )
