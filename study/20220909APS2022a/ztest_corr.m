ead0a4334731bb45239ed0f15c2d18a7512afb48
Plasma Dan <draburn@alumni.princeton.edu>
2022-09-22 02:59:18 -0700

    Realized corr scaling could consider multiple rows, but moving on.

--- study/20220909APS2022a/ztest_corr.m
+++ study/20220909APS2022a/ztest_corr.m
@@ -3,6 +3,13 @@ mydefs;
 numFigs = 0;
 if (1)
 	setprngstates(0);
+	sizeX = 10
+	sizeF = 8
+	sizeK = 6
+	sizeL = 4
+	
+elseif (1)
+	setprngstates(0);
 	sizeX = 5
 	sizeF = 4
 	sizeK = 3
@@ -25,7 +32,8 @@ for l=1:sizeL
 endfor
 endfor
 %
-matV(:,1) = 0.0; matV(2,1) = 1.0;
+%matV(:,1) = 0.0;
+matV(8,1) = 10.0;
 %matV(:,1) = 1.0;
 matV = utorthdrop( matV );
 matW = matJSecret*matV;
@@ -42,7 +50,7 @@ matChi1 = zeros(sizeF,sizeX);
 matEtaS1 = zeros(sizeF,sizeX);
 matChiS1 = zeros(sizeF,sizeX);
 %matEtaSX1 = zeros(sizeF,sizeX);
-matChiSX1 = zeros(sizeF,sizeX);
+%matChiSX1 = zeros(sizeF,sizeX); Moot now that I knwo how to calc matChiS.
 for m=1:sizeF
 for n=1:sizeX
 	sumWV = 0.0;
@@ -69,15 +77,15 @@ for n=1:sizeX
 	matChi1(m,n) =  (sumWV^2)/((eps+sumVV)*(eps+sumWW));
 	matEtaS1(m,n) = sumWVS /(eps+sumVVS);
 	matChiS1(m,n) =  (sumWVS^2)/((eps+sumVVS)*(eps+sumWWS));
-	matChiSX1(m,n) = (sumWVS^2)/((eps+sumVVSS)*(eps+sumWW));
+	%matChiSX1(m,n) = (sumWVS^2)/((eps+sumVVSS)*(eps+sumWW));
 endfor
 endfor
 %matOMP1
-matEta1
+matEta1Sq = matEta1.^2
 matChi1
-matEtaS1
+%matEtaS1
 matChiS1
-matChiSX1
+%matChiSX1
 %
 matEta2 = (matW * (matV')) * diag(1.0./(eps+sumsq(matV,2)));
 matChi2 = diag(1.0./(eps+sumsq(matW,2))) * ((matW * (matV')).^ 2) * diag(1.0./(eps+sumsq(matV,2)));
@@ -86,22 +94,37 @@ matChi2 = diag(1.0./(eps+sumsq(matW,2))) * ((matW * (matV')).^ 2) * diag(1.0./(e
 matFoo_wvs = matW * (( matV.*matS )');
 matFoo_vvsInv = diag( 1.0 ./ (eps+sum( (matV.^2).*matS, 2 )) );
 matFoo_wws = (matW.^2) * (matS');
-matChiS2 = ((matFoo_wvs.^2)*matFoo_vvsInv) ./ (eps+matFoo_wws)
+matChiS2 = ((matFoo_wvs.^2)*matFoo_vvsInv) ./ (eps+matFoo_wws);
 
-matChiSX2 = diag(1.0./(eps+sumsq(matW,2))) * ((matW * ((matV.*matS)')).^ 2) * diag(1.0./(eps+sumsq(matV.*matS,2)));
+%matChiSX2 = diag(1.0./(eps+sumsq(matW,2))) * ((matW * ((matV.*matS)')).^ 2) * diag(1.0./(eps+sumsq(matV.*matS,2)));
 %
 rdEta = reldiff( matEta1, matEta2 )
 rdChi = reldiff( matChi1, matChi2 )
-%rdChiS = reldiff( matChiS1, matChiS2 )
-rdChiSX = reldiff( matChiSX1, matChiSX2 )
+rdChiS = reldiff( matChiS1, matChiS2 )
+%rdChiSX = reldiff( matChiSX1, matChiSX2 )
 %rdSX = reldiff( matChi1, matChiSX2 )
 %rdX = reldiff( matChiS1, matChiSX2 )
 
 numFigs++; figure(numFigs);
-imagesc(matChi1);
+matEta1Scl = diag(1./max(matEta1'.^2))*(matEta1.^2);
+image(64*matEta1Scl);
+colormap(hot(64));
+
+numFigs++; figure(numFigs);
+image(64*matChi1);
+colormap(hot(64));
 %
 numFigs++; figure(numFigs);
-imagesc(matChiS1);
+image(64*matChiS1);
+colormap(hot(64));
+%
+%numFigs++; figure(numFigs);
+%imagesc(matChiSX1);
+
+%matJSSq = matJSecret.^2;
+%matJSSqScl = diag(1./max(matJSSq'))*matJSSq
 %
 numFigs++; figure(numFigs);
-imagesc(matChiSX1);
+%image(64*(matJSSqScl.^0.25));
+image(64*double(matJSecret~=0.0));
+colormap(hot(64));
