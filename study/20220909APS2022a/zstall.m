2f88dd4cae7028675f7e03a4e4b45f930d331613
Plasma Dan <draburn@alumni.princeton.edu>
2022-10-16 15:35:14 -0700

    Added zstall.m based on zrun.m.

---
+++ study/20220909APS2022a/zstall.m
@@ -0,0 +1,34 @@
+clear
+mydefs;
+printf("\n\n");
+%
+% SET PRM.
+seedTime = mod( round(now*1E11), 1E8 ); % In case you want this.
+probSetPrm = [];
+probSetPrm.probType = "aps2022simplesparse";
+probSetPrm.numProbs = 10;
+probSetPrm.numUnknowns = 50;
+probSetPrm.setSeed = 0;
+%
+n = 0;
+n++; a.s(n).f = @groot_fsolve;
+%n++; a.s(n).f = @groot_jfnk_basic;
+%n++; a.s(n).f = @groot_jfnk_convent;
+%n++; a.s(n).f = @groot_jfnk_baseline;
+n++; a.s(n).f = @groot_jfnk_sja;
+%n++; a.s(n).f = @groot_jfnk_sja_looptr;
+%n++; a.s(n).f = @groot_jfnk_sja; a.s(n).p.linsolfPrm.sjaMethod = "omp";
+%
+% Note: zapsrun__start converts "a" to "algoSetPrm".
+zrun__start;
+default_solverPrm.linsolfPrm.tol = 0.01;
+default_solverPrm.fevalLimit = 1000;
+%
+% DO WORK.
+zcompPrm.probRunList = 9;
+zcdo = zcomp( probSetPrm, algoSetPrm, default_solverPrm, zcompPrm );
+%
+zrun__finish; % Does nothing?
+%
+msg( __FILE__, __LINE__, "Goodbye." );
+printf("\n\n");
