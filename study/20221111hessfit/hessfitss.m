76ad393e547e201be7198f90d47187decf892fdc
Plasma Dan <draburn@alumni.princeton.edu>
2022-11-14 18:28:14 -0800

    hessfitss: set vecX0 correctly. Seeing saddle point.

--- study/20221111hessfit/hessfitss.m
+++ study/20221111hessfit/hessfitss.m
@@ -6,7 +6,7 @@
 % DRaburn 2022-11-11.
 
 function [ matV, fss, vecGss, matHss, datOut ] = hessfitss( sizeX, numPts, matX, rvecF, matG, prm=[] )
-	[ matV, matDX, pOfMin ] = __init( sizeX, numPts, matX, rvecF, matG, prm );
+	[ matV, matDX, pt0 ] = __init( sizeX, numPts, matX, rvecF, matG, prm );
 	sizeV = size(matV,2)
 	%
 	datOut = [];
@@ -14,16 +14,19 @@ function [ matV, fss, vecGss, matHss, datOut ] = hessfitss( sizeX, numPts, matX,
 	matVTG = matV' * matG;
 	%
 	hessfitPrm = [];
-	hessfitPrm.f0 = rvecF(pOfMin);
+	hessfitPrm.f0 = rvecF(pt0);
 	hessfitPrm.useCnstF = true;
-	hessfitPrm.vecG0 = matVTG(:,pOfMin);
+	hessfitPrm.vecG0 = matVTG(:,pt0);
+	hessfitPrm.rvecW0 = sum( matY.^2, 1 ) ./ ( eps + sum( matDX.^2, 1) );
+	hessfitPrm.vecW0(pt0) = 1.0;
+	hessfitPrm.rvecW1 = hessfitPrm.rvecW0 * 0.01 / sizeX;
 	hessfitPrm = mygetfield( prm, "hessfitPrm", hessfitPrm );
-	[ fss, vecGss, matHss, hessfitDat ] = hessfit( sizeV, numPts, matY, rvecF, matVTG, hessfitPrm )
+	[ fss, vecGss, matHss, hessfitDat ] = hessfit( sizeV, numPts, matY, rvecF, matVTG, hessfitPrm );
 return;
 endfunction
 
 
-function [ matV, matDX, pOfMin ] = __init( sizeX, numPts, matX, rvecF, matG, prm=[] )
+function [ matV, matDX, pt0 ] = __init( sizeX, numPts, matX, rvecF, matG, prm=[] )
 	assert( isposintscalar(sizeX) );
 	assert( isposintscalar(numPts) );
 	assert( 2 <= numPts );
@@ -31,12 +34,16 @@ function [ matV, matDX, pOfMin ] = __init( sizeX, numPts, matX, rvecF, matG, prm
 	assert( isrealarray(rvecF,[1,numPts]) );
 	assert( isrealarray(matG,[sizeX,numPts]) );
 	%
-	[ fMin, pOfMin ] = min ( rvecF );
-	pOfMin
-	norm(fMin)
+	pt0 = mygetfield( prm, "pt0", [] );
+	if (isempty(pt0))
+		[ fMin, pt0 ] = min ( rvecF );
+		clear fMin;
+	endif
+	assert( isposintscalar(pt0) );
+	assert( pt0 <= numPts );
 	%
-	prm.vecX0 = matX(:,pOfMin);
-	[ matV, matDX ] = genbasis( matX, prm );
+	genbasisPrm.vecX0 = matX(:,pt0);
+	[ matV, matDX ] = genbasis( matX, genbasisPrm );
 	sizeK = size(matV,2);
 	assert( isrealarray(matV,[sizeX,sizeK]) );
 return;
--- study/20221111hessfit/test_hessfitss.m
+++ study/20221111hessfit/test_hessfitss.m
@@ -2,8 +2,8 @@ clear
 mydefs;
 setprngstates( 0 );
 %
-sizeX = 10
-numPts = 51
+sizeX = 5
+numPts = 11
 %numPts = 200
 numUnk = (sizeX*(sizeX+1))/2 + sizeX + 1
 numGvn = (1+sizeX)*numPts
@@ -20,18 +20,25 @@ secret_matDX = matX - secret_vecX0;
 rvecF = secret_f0 + sum( secret_matDX .* ( secret_matH * secret_matDX ), 1 )/2.0;
 matG = secret_matH * secret_matDX;
 %
-matG += 0.1*randn(size(matG));
+%%%matG += 0.1*randn(size(matG));
+
+[ f_minWas, pt0 ] = min(rvecF);
 %
 prm = [];
+prm.pt0 = pt0;
 msg( __FILE__, __LINE__, "Calling hessfitss()..." );
 tic();
-[ matV, fss, vecGss, matHss, hessfitssDat ] = hessfitss( sizeX, numPts, matX, rvecF, matG, prm );
+[ matV, fss, vecGss, matHss, hessfitssDat ] = hessfitss( sizeX, numPts, matX, rvecF, matG, pt0, prm );
+eigsOfMatHss = eig(matHss)
 toc();
-calc_vecX0ss = matHss\vecGss;
-calc_vecX0 = -matV*calc_vecX0ss;
-secret_vecDX0 = calc_vecX0 - secret_vecX0;
-f_minWas = min(rvecF)
-f_expect = fss + (vecGss'*calc_vecX0ss) + (calc_vecX0ss'*matHss*calc_vecX0ss)/2.0
+vecYNewton = -(matHss\vecGss);
+vecXNext = matX(:,pt0) + matV * vecYNewton;
+secret_vecDX0 = vecXNext - secret_vecX0;
+f_minWas = f_minWas
+vecYTest = -0.001*vecGss;
+f_test = fss + ( vecGss'*vecYTest ) + ( vecYTest'*matHss*vecYTest )/2.0
+f_expect = fss + ( vecGss'*vecYNewton ) + ( vecYNewton'*matHss*vecYNewton )/2.0
+%vecNablaYF_expect = vecGss + matHss*vecYNewton
 f_wouldBe = secret_f0 + (secret_vecDX0'*secret_matH*secret_vecDX0)/2.0
 %
 msg( __FILE__, __LINE__, "End of test." ); return;
