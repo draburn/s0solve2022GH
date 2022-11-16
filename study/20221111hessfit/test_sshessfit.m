0d4ad3b7052f45d29e26feaba8e69bc66ca3c34b
Plasma Dan <draburn@alumni.princeton.edu>
2022-11-15 22:57:19 -0800

    sshessfit: put vecX0 further away; results now make sense. Ultimate efficacy in question.

--- study/20221111hessfit/test_sshessfit.m
+++ study/20221111hessfit/test_sshessfit.m
@@ -2,21 +2,21 @@ clear
 mydefs;
 setprngstates( 0 );
 %
-sizeX = 100
-numPts = 5
+sizeX = 1000
+numPts = 10
 numUnk = (sizeX*(sizeX+1))/2 + sizeX + 1
 numGvn = (1+sizeX)*numPts
 %
 secret_f0 = 0.0;
-secret_vecX0 = randn(sizeX,1);
+secret_vecX0 = 1000.0*randn(sizeX,1);
 secret_matH = randn(sizeX,sizeX);
 secret_matH = secret_matH'*secret_matH;
 funchF = @(x)( secret_f0 + 0.5*sum((x-secret_vecX0).*( secret_matH * (x-secret_vecX0) ),1) );
 funchG = @(x)( secret_matH*(x-secret_vecX0) );
 %
-%matX = randn(sizeX,numPts);
+matX = randn(sizeX,numPts);
 %matX = full(eye(sizeX,numPts));
-matX = randn(sizeX,numPts); matX(1,:) += 100.0; matX(2,2:end) += 100.0;
+%matX = randn(sizeX,numPts); matX(1,:) += 100.0; matX(2,2:end) += 100.0;
 rvecF = funchF(matX);
 matG = funchG(matX);
 %
@@ -28,9 +28,13 @@ prm.useCnstF = true;
 prm.f0 = rvecF(pt0); % Only relevant for hessfit()?
 %prm.useCnstG = true;
 %prm.vecG0 = matG(:,pt0); % Only relevant for hessfit()?
-%[ vecX0, matV, fss, vecGss, matHss, datOut ] = sshessfit1115_nonorth( sizeX, numPts, matX, rvecF, matG, pt0, prm );
+msg( __FILE__, __LINE__, "Generating fit..." );
+tic();
+[ vecX0, matV, fss, vecGss, matHss, datOut ] = sshessfit1115_nonorth( sizeX, numPts, matX, rvecF, matG, pt0, prm );
 %[ vecX0, matV, fss, vecGss, matHss, datOut ] = sshessfit1115_orth( sizeX, numPts, matX, rvecF, matG, pt0, prm );
-[ fss, vecGss, matHss, datOut ] = hessfit( sizeX, numPts, matX, rvecF, matG, prm ); vecX0 = zeros(sizeX,1); matV = eye(sizeX);
+%[ fss, vecGss, matHss, datOut ] = hessfit( sizeX, numPts, matX, rvecF, matG, prm ); vecX0 = zeros(sizeX,1); matV = eye(sizeX);
+msgnnl( __FILE__, __LINE__, "Finished generating fit. " );
+toc();
 %
 eigVals = eig(matHss);
 eigMin = min(eigVals)
