256d63dd72eac80068f9d7bc05c6e5285aa45e2b
Plasma Dan <draburn@alumni.princeton.edu>
2022-11-15 23:54:23 -0800

    test_sshessfit & non-orthogonalized: Heck'n yeah!

--- study/20221111hessfit/test_sshessfit.m
+++ study/20221111hessfit/test_sshessfit.m
@@ -2,15 +2,18 @@ clear
 mydefs;
 setprngstates( 0 );
 %
-sizeX = 1000
-numPts = 10
+sizeX1 = 1E1;
+sizeX = 2E4
+numPts = 1E2
 numUnk = (sizeX*(sizeX+1))/2 + sizeX + 1
 numGvn = (1+sizeX)*numPts
 %
 secret_f0 = 0.0;
 secret_vecX0 = 1000.0*randn(sizeX,1);
-secret_matH = randn(sizeX,sizeX);
-secret_matH = secret_matH'*secret_matH;
+msg( __FILE__, __LINE__, "Generating Hessian..." ); tic();
+%%%secret_matH = randn(sizeX,sizeX); secret_matH = secret_matH'*secret_matH;
+secret_matH = randn(sizeX1,sizeX); secret_matH = secret_matH'*secret_matH;
+msgnnl( __FILE__, __LINE__, "Finished generating Hessian. " ); toc();
 funchF = @(x)( secret_f0 + 0.5*sum((x-secret_vecX0).*( secret_matH * (x-secret_vecX0) ),1) );
 funchG = @(x)( secret_matH*(x-secret_vecX0) );
 %
@@ -28,13 +31,11 @@ prm.useCnstF = true;
 prm.f0 = rvecF(pt0); % Only relevant for hessfit()?
 %prm.useCnstG = true;
 %prm.vecG0 = matG(:,pt0); % Only relevant for hessfit()?
-msg( __FILE__, __LINE__, "Generating fit..." );
-tic();
+msg( __FILE__, __LINE__, "Generating fit..." ); tic();
 [ vecX0, matV, fss, vecGss, matHss, datOut ] = sshessfit1115_nonorth( sizeX, numPts, matX, rvecF, matG, pt0, prm );
 %[ vecX0, matV, fss, vecGss, matHss, datOut ] = sshessfit1115_orth( sizeX, numPts, matX, rvecF, matG, pt0, prm );
 %[ fss, vecGss, matHss, datOut ] = hessfit( sizeX, numPts, matX, rvecF, matG, prm ); vecX0 = zeros(sizeX,1); matV = eye(sizeX);
-msgnnl( __FILE__, __LINE__, "Finished generating fit. " );
-toc();
+msgnnl( __FILE__, __LINE__, "Finished generating fit. " ); toc();
 %
 eigVals = eig(matHss);
 eigMin = min(eigVals)
@@ -46,5 +47,6 @@ vecXNext = vecX0 + matV*vecDeltaY;
 fNext = funchF(vecXNext)
 %vecGNext = funchG(vecXNext)
 %vecVTGNext = matV'*vecGNext
+fallFrac = 1.0 - fNext/f_minWas
 
 return;
