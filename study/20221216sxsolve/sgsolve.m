c7c16e7adca0f2170bb02f6cacb13a411e5b54c6
Plasma Dan <draburn@alumni.princeton.edu>
2023-01-04 20:19:04 -0800

    sgsolve.m: __jump_januarySeedy() cleanup, before changes.

--- study/20221216sxsolve/sgsolve.m
+++ study/20221216sxsolve/sgsolve.m
@@ -1158,14 +1158,11 @@ function [ vecXNew, vecPNew, jumpDat ] = __jump_januarySeedy( vecXSeed, vecPSeed
 	[ fAnchor, indexAnchor ] = min(rvecF);
 	vecXAnchor = matX(:,indexAnchor);
 	vecGAnchor = matG(:,indexAnchor);
-	
-	% Fit is changed from original __jump_basicCts().
 	%
 	% Generate subspace basis matrix and drop columns.
 	matD = matX - vecXAnchor; % Anchor column will necessarily be dropped.
-	%
-	% SKIP - Drop BC magnitude of columns of matD.
-	%
+	% We could consider dropping columns because of the magnitude of the corresponding vector of matD is too small.
+	% This is done in <some earlier __jump version>.
 	basisDropThresh = mygetfield( prm, "basisDropThresh", 0.01 );
 	[ matV, rvecDrop ] = utorthdrop( matD, basisDropThresh );
 	jumpDat.sizeK = size(matV,2);
@@ -1189,41 +1186,13 @@ function [ vecXNew, vecPNew, jumpDat ] = __jump_januarySeedy( vecXSeed, vecPSeed
 	vecGammaFit = vecGammaAnchor;
 	matA = (matY') \ (( matGamma - vecGammaAnchor)');
 	matHFit = (matA'+matA)/2.0;
-	% DRaburn 2023-1-04:
-	% Alternatives approaches to matHFit are possible,
-	%  as, considering rvecF, we have two measures of every element of matHFit.
-	% However, my attempts did not work particularly well and added to the time.
-	% See hessfit_simple_posdefy.m, for example.
-	
-	doComparison = false;
-	if (doComparison)
-		[ fTrue, vecGTrue ] = prm.funchFG_noiseless( vecXAnchor );
-		vecGammaTrue = matV'*vecGTrue;
-		matVTHFSV = matV'*prm.matHSecret*matV;
-		rdH = reldiff( matHFit, matVTHFSV );
-		rdG = reldiff( vecGammaFit, vecGammaTrue );
-		rdF = reldiff( fFit, fTrue );
-		%
-		dumpInfo = true;
-		if (dumpInfo)
-			matHFit
-			matVTHFSV
-			matRes = matHFit - matVTHFSV
-			vecGammaCompare = [ vecGammaFit, vecGammaTrue, vecGammaFit - vecGammaTrue ]
-			fCompare = [ fFit, fTrue, fFit - fTrue ]
-			rdVals = [ rdF, rdG, rdH ]
-		endif
-		if ( rdH > 0.1 )
-			error( "*** THE BAD THING HAPPENED ***" );
-		endif
-	endif
-	
+	% DRaburn 2023-01-04:
+	% Alternatives approaches to matHFit are possible. See other versions of jump code.
+	% Omitting fit comparision.
 	%
-	% SKIP - Find nearest point to vecXSeed to use as launch.
+	% TO ADD: DECOMPOSE "SEED" VECX AND VECP.
 	%
-	%
-	% Find point on Levenberg curve subject to trust region.
-	%
+	% Set up Levenber curve.
 	vecYLaunch = zeros(size(vecGammaFit));
 	fLaunch = fFit;
 	vecGammaLaunch = vecGammaFit;
@@ -1234,27 +1203,17 @@ function [ vecXNew, vecPNew, jumpDat ] = __jump_januarySeedy( vecXSeed, vecPSeed
 	trCoeff = mygetfield( prm, "trCoeff", 3.0 );
 	bMax = trCoeff * stepSizeCoeff;
 	levPrm = [];
+	%
 	vecZ = levsol_eig( fLaunch, vecGammaLaunch, matHFit, matB, bMax, levPrm );
-	if ( mygetfield( prm, "debugMode", false ) )
-		vecDelta = matV*vecZ;
-		vecCapFS = max(abs(matDForFit),[],2);
-		vecCapFS += sqrt(eps)*max(vecCapFS);
-		if ( abs(vecZ)./vecCap > bMax*1.1 || abs(vecDelta)./vecCapFS > bMax*1.1 )
-			[ vecDelta, vecCapFS, abs(vecDelta)./vecCapFS ]
-			[ vecZ, vecCap, abs(vecZ)./vecCap ]
-			bMax
-		endif
-		assert( abs(vecZ)./vecCap <= bMax*1.1 || abs(vecDelta)./vecCapFS > bMax*1.1 )
-	endif
+	% Omitting checks on "cap/matB".
 	%
 	% Note that fNew and vecGammaNew are merely estimates.
 	vecYNew = vecYLaunch + vecZ;
 	fNew = fFit + vecYNew'*vecGammaFit + (vecYNew'*matHFit*vecYNew)/2.0;
 	vecGammaNew = vecGammaFit + matHFit*vecYNew;
 	%
-	% SKIP - Decomposition of vecXSeed and vecPSeed and analysis
-	%  to get improved vecXNew and vecPNew, as well as make output "new" values
-	%  approach input "seed" values in the limit of step size -> 0.
+	% TO ADD: Make use of decomposition of seed data to improve "new" values
+	%  and yield continuity in the limit of step size -> 0.
 	vecXNew = vecXAnchor + matV*vecYNew;
 	vecPNew = vecPSeed * norm(vecGammaNew) / norm(vecGammaFit);
 return;
