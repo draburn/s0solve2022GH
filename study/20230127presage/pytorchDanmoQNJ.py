87948c280edace7ec877d6893faad0a8da33e998
Plasma Dan <draburn@alumni.princeton.edu>
2023-01-28 00:06:52 -0800

    "Should we use metric?" "No, we've always used spaces!"

--- study/20230127presage/pytorchDanmoQNJ.py
+++ study/20230127presage/pytorchDanmoQNJ.py
@@ -212,128 +212,128 @@ from scipy import optimize
 # Init logging.
 frame = inspect.currentframe()
 def msg( *arguments, **keywords ):
-	#print( f"[", __file__, ".", frame.f_lineno, "] ", *arguments, **keywords )
-	print( f'[{__file__}.{frame.f_lineno:05d}]', *arguments, **keywords )
+    #print( f"[", __file__, ".", frame.f_lineno, "] ", *arguments, **keywords )
+    print( f'[{__file__}.{frame.f_lineno:05d}]', *arguments, **keywords )
 def reldiff( a, b ):
-	sa = np.sum(np.abs(a))
-	sb = np.sum(np.abs(b))
-	if ( 0.0 == sa and 0.0 == sb ):
-		return 0.0
-	return ( np.sum(np.abs(a-b)) / ( sa + sb ) )
+    sa = np.sum(np.abs(a))
+    sb = np.sum(np.abs(b))
+    if ( 0.0 == sa and 0.0 == sb ):
+        return 0.0
+    return ( np.sum(np.abs(a-b)) / ( sa + sb ) )
 def utorthdrop( matA, dropRelThresh, dropAbsThresh ):
-	#msg( 'Hey hey hey!' )
-	matV = matA.copy() # Superfluous?
-	sizeK = matA.shape[1]
-	#msg( 'sizeK = ', sizeK )
-	rvcDrop = np.zeros( (sizeK), dtype='bool' )
-	rvcDrop[:] = False # Superfluous?
-	#msg( 'matV = \n', matV )
-	#msg( 'rvcDrop =', rvcDrop )
-	for k in range ( 0, sizeK ):
-		vNorm = linalg.norm( matV[:,k] )
-		if ( vNorm <= dropAbsThresh ):
-			rvcDrop[k] = True
-			matV[:,k] = 0.0
-		else:
-			#msg( f'divding {k} by {vNorm}.' )
-			matV[:,k] /= vNorm
-	#msg( 'matV = \n', matV )
-	#msg( 'rvcDrop =', rvcDrop )
-	for k in range ( 1, sizeK ):
-		###if ( rvcDrop[k] ):
-		###	continue
-		#msg( 'k = ', k )
-		#msg( 'matV[:,k] = ', matV[:,k] )
-		matV[:,k] -= matV[:,0:k] @ ( matV[:,0:k].T @ matV[:,k] )
-		#msg( 'matV[:,k] = ', matV[:,k] )
-		vNorm = linalg.norm( matV[:,k] )
-		if ( vNorm <= dropRelThresh ):
-			rvcDrop[k] = True
-			matV[:,k] = 0.0
-		else:
-			matV[:,k] /= vNorm
-			matV[:,k] -= matV[:,0:k] @ ( matV[:,0:k].T @ matV[:,k] )
-			vNorm = linalg.norm( matV[:,k] )
-			if ( vNorm <= dropRelThresh ):
-				rvcDrop[k] = True
-				matV[:,k] = 0.0
-			else:
-				matV[:,k] /= vNorm
-				# Note: if dropThresh is too small, may end up keeping more than sizeX vectors.
-	#msg( 'matV = \n', matV )
-	#msg( 'rvcDrop =', rvcDrop )
-	rvcKeep = ~rvcDrop
-	matV = matV[:,rvcKeep]
-	#msg( 'matV = \n', matV )
-	return ( matV, rvcKeep )
+    #msg( 'Hey hey hey!' )
+    matV = matA.copy() # Superfluous?
+    sizeK = matA.shape[1]
+    #msg( 'sizeK = ', sizeK )
+    rvcDrop = np.zeros( (sizeK), dtype='bool' )
+    rvcDrop[:] = False # Superfluous?
+    #msg( 'matV = \n', matV )
+    #msg( 'rvcDrop =', rvcDrop )
+    for k in range ( 0, sizeK ):
+        vNorm = linalg.norm( matV[:,k] )
+        if ( vNorm <= dropAbsThresh ):
+            rvcDrop[k] = True
+            matV[:,k] = 0.0
+        else:
+            #msg( f'divding {k} by {vNorm}.' )
+            matV[:,k] /= vNorm
+    #msg( 'matV = \n', matV )
+    #msg( 'rvcDrop =', rvcDrop )
+    for k in range ( 1, sizeK ):
+        ###if ( rvcDrop[k] ):
+        ###    continue
+        #msg( 'k = ', k )
+        #msg( 'matV[:,k] = ', matV[:,k] )
+        matV[:,k] -= matV[:,0:k] @ ( matV[:,0:k].T @ matV[:,k] )
+        #msg( 'matV[:,k] = ', matV[:,k] )
+        vNorm = linalg.norm( matV[:,k] )
+        if ( vNorm <= dropRelThresh ):
+            rvcDrop[k] = True
+            matV[:,k] = 0.0
+        else:
+            matV[:,k] /= vNorm
+            matV[:,k] -= matV[:,0:k] @ ( matV[:,0:k].T @ matV[:,k] )
+            vNorm = linalg.norm( matV[:,k] )
+            if ( vNorm <= dropRelThresh ):
+                rvcDrop[k] = True
+                matV[:,k] = 0.0
+            else:
+                matV[:,k] /= vNorm
+                # Note: if dropThresh is too small, may end up keeping more than sizeX vectors.
+    #msg( 'matV = \n', matV )
+    #msg( 'rvcDrop =', rvcDrop )
+    rvcKeep = ~rvcDrop
+    matV = matV[:,rvcKeep]
+    #msg( 'matV = \n', matV )
+    return ( matV, rvcKeep )
 def getLambdaFloor( f0, vecPhi, vecLambda, fFloor ):
-	def fRes( lambdaF ):
-		fRes = f0 - fFloor
-		for k in range( 0, vecLambda.shape[0] ):
-			fRes -= ((vecPhi[k])**2) / (2.0*max( vecLambda[k], lambdaF ))
-		return fRes
-	assert f0 > fFloor
-	if ( min(vecLambda) > 0.0 ):
-		if ( fRes( 0.0 ) >= 0.0 ):
-			return 0.0
-	lambdaHi = (vecPhi @ vecPhi) / ( 2.0 * ( f0 - fFloor )  )
-	#msg( 'lambda: ', lambdaHi )
-	if ( lambdaHi > max(vecLambda) ):
-		return lambdaHi
-	lambdaLo = MYEPS * max(vecLambda)
-	#msg( 'lambda: ', lambdaHi, lambdaLo )
-	if ( fRes( lambdaLo ) >= 0.0 ):
-		return lambdaLo
-	lambdaF = optimize.bisect( fRes, lambdaLo, lambdaHi )
-	#msg( 'lambda: ', lambdaHi, lambdaLo, lambdaF )
-	#msg( 'res = ', fRes(lambdaF) )
-	#exit()
-	return lambdaF
+    def fRes( lambdaF ):
+        fRes = f0 - fFloor
+        for k in range( 0, vecLambda.shape[0] ):
+            fRes -= ((vecPhi[k])**2) / (2.0*max( vecLambda[k], lambdaF ))
+        return fRes
+    assert f0 > fFloor
+    if ( min(vecLambda) > 0.0 ):
+        if ( fRes( 0.0 ) >= 0.0 ):
+            return 0.0
+    lambdaHi = (vecPhi @ vecPhi) / ( 2.0 * ( f0 - fFloor )  )
+    #msg( 'lambda: ', lambdaHi )
+    if ( lambdaHi > max(vecLambda) ):
+        return lambdaHi
+    lambdaLo = MYEPS * max(vecLambda)
+    #msg( 'lambda: ', lambdaHi, lambdaLo )
+    if ( fRes( lambdaLo ) >= 0.0 ):
+        return lambdaLo
+    lambdaF = optimize.bisect( fRes, lambdaLo, lambdaHi )
+    #msg( 'lambda: ', lambdaHi, lambdaLo, lambdaF )
+    #msg( 'res = ', fRes(lambdaF) )
+    #exit()
+    return lambdaF
 # Note:
 #  "zeta" here was "phi" in 20230106stage\levsol0111.m;
 #  "phi" here was "gamma" in 20230106stage\levsol0111.m.
 def levsol( f0, vecPhi, matPsi, vecLambdaCurve, sMax, vecS, dMax, vecLambdaObjf, fMin ):
-	def zetaOfP( p ):
-		if ( p < MYEPS ):
-			vecZeta = p * vecPhi / np.min( vecLambdaCurve )
-		else:
-			mu = np.min( vecLambdaCurve ) * ( (1.0/p) - 1.0 )
-			vecZeta = vecPhi / ( vecLambdaCurve + mu )
-		return vecZeta
-	def deltaYOfP( p ):
-		vecZeta = zetaOfP( p )
-		vecDeltaY = ( matPsi @ vecZeta ) / vecS
-		return vecDeltaY
-	def fOfP( p ):
-		vecZeta = zetaOfP( p )
-		f = f0 - ( vecZeta @ vecPhi ) + (( vecZeta @ ( vecLambdaObjf * vecZeta ))/2.0)
-		return f
-	def sPastMaxOfP( p ):
-		return linalg.norm( zetaOfP(p) ) - sMax
-	def dPastMaxOfP( p ):
-		return linalg.norm( deltaYOfP(p) ) - dMax
-	def fTillMinOfP( p ):
-		return fOfP( p ) - fMin
-	assert np.min(vecS) > 0.0
-	assert np.min(vecLambdaCurve) > 0.0
-	assert linalg.norm(vecPhi) > 0.0
-	p1 = 1.0
-	if ( sMax > 0.0 ):
-		assert sPastMaxOfP(0.0) < 0.0
-		if ( sPastMaxOfP(p1) > 0.0 ):
-			p1New = optimize.bisect( sPastMaxOfP, 0.0, p1 )
-			p1 = p1New
-	if ( dMax > 0.0 ):
-		assert dPastMaxOfP(0.0) < 0.0
-		if ( dPastMaxOfP(p1) > 0.0 ):
-			p1New = optimize.bisect( dPastMaxOfP, 0.0, p1 )
-			p1 = p1New
-	# Ugh. Just require fMin.
-	assert fTillMinOfP(0.0) > 0.0
-	if ( fTillMinOfP(p1) < 0.0 ):
-		p1New = optimize.bisect( fTillMinOfP, 0.0, p1 )
-		p1New = p1
-	return deltaYOfP( p1 )
+    def zetaOfP( p ):
+        if ( p < MYEPS ):
+            vecZeta = p * vecPhi / np.min( vecLambdaCurve )
+        else:
+            mu = np.min( vecLambdaCurve ) * ( (1.0/p) - 1.0 )
+            vecZeta = vecPhi / ( vecLambdaCurve + mu )
+        return vecZeta
+    def deltaYOfP( p ):
+        vecZeta = zetaOfP( p )
+        vecDeltaY = ( matPsi @ vecZeta ) / vecS
+        return vecDeltaY
+    def fOfP( p ):
+        vecZeta = zetaOfP( p )
+        f = f0 - ( vecZeta @ vecPhi ) + (( vecZeta @ ( vecLambdaObjf * vecZeta ))/2.0)
+        return f
+    def sPastMaxOfP( p ):
+        return linalg.norm( zetaOfP(p) ) - sMax
+    def dPastMaxOfP( p ):
+        return linalg.norm( deltaYOfP(p) ) - dMax
+    def fTillMinOfP( p ):
+        return fOfP( p ) - fMin
+    assert np.min(vecS) > 0.0
+    assert np.min(vecLambdaCurve) > 0.0
+    assert linalg.norm(vecPhi) > 0.0
+    p1 = 1.0
+    if ( sMax > 0.0 ):
+        assert sPastMaxOfP(0.0) < 0.0
+        if ( sPastMaxOfP(p1) > 0.0 ):
+            p1New = optimize.bisect( sPastMaxOfP, 0.0, p1 )
+            p1 = p1New
+    if ( dMax > 0.0 ):
+        assert dPastMaxOfP(0.0) < 0.0
+        if ( dPastMaxOfP(p1) > 0.0 ):
+            p1New = optimize.bisect( dPastMaxOfP, 0.0, p1 )
+            p1 = p1New
+    # Ugh. Just require fMin.
+    assert fTillMinOfP(0.0) > 0.0
+    if ( fTillMinOfP(p1) < 0.0 ):
+        p1New = optimize.bisect( fTillMinOfP, 0.0, p1 )
+        p1New = p1
+    return deltaYOfP( p1 )
 # Init problem.
 MYEPS = 1.0E-8
 rngSeed = 0
@@ -354,11 +354,11 @@ noiseX = 1.0E-5
 #noiseX = 0.0
 # DRaburn 2023-01-27, pytorchDanmo: Not you.
 #def funcFG( x ):
-#	#d = x - vecXCrit
-#	d = x - vecXCrit + noiseX*rng.standard_normal(( sizeX ))
-#	g = matHCrit @ d
-#	f = fCrit + (( d @ g )/2.0)
-#	return ( f, g )
+#    #d = x - vecXCrit
+#    d = x - vecXCrit + noiseX*rng.standard_normal(( sizeX ))
+#    g = matHCrit @ d
+#    f = fCrit + (( d @ g )/2.0)
+#    return ( f, g )
 #vecX0 = np.zeros(( sizeX ))
 #f0, vecG0 = funcFG( vecX0 )
 #msg( 'f0 = ', f0 )
