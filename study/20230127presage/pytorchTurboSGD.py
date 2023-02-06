f48e2cd47d5880a3a309db700c72dac0e93376c3
Plasma Dan <draburn@alumni.princeton.edu>
2023-02-06 00:25:39 -0800

    Revised pytorchTurboSGD.py.

--- study/20230127presage/pytorchTurboSGD.py
+++ study/20230127presage/pytorchTurboSGD.py
@@ -12,6 +12,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+import numpy
+import matplotlib.pyplot as plt
+torch.manual_seed(0)
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -72,16 +76,21 @@ net = Net()
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+###optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.0)
 
 
 #
 # 4. Train the network
 #
 
-for epoch in range(2):  # loop over the dataset multiple times
+print('Starting Training')
+
+###for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(250):  # loop over the dataset multiple times
 
     running_loss = 0.0
+    running_minibatchcount = 0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
@@ -97,12 +106,11 @@ for epoch in range(2):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
+        running_minibatchcount += 1
+    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / running_minibatchcount:.17f}')
 
 print('Finished Training')
 
 # Let's quickly save...
-PATH = './cifar_net.pth'
+PATH = './temp.pth'
 torch.save(net.state_dict(), PATH)
