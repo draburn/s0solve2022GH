41cae4e9a787315f375a16057e637b3c397f7d15
Plasma Dan <draburn@alumni.princeton.edu>
2023-02-20 17:46:48 -0800

    z_qnjsgd0220 is a go!

--- study/20230219simpledemos/z_qnjsgd0220.py
+++ study/20230219simpledemos/z_qnjsgd0220.py
@@ -26,23 +26,25 @@ import matplotlib.pyplot as plt
 msgtime()
 
 msg('')
-learning_rate = 1.0e-5
+learning_rate = 1.0e-1
+momentum_coefficient = 0.9
+tr_size = 1.0e-1
 epoch_limit = 50
 max_num_records = 10
 divergence_coeff = 100.0
 report_interval = 1
 msg(f'learning_rate = {learning_rate:0.18e}')
-msg('*** UGH! HACKY! SOLVER SHOULD BE SUCH-THAT learning_rate IS UNNECESSARY! ***')
+msg(f'momentum_coefficient = {momentum_coefficient:0.18e}')
+msg(f'tr_size = {tr_size:0.18e}')
 msg(f'epoch_limit = {epoch_limit}')
 msg(f'max_num_records = {max_num_records}')
 msg(f'divergence_coeff = {divergence_coeff:0.18e}')
 msg(f'report_interval = {report_interval}')
-vecX0 = prob.get_vecX()
-sizeX = vecX0.shape[0]
-f0, vecG0 = prob.fgeval(vecX0)
-msg(f'vecX0[0] = {vecX0[0]:0.18e}, ||vecX0|| = {norm(vecX0):0.18e}')
-msg(f'f0 = {f0:0.18E}')
-msg(f'vecG0[0] = {vecG0[0]:0.18E}, ||vecG0|| = {norm(vecG0):0.18E}')
+vecXSeed0 = prob.get_vecX()
+sizeX = vecXSeed0.shape[0]
+vecPSeed0 = np.zeros(sizeX)
+msg(f'vecXSeed0[0] = {vecXSeed0[0]:0.18e}, ||vecXSeed0|| = {norm(vecXSeed0):0.18e}')
+msg(f'vecPSeed0[0] = {vecPSeed0[0]:0.18e}, ||vecPSeed0|| = {norm(vecPSeed0):0.18e}')
 msgtime()
 
 msg('')
@@ -51,16 +53,21 @@ record_matX = np.zeros((sizeX, max_num_records))
 record_matG = np.zeros((sizeX, max_num_records))
 record_rvcF = np.zeros((1, max_num_records))
 num_records = 0
-vecX = vecX0
-f = f0
-vecG = vecG0
-ignore_vecPHarvest = np.zeros(sizeX)
-tr_size = 1.0e-1
+vecXSeed = vecXSeed0.copy()
+vecPSeed = vecPSeed0.copy()
+vecXHarvest = np.zeros(sizeX)
+vecPHarvest = np.zeros(sizeX)
 qnj_prm = qnj.prm()
 print('[')
-print(f'[{time.time()-start_time:8.2f} {0:6d} {f0:12.6e} {norm(vecG0):12.6e}]')
 for epoch_index in range(epoch_limit):
 	
+	f, vecG, dat = prob.eval_epoch_sgd( vecXSeed, vecPSeed, learning_rate, momentum_coefficient, -1 )
+	vecXHarvest[:] = dat.vecXHarvest[:]
+	vecPHarvest[:] = dat.vecPHarvest[:]
+	if ( 0 == epoch_index ):
+		f0 = f
+		vecG0 = vecG.copy()
+		
 	# Add record.
 	# DRaburn 2023-02-16: Always rolling may be wasteful, but POITROME.
 	record_matX = np.roll( record_matX, 1 )
@@ -68,15 +75,15 @@ for epoch_index in range(epoch_limit):
 	record_rvcF = np.roll( record_rvcF, 1 )
 	if ( num_records < max_num_records ):
 		num_records += 1
-	record_matX[:,0] = vecX[:]
+	record_matX[:,0] = dat.avg_vecX[:]
 	record_matG[:,0] = vecG[:]
 	record_rvcF[0,0] = f
 	
 	# Calculate jump.
 	qnj_prm.doViz = False
-	qnjCode, vecXNext, ignore_vecPNext, sizeK, gammaRat, fPred = qnj.calcJump(
-	  vecX,
-	  ignore_vecPHarvest,
+	qnjCode, vecXNext, vecPNext, sizeK, gammaRat, fPred = qnj.calcJump(
+	  vecXHarvest,
+	  vecPHarvest,
 	  record_matX[:,0:num_records],
 	  record_matG[:,0:num_records],
 	  record_rvcF[:,0:num_records],
@@ -87,9 +94,9 @@ for epoch_index in range(epoch_limit):
 		qnj_prm.doViz = True
 	if (qnj_prm.doViz):
 		qnj_prm.viz_numPts = 100
-		qnjCode, vecXNext, ignore_vecPNext, sizeK, gammaRat, fPred, lev_matDelta, lev_vecD, lev_vecF, lev_vecFC = qnj.calcJump(
-		  vecX,
-		  ignore_vecPHarvest,
+		qnjCode, vecXNext, vecPNext, sizeK, gammaRat, fPred, lev_matDelta, lev_vecD, lev_vecF, lev_vecFC = qnj.calcJump(
+		  vecXHarvest,
+		  vecPHarvest,
 		  record_matX[:,0:num_records],
 		  record_matG[:,0:num_records],
 		  record_rvcF[:,0:num_records],
@@ -98,7 +105,7 @@ for epoch_index in range(epoch_limit):
 		lev_vecFActual = np.zeros(qnj_prm.viz_numPts)
 		for n in range (qnj_prm.viz_numPts):
 			msg(f'Viz: {n} / {qnj_prm.viz_numPts}')
-			temp_f, temp_vecG = prob.fgeval( vecX + lev_matDelta[:,n])
+			temp_f, temp_vecG = prob.fgeval( vecXHarvest + lev_matDelta[:,n])
 			lev_vecFActual[n] = temp_f
 		plt.plot(lev_vecD, lev_vecF, 'o-')
 		plt.plot(lev_vecD, lev_vecFC, 's-')
@@ -111,8 +118,6 @@ for epoch_index in range(epoch_limit):
 		msg('HALT!')
 		exit()
 	
-	vecX[:] = vecXNext[:] + ((-learning_rate)*vecG[:])
-	f, vecG = prob.fgeval(vecX)
 	if (f > divergence_coeff*f0):
 		print(f'[{time.time()-start_time:8.2f} {epoch_index:6d} {f:12.6e} {norm(vecG):12.6e}]')
 		print('];')
@@ -133,6 +138,9 @@ for epoch_index in range(epoch_limit):
 		break
 	if ((report_interval > 0) and ((epoch_index+1)%report_interval == 0)):
 		print(f'[{time.time()-start_time:8.2f} {epoch_index+1:6d} {f:12.6e} {norm(vecG):12.6e}]')
+	
+	vecXSeed[:] = vecXHarvest[:]
+	vecPSeed[:] = vecPHarvest[:]
 # End epoch loop.
 print('];')
 print('')
@@ -141,7 +149,7 @@ msgtime()
 msg('Finished main loop.')
 
 msg('')
-vecXF = vecX.copy()
+vecXF = vecXHarvest.copy()
 fF, vecGF = prob.fgeval(vecXF)
 msg(f'vecXF[0] = {vecXF[0]:0.18e}, ||vecXF|| = {norm(vecXF):0.18e}')
 msg(f'fF = {fF:0.18E}')
