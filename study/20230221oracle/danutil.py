ee698be1cf3238ab16c992f97546831b8b728f9a
Plasma Dan <draburn@alumni.princeton.edu>
2023-02-22 13:13:18 -0800

    Moved (Python) utorthdrop to danutil.

--- study/20230221oracle/danutil.py
+++ study/20230221oracle/danutil.py
@@ -32,3 +32,37 @@ def reldiff( a, b ):
 def msg( *arguments, **keywords ):
 	print(f'[{inspect.stack()[1].filename}.{inspect.stack()[1].lineno:05d}]', *arguments, **keywords)
 # End def msg().
+
+# Upper-triangular orthonormalization, with drop.
+# DRaburn 2023-02-21:
+#  Ideally, we'd also calculate and return the matR of the QR factorization, but, POITROME.
+def utorthdrop( matA, dropRelThresh, dropAbsThresh ):
+	matV = matA.copy() # Superfluous?
+	sizeK = matA.shape[1]
+	vecKeep = np.zeros((sizeK), dtype='bool')
+	vecKeep[:] = True
+	for k in range (sizeK):
+		vNorm = norm(matV[:,k])
+		if ( vNorm <= dropAbsThresh ):
+			vecKeep[k] = False
+			matV[:,k] = 0.0
+		else:
+			matV[:,k] /= vNorm
+	for k in range (1, sizeK):
+		matV[:,k] -= matV[:,0:k] @ ( matV[:,0:k].T @ matV[:,k] )
+		vNorm = norm( matV[:,k] )
+		if ( vNorm <= dropRelThresh ):
+			vecKeep[k] = False
+			matV[:,k] = 0.0
+		else:
+			matV[:,k] /= vNorm
+			matV[:,k] -= matV[:,0:k] @ ( matV[:,0:k].T @ matV[:,k] )
+			vNorm = norm( matV[:,k] )
+			if ( vNorm <= dropRelThresh ):
+				vecKeep[k] = False
+				matV[:,k] = 0.0
+			else:
+				matV[:,k] /= vNorm
+	matV = matV[:,vecKeep]
+	return matV, vecKeep
+# End utorthdrop().
