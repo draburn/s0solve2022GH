55e94e5568f2cec43092cfbe08a33e3bb24cf47e
Plasma Dan <draburn@alumni.princeton.edu>
2025-05-01 14:49:09 -0700

    .

--- init.m
+++ init.m
@@ -6,7 +6,8 @@ addpath( "myutil" );
 my_editor = "/usr/bin/mousepad";
 EDITOR( [my_editor ' %s'] ); % Moot if not USING_GUI; see options within GUI.
 %
-switch 340
+%switch 340
+switch 330
 case 10
 	msg(__FILE__,__LINE__,"Adding path \"study/20220222findLocMin/testfunc2021/\".");
 	addpath("study/20220222findLocMin/testfunc2021/");
